39125c745ab4e59199ff32ceb13795d0e52ed456
dimaslanjaka <dimaslanjaka@gmail.com>
2021-08-18 23:48:36 +0700

    try fix

--- themes/hexo-webnary-theme/scripts/after_render.js
+++ themes/hexo-webnary-theme/scripts/after_render.js
@@ -54,7 +54,28 @@ function MDReadFile(data) {
     fs.writeFileSync(path.join(rootFolder, "docs/log/Readfile.json"), JSON.stringify(match, null, 2));
   }
 
-  fs.writeFileSync(path.join(rootFolder, "docs/log/data.json"), JSON.stringify(data, null, 2));
+  fs.writeFileSync(path.join(rootFolder, "docs/log/data.json"), JSON.stringify(censor(data), null, 2));
+}
+
+/**
+ * Avoid the circular references.
+ * @param {Object} censor
+ * @returns
+ */
+function censor(censor) {
+  var i = 0;
+
+  return function (key, value) {
+    if (i !== 0 && typeof censor === "object" && typeof value == "object" && censor == value) return "[Circular]";
+
+    if (i >= 29)
+      // seems to be a harded maximum of 30 serialized objects?
+      return "[Unknown]";
+
+    ++i; // so we know we aren't using the original object anymore
+
+    return value;
+  };
 }
 
 hexo.extend.filter.register("after_post_render", (data) => {
