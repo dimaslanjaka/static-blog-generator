f34f4ce37d856d3fa1c9fc53023a668907acffab
dimaslanjaka <dimaslanjaka@gmail.com>
2021-08-20 09:38:12 +0700

    Add Workflow

--- src/electron-express.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import spawner from "./node/spawner";
-import { kill } from "./electron/kill";
-
-spawner.spawn("npm", ["run", "dev:nodemon"]);
-spawner.spawn("npm", ["run", "dev:electron"], function (child) {
-  kill.pids.push(child.pid);
-});
