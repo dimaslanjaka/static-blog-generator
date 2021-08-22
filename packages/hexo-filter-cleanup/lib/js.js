71c49c70c02dd2b7c481de28bfecefa10189f153
dimaslanjaka <dimaslanjaka@gmail.com>
2021-08-22 15:26:03 +0700

    Fix

--- packages/hexo-filter-cleanup/lib/js.js
+++ packages/hexo-filter-cleanup/lib/js.js
@@ -6,7 +6,6 @@ const minimatch = require("minimatch");
 const uglifyJS = require("uglify-js");
 const pkg = require("../package.json");
 const utils = require("./utils");
-const Terser = require("terser");
 
 module.exports = function (str, data) {
   let hexo = this,
@@ -24,57 +23,16 @@ module.exports = function (str, data) {
   delete options.exclude;
 
   return new Promise(function (resolve, reject) {
-    // noinspection DuplicatedCode
-    utils.isFileChanged(path0).then(async (yes) => {
+    utils.isFileChanged(path0).then((yes) => {
       let rstr = "";
       if (!yes) {
         rstr = utils.getFileCache(path0);
         return resolve(rstr);
       }
       // file was changed
-      //let result = uglifyJS.minify(str, options);
-      const result = await Terser.minify(str, {
-        parse: {
-          ecma: 8,
-        },
-        compress: {
-          ecma: 5,
-          warnings: false,
-          arrows: false,
-          collapse_vars: false,
-          comparisons: false,
-          computed_props: false,
-          hoist_funs: false,
-          hoist_props: false,
-          hoist_vars: false,
-          inline: false,
-          loops: false,
-          negate_iife: false,
-          properties: false,
-          reduce_funcs: false,
-          reduce_vars: false,
-          switches: false,
-          toplevel: false,
-          typeofs: false,
-          booleans: true,
-          if_return: true,
-          sequences: true,
-          unused: true,
-          conditionals: true,
-          dead_code: true,
-          evaluate: true,
-        },
-        mangle: {
-          safari10: true,
-        },
-        output: {
-          ecma: 5,
-          comments: false,
-          ascii_only: true,
-        },
-      });
+      let result = uglifyJS.minify(str, options);
       // minify fails, use raw code and print warning msg
-      if (result.hasOwnProperty("error")) {
+      if (result.error) {
         log.warn("%s(JS): %s Error: [ %s ], Skip minifying.", pkg.name, path0, result.error);
         return resolve(str);
       }
