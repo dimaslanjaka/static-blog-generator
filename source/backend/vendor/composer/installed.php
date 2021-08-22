600836fbce65aaa8effd04510add68a6b74714a5
dimaslanjaka <dimaslanjaka@gmail.com>
2021-08-22 17:23:14 +0700

    Add Backend Controller

---
+++ source/backend/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '54a9a579dee9bbca5a4f5c1406247b62f90cf1ca',
+        'name' => 'dimas/nodejs',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dimas/nodejs' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '54a9a579dee9bbca5a4f5c1406247b62f90cf1ca',
+            'dev_requirement' => false,
+        ),
+    ),
+);
