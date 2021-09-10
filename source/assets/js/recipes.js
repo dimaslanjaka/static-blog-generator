c8450c958f8040c1c96d178c638fd9784705018b
dimaslanjaka <dimaslanjaka@gmail.com>
2021-09-11 03:01:31 +0700

    Update js rec

--- source/assets/js/recipes.js
+++ source/assets/js/recipes.js
@@ -15,14 +15,15 @@ function CalculateTR(table) {
   if (tr.length > 0)
     for (i = 0; i < tr.length; i++) {
       let td = tr[i].getElementsByTagName("td"),
-        str;
-      if (typeof td[3] != "undefined") {
+        str,
+        thirdTD = typeof td[3] != "undefined";
+
+      if (thirdTD) {
         str = td[3].innerText;
+        console.log(str);
         if (/[+\(\)]/gm.test(str)) {
-          console.log(str);
           str = str.trim().replaceAll(/x/gs, "*");
           td[3].innerText = eval(str);
-          console.log(str);
         }
       }
     }
