6630f97f8494a41a5a6a9e95a310492bf0c8004a
dimaslanjaka <dimaslanjaka@gmail.com>
2021-09-11 02:58:20 +0700

    Migrate assets

--- source/assets/js/recipes.js
+++ source/assets/js/recipes.js
@@ -10,6 +10,7 @@ for (i = 0; i < table.length; i++) {
  * @param {HTMLTableElement} table
  */
 function CalculateTR(table) {
+  console.log(`Start Calculating Table ${table.id}`);
   let tr = table.getElementsByTagName("tr");
   if (tr.length > 0)
     for (i = 0; i < tr.length; i++) {
@@ -18,6 +19,7 @@ function CalculateTR(table) {
       if (typeof td[3] != "undefined") {
         str = td[3].innerText;
         if (/[+\(\)]/gm.test(str)) {
+          console.log(str);
           str = str.trim().replaceAll(/x/gs, "*");
           td[3].innerText = eval(str);
           console.log(str);
