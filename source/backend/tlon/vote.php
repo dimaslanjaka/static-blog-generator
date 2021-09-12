940e4314d48489626b1f210d75cdffc7141a089c
dimaslanjaka <dimaslanjaka@gmail.com>
2021-09-12 23:44:38 +0700

    update

--- source/backend/tlon/vote.php
+++ source/backend/tlon/vote.php
@@ -66,6 +66,10 @@ function getIpAddress()
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
   <style>
+    * {
+      word-wrap: break-word;
+    }
+
     @media screen and (max-width:576px) {
       .btn-group {
         display: flex;
