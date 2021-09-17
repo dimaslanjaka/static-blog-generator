d77d1de786489d35bb275562903f568d0e60c61c
dimaslanjaka <dimaslanjaka@gmail.com>
2021-09-17 18:06:18 +0700

    Up

---
+++ docs-/backend/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '562410dee0704628be504c525214c08c3a067408',
+        'name' => 'dimas/nodejs',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dimas/nodejs' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '562410dee0704628be504c525214c08c3a067408',
+            'dev_requirement' => false,
+        ),
+    ),
+);
