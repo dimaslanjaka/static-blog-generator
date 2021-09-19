e74d8ba75253b425d5b95f6f638c681404fb2b51
dimaslanjaka <dimaslanjaka@gmail.com>
2021-09-19 17:44:38 +0700

    Add validator

--- userscripts/validate.js
+++ userscripts/validate.js
@@ -2,3 +2,14 @@
 const fs = require("fs");
 const path = require("path");
 const root = path.join(__dirname, "/../");
+const docs = path.join(root, "docs");
+
+let tests = [path.join(docs, "index.html")];
+tests.forEach(function (test) {
+  if (fs.existsSync(test)) {
+    let sizes_byte = fs.statSync(test).size;
+    if (sizes_byte == 0) throw "File size 0";
+    let content_sizes = fs.readFileSync(test).toString().trim().length;
+    if (sizes_byte == 0) throw "File content empty";
+  }
+});
