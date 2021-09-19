124053fff72b187995dcd4256b71ca5c9950e291
dimaslanjaka <dimaslanjaka@gmail.com>
2021-09-19 16:30:48 +0700

    Update

--- userscripts/after_generated.js
+++ userscripts/after_generated.js
@@ -1,6 +1,11 @@
 const fs = require("fs");
 const path = require("path");
 const _ = require("lodash");
+const replaceAll = function (find, replace, str) {
+  var find = find.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
+  return str.replace(new RegExp(find, "g"), replace);
+};
+
 // process docs/assets/js/quiz.js
 let read = fs.readFileSync(path.join(__dirname, "/../docs/assets/js/quiz.js")).toString();
-read = read.replaceAll(/\#uniqid\(\)/gm, _.uniqueId("uuid="));
+read = replaceAll(/\#uniqid\(\)/gm, _.uniqueId("uuid="), read);
