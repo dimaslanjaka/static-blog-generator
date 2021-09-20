118fe3ccbabd97f7db0074c3d76063d8c66af1f5
Dimas Lanjaka <dimaslanjaka@gmail.com>
2021-09-20 22:18:37 +0700

    Update after_generated.js

--- userscripts/after_generated.js
+++ userscripts/after_generated.js
@@ -1,28 +1,18 @@
 const fs = require("fs");
 const path = require("path");
 const _ = require("lodash");
-const replaceAll = function (replaceThis, withThis, inThis) {
-  withThis = withThis.replace(/\$/g, "$$$$");
-  return inThis.replace(
-    new RegExp(
-      replaceThis.replace(
-        /([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|<>\-\&])/g,
-        "\\$&"
-      ),
-      "g"
-    ),
-    withThis
-  );
-};
+
+if (typeof "".replaceAll != "function") {
+  String.prototype.replaceAll = function (search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, "g"), replacement);
+  };
+}
 
 // process docs/assets/js/quiz.js
 let quizjs = path.join(__dirname, "/../docs/assets/js/quiz.js");
 let read = fs.readFileSync(quizjs).toString();
 let regex = /\#uniqid\(\)/gm;
 let uuid = _.uniqueId();
-if (typeof read.replaceAll == "function") {
-  read = read.replaceAll(regex, uuid);
-} else {
-  read = replaceAll(regex, uuid, read);
-}
+read = read.replaceAll(regex, uuid);
 fs.writeFileSync(quizjs, read);
