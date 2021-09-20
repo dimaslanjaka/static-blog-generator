e51e1712fbb29c6a4c8c00329e478c4b8fc39a8f
Dimas Lanjaka <dimaslanjaka@gmail.com>
2021-09-20 21:33:58 +0700

    Update after_generated.js

--- userscripts/after_generated.js
+++ userscripts/after_generated.js
@@ -1,13 +1,28 @@
 const fs = require("fs");
 const path = require("path");
 const _ = require("lodash");
-const replaceAll = function (find, replace, str) {
-  var find = find.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
-  return str.replace(new RegExp(find, "g"), replace);
+const replaceall = function (replaceThis, withThis, inThis) {
+  withThis = withThis.replace(/\$/g, "$$$$");
+  return inThis.replace(
+    new RegExp(
+      replaceThis.replace(
+        /([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|<>\-\&])/g,
+        "\\$&"
+      ),
+      "g"
+    ),
+    withThis
+  );
 };
 
 // process docs/assets/js/quiz.js
 let quizjs = path.join(__dirname, "/../docs/assets/js/quiz.js");
 let read = fs.readFileSync(quizjs).toString();
-read = replaceAll(/\#uniqid\(\)/gm, _.uniqueId(), read);
+let regex = /\#uniqid\(\)/gm;
+let uuid = _.uniqueId();
+if (typeof read.replaceAll == "function") {
+  read.replaceAll(regex, uuid);
+} else {
+  read = replaceAll(regex, uuid, read);
+}
 fs.writeFileSync(quizjs, read);
