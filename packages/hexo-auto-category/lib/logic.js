dd9df58f817f241e5cb3dc11899514f41bd38ee1
dimaslanjaka <dimaslanjaka@gmail.com>
2021-09-22 23:22:17 +0700

    Update

--- packages/hexo-auto-category/lib/logic.js
+++ packages/hexo-auto-category/lib/logic.js
@@ -1,56 +1,73 @@
 "use strict";
 
+const Hexo = require("hexo");
 var front = require("hexo-front-matter");
 var fs = require("hexo-fs");
 debugger;
-let logic = function (data) {
-  var log = this.log;
+/**
+ *
+ */
+class logic extends Hexo {
+  constructor(data) {
+    var log = this.log;
+    var defConfig = {
+      enable: false,
+      depth: null,
+    };
 
-  if (data.layout != "post") {
-    return data;
-  }
-  if (!this.config.render_drafts && data.source.startsWith("_drafts/")) {
-    return data;
-  }
+    this.config = Object.assign(defConfig, this.config);
 
-  var overwrite = true;
-  if (this.config.hasOwnProperty("auto_category") && this.config.auto_category.enable && overwrite) {
-    let postStr;
-    // 1. parse front matter
-    var tmpPost = front.parse(data.raw);
-    // 2. read old categories
-    //
-    // 3. generate categories from directory
-    // var categories = data.slug.split('/');
-    var categories = data.source.split("/");
-    // 3.1 handle depth
-    var depth = this.config.auto_category.depth || categories.length - 2;
-    if (depth == 0) {
-      // Uncategorized
-      //tmpPost.categories = ["Uncategorized"];
+    if (data.layout != "post") {
       return data;
     }
-    var newCategories = categories.slice(1, 1 + Math.min(depth, categories.length - 2));
-    // need to see if categories was defined in front-matter
-    if (tmpPost.categories) {
-      let postCategories = tmpPost.categories;
-      // now check if user defined categories in front-matter as an array. Otherwise if they wrote it like ```categories: work``` then it's the string type!
-      if (typeof tmpPost.categories === "object") {
-        // now convert categories to a merged string
-        postCategories = tmpPost.categories.join("_");
-      }
-      // since categories is already a string, we can now compare to newCategories
-      if (postCategories == newCategories.join("_")) return data;
+    if (!this.config.render_drafts && data.source.startsWith("_drafts/")) {
+      return data;
     }
-    tmpPost.categories = newCategories;
 
-    // 4. process post
-    postStr = front.stringify(tmpPost);
-    postStr = "---\n" + postStr;
-    fs.writeFile(data.full_source, postStr, "utf-8");
-    log.i("Generated: categories [%s] for post [%s]", tmpPost.categories, categories[categories.length - 1]);
+    var overwrite = true;
+    if (
+      this.config.hasOwnProperty("auto_category") &&
+      this.config.auto_category.hasOwnProperty("enable") &&
+      this.config.auto_category.enable &&
+      overwrite
+    ) {
+      let postStr;
+      // 1. parse front matter
+      var tmpPost = front.parse(data.raw);
+      // 2. read old categories
+      //
+      // 3. generate categories from directory
+      // var categories = data.slug.split('/');
+      var categories = data.source.split("/");
+      // 3.1 handle depth
+      var depth = this.config.auto_category.depth || categories.length - 2;
+      if (depth == 0) {
+        // Uncategorized
+        //tmpPost.categories = ["Uncategorized"];
+        return data;
+      }
+      var newCategories = categories.slice(1, 1 + Math.min(depth, categories.length - 2));
+      // need to see if categories was defined in front-matter
+      if (tmpPost.categories) {
+        let postCategories = tmpPost.categories;
+        // now check if user defined categories in front-matter as an array. Otherwise if they wrote it like ```categories: work``` then it's the string type!
+        if (typeof tmpPost.categories === "object") {
+          // now convert categories to a merged string
+          postCategories = tmpPost.categories.join("_");
+        }
+        // since categories is already a string, we can now compare to newCategories
+        if (postCategories == newCategories.join("_")) return data;
+      }
+      tmpPost.categories = newCategories;
+
+      // 4. process post
+      postStr = front.stringify(tmpPost);
+      postStr = "---\n" + postStr;
+      fs.writeFile(data.full_source, postStr, "utf-8");
+      log.i("Generated: categories [%s] for post [%s]", tmpPost.categories, categories[categories.length - 1]);
+    }
+    return data;
   }
-  return data;
-};
+}
 
 module.exports = logic;
