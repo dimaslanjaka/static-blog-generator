d527f3849e8780b40a358a4da73953cdf9809645
dimaslanjaka <dimaslanjaka@gmail.com>
2021-11-01 22:33:08 +0700

    Update

--- gulpfile.ts
+++ gulpfile.ts
@@ -138,9 +138,12 @@ gulp.task("blogger", function (done) {
     output: isProduction ? "./src-posts" : "./build/src-posts",
     hostname: ["webmanajemen.com", "git.webmanajemen.com", "dimaslanjaka.github.io"],
     callback: require("./userscripts/post_callback"),
+    on: {
+      finish: done,
+    },
   });
 
-  done();
+  //done();
 });
 
 gulp.task("arg", function (done) {
