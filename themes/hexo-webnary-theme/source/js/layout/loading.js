3be4bf2ed2898d90e039513d547e1eb5f1654882
dimaslanjaka <dimaslanjaka@gmail.com>
2021-11-02 22:28:11 +0700

    add theme folder

---
+++ themes/hexo-webnary-theme/source/js/layout/loading.js
@@ -0,0 +1,23 @@
+// loading
+document.onreadystatechange = function () {
+  var page = document.getElementById("page");
+  if (document.readyState == "interactive") {
+    window.setTimeout(function () {
+      disableLoad();
+    }, 4000);
+  }
+
+  if (document.readyState == "complete") {
+    if (page.classList.contains("js-hidden")) {
+      disableLoad();
+    }
+  }
+};
+
+function disableLoad() {
+  var page = document.getElementById("page"),
+    loading = document.getElementById("page-loading");
+
+  if (loading) loading.classList.add("js-hidden");
+  if (page) page.classList.remove("js-hidden");
+}
