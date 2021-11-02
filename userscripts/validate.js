e66e6c2139b01eb1880393b28c9e5e66312601fd
dimaslanjaka <dimaslanjaka@gmail.com>
2021-11-02 21:47:21 +0700

    Fix validate

--- userscripts/validate.js
+++ userscripts/validate.js
@@ -4,13 +4,13 @@ const path = require("path");
 const root = path.join(__dirname, "/../");
 const docs = path.join(root, "docs");
 
-let tests = [path.join(docs, "index.html")];
+const tests = [path.join(docs, "index.html")];
 tests.forEach(function (test) {
   if (fs.existsSync(test)) {
-    let sizes_byte = fs.statSync(test).size;
-    if (sizes_byte == 0) exitMsg("File size 0");
-    let content_sizes = fs.readFileSync(test).toString().trim().length;
-    if (content_sizes == 0) exitMsg("File content empty");
+    const sizes_byte = fs.statSync(test).size;
+    if (sizes_byte === 0) exitMsg(test, "File size 0");
+    const content_sizes = fs.readFileSync(test).toString().trim().length;
+    if (content_sizes === 0) exitMsg("File content empty");
   }
 });
 
