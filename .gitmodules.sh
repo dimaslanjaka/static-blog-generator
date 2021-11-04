0300aca73ee406b94c6cfebe227faf5c665b0ad9
dimaslanjaka <dimaslanjaka@gmail.com>
2021-11-04 17:32:21 +0700

    Update submodule

--- .gitmodules.sh
+++ .gitmodules.sh
@@ -1,2 +1,3 @@
 git submodule add -b compiler https://github.com/dimaslanjaka/hexo-blogger-xml.git packages/hexo-blogger-xml
+git submodule add -b theme-v2 https://github.com/dimaslanjaka/niemingzhao.github.io.git themes/material
 git submodule update --remote
