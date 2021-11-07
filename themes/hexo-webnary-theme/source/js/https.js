0ec229627422506801894cf25fedda3865e4c21c
dimaslanjaka <dimaslanjaka@gmail.com>
2021-11-07 22:00:21 +0700

    Update themes

--- themes/hexo-webnary-theme/source/js/https.js
+++
@@ -1,4 +0,0 @@
-const LOCAL_DOMAINS = ["localhost", "127.0.0.1", "0.0.0.0"];
-if (!LOCAL_DOMAINS.includes(window.location.hostname) && location.protocol && location.protocol !== "https:") {
-  location.replace(`https:${location.href.substring(location.protocol.length)}`);
-}
