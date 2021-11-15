b0763c939728837e1e30c3cb87bce35057a25c2b
dimaslanjaka <dimaslanjaka@gmail.com>
2021-11-15 12:01:55 +0700

    fix schema hexo-seo

--- deploy.js
+++ deploy.js
@@ -26,54 +26,14 @@ function run() {
       cmds.shift();
       if (stdout) console.log(stdout);
       if (stderr) console.log(stderr);
-      if (cmds.length > 0) return run(cmds[0]);
+      if (cmds.length > 0) run(cmds[0]);
     } else {
       throw err;
     }
-
-
   });
 }
 
 run();
-
-/*
-exec("npm i -g npm hexo-cli gulp-cli typescript ts-node", (err, stdout, stderr) => {
-  if (!err) {
-    exec("npm install", (err, stdout, stderr) => {
-      if (!err) {
-        exec("npm i ./packages/hexo-filter-cleanup", (err) => {
-          if (!err) {
-            exec("npx gulp article:copy", (err, stdout, stderr) => {
-              if (!err) {
-                exec("hexo generate", (err, stdout, stderr) => {
-                  if (!err) {
-                    exec("npm install", "docs/page", (err, stdout, stderr) => {
-                      if (!err)
-                        exec("node userscripts/custom-domain.js", (err, stdout, stderr) => {
-                          if (!err) {
-                            exec("node userscripts/copy.js", (err, stdout, stderr) => {
-                              if (!err)
-                                exec("node userscripts/after_generated.js", (err, stdout, stderr) => {
-                                  if (!err) {
-                                    exec("node userscripts/validate.js", (err, stdout, stderr) => {});
-                                  }
-                                });
-                            });
-                          }
-                        });
-                    });
-                  }
-                });
-              }
-            });
-          }
-        });
-      }
-    });
-  }
-});
-*/
 
 /**
  * Shadow exec
