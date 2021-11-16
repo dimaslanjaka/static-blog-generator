d362812d14901a1538cc82fcc0bc21ead3c04239
dimaslanjaka <dimaslanjaka@gmail.com>
2021-11-16 16:22:18 +0700

    update

--- userscripts/after_generated.js
+++ userscripts/after_generated.js
@@ -1,19 +1,23 @@
+/* eslint-disable @typescript-eslint/no-this-alias */
 const fs = require("fs");
 const path = require("path");
 const _ = require("lodash");
+const root = path.join(__dirname, "/../");
+const docs = path.join(root, "docs");
 
 if (typeof "".replaceAll != "function") {
   String.prototype.replaceAll = function (search, replacement) {
-    var target = this;
     let find = typeof search == "string" ? new RegExp(search, "g") : search;
-    return target.replace(find, replacement);
+    return this.replace(find, replacement);
   };
 }
 
 // process docs/assets/js/quiz.js
 let quizjs = path.join(__dirname, "/../docs/assets/js/quiz.js");
 let read = fs.readFileSync(quizjs).toString();
-let regex = /\#uniqid\(\)/gm;
+let regex = /#uniqid\(\)/gm;
 let uuid = _.uniqueId();
 read = read.replaceAll(regex, uuid);
 fs.writeFileSync(quizjs, read);
+// copy readme to docs
+fs.copyFileSync(path.join(root, "README.md"), path.join(docs, "readme.md"));
