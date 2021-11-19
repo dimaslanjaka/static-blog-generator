48d1dd97393559f90b275a0d0a9204d0666690cf
dimaslanjaka <dimaslanjaka@gmail.com>
2021-11-19 20:44:21 +0700

    reduce source

--- packages/traffic-generator/dist/traffic-generator/src/views/js/renderer.js
+++
@@ -1,5 +0,0 @@
-var handleError = function (error, url) {
-    var msg = "\n    There was an issue adding \"".concat(url, "\": ").concat(error.message, "\n  ").trim();
-    setTimeout(function () { return errorMessage.innerText = null; }, 5000);
-};
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicmVuZGVyZXIuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi8uLi9zcmMvdmlld3MvanMvcmVuZGVyZXIuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQ0EsSUFBTSxXQUFXLEdBQUcsVUFBQyxLQUFLLEVBQUUsR0FBRztJQUM3QixJQUFNLEdBQUcsR0FBRyw0Q0FDbUIsR0FBRyxpQkFBTSxLQUFLLENBQUMsT0FBTyxTQUNwRCxDQUFDLElBQUksRUFBRSxDQUFDO0lBQ1QsVUFBVSxDQUFDLGNBQU0sT0FBQSxZQUFZLENBQUMsU0FBUyxHQUFHLElBQUksRUFBN0IsQ0FBNkIsRUFBRSxJQUFJLENBQUMsQ0FBQztBQUN4RCxDQUFDLENBQUEifQ==
--- packages/traffic-generator/src/context-menu/menu2.ts
+++ packages/traffic-generator/src/context-menu/menu2.ts
@@ -1,27 +1,12 @@
-import { Menu, MenuItem } from "electron";
 import contextMenu from ".";
-
-const menu = new Menu();
-
-//Basic Menu For Testing
-menu.append(
-  new MenuItem({
-    label: "MenuItem1",
-    click: function () {
-      console.log("YES");
-    }
-  })
-);
-menu.append(new MenuItem({ type: "separator" }));
-menu.append(
-  new MenuItem({ label: "MenuItem2", type: "checkbox", checked: true })
-);
 
 export default function (app: Electron.App) {
   app.on("web-contents-created", (e, contents) => {
     if (contents.getType() == "webview") {
-      // set context menu in webview
-      contextMenu({ window: contents });
+      // set context menu in
+      const opt = contextMenu.defaultActions;
+      opt.window = contents;
+      contextMenu.default(opt);
     }
   });
 }
