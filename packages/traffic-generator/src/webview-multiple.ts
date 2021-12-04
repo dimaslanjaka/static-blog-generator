be17458dc7444993225b83fa05738c809ed9225a
dimaslanjaka <dimaslanjaka@gmail.com>
2021-12-04 11:16:06 +0700

    electron multiple webview update

--- packages/traffic-generator/src/webview-multiple.ts
+++ packages/traffic-generator/src/webview-multiple.ts
@@ -1,6 +1,6 @@
-import { app, BrowserWindow, globalShortcut } from "electron";
+import { app, BrowserWindow, session } from "electron";
 import path from "path";
-import webviewProxy from "./proxies/webview-proxy";
+import { isAds } from "./proxies/webview-proxy";
 import * as proxies from "./proxies";
 let win: BrowserWindow;
 
@@ -14,16 +14,31 @@ function createWindow() {
     }
   });
 
-  function injectWebViewProxy(url?: string) {
-    const proxy = proxies.random();
+  function injectWebViewProxy() {
     const persists = ["persist:webviewsession", "persist:multi1"];
-    persists.forEach((persist) => {
-      webviewProxy(proxy, persist, (details) => {
-        // delete dead proxy
-        // proxies.remove(proxy);
-        // proxy = proxies.random();
-        // rotate proxies
-        injectWebViewProxy(details.url);
+    persists.forEach((partition) => {
+      const proxy = proxies.random();
+      const ses = session.fromPartition(partition);
+      ses.setProxy({ proxyRules: proxy }).then(() => {
+        console.log(`injected [${partition}] with proxy: ${proxy}`);
+      });
+      ses.webRequest.onErrorOccurred((details) => {
+        if (
+          details.resourceType == "mainFrame" &&
+          !isAds(new URL(details.url).hostname)
+        ) {
+          const errors = ["ERR_TIMED_OUT", "ERR_PROXY_CONNECTION_FAILED"];
+          // check if the string has some of the terms
+          const hasProxyError = errors.some((term) =>
+            details.error.toUpperCase().includes(term)
+          );
+
+          console.log(
+            hasProxyError ? "proxy error" : "request error",
+            details.error,
+            details.url
+          );
+        }
       });
     });
 
