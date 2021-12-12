a9aa0eb0b8e63236c0cfb8c7f87a1aafe81dd361
dimaslanjaka <dimaslanjaka@gmail.com>
2021-12-13 01:03:34 +0700

    clean cache

---
+++ packages/electron-browser/src/translator/index.ts
@@ -0,0 +1,31 @@
+//https://translate.google.com/translate?sl=auto&tl=en&u=https%3A%2F%2Fgamewith.net%2Fgenshin-impact%2Farticle%2Fshow%2F24530
+
+function createWindow(){
+  let mainWindow = new BrowserWindow({
+    autoHideMenuBar: true,
+    width: 640,
+    height: 480,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      contextIsolation: false,
+      spellcheck: true,
+      webviewTag: true,
+      webSecurity: false,
+      preload: path.join(__dirname, "preload.js"),
+    },
+    icon: options.icon,
+    show: false,
+    center: true,
+    frame: false,
+  });
+  mainWindow.on("close", () => {
+    mainWindow.webContents.send("stop-server");
+  });
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+  return mainWindow
+}
+
+const win = createWindow()
