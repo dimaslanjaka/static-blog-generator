07c0374619dd036d93d796fd0464698c31921dd6
dimaslanjaka <dimaslanjaka@gmail.com>
2021-12-19 06:04:20 +0700

    update

--- packages/google-news-sitemap/src/index.ts
+++ packages/google-news-sitemap/src/index.ts
@@ -6,6 +6,9 @@ export const root = {
   urlset: {
     "@xmlns": "http://www.sitemaps.org/schemas/sitemap/0.9",
     "@xmlns:news": "http://www.google.com/schemas/sitemap-news/0.9",
+    "@xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+    "@xsi:schemaLocation":
+      "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-news/0.9 http://www.google.com/schemas/sitemap-news/0.9/sitemap-news.xsd",
     url: [],
   },
 };
