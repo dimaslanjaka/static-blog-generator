7bbe2c700ca311b886ac4a3f4b21b27f687cf42c
dimaslanjaka <dimaslanjaka@gmail.com>
2021-12-20 15:41:52 +0700

    Update

--- packages/nwjs/src/js/init.js
+++
@@ -1,9 +0,0 @@
-const os = require("os");
-
-(function ($) {
-  $(function () {
-    $(".sidenav").sidenav();
-  }); // end of document ready
-
-  document.querySelector("#os").innerHTML = os.platform().toUpperCase();
-})(jQuery); // end of jQuery name space
