022dc036703ec1af1699637f4f6772807e3204be
dimaslanjaka <dimaslanjaka@gmail.com>
2022-02-18 14:37:45 +0700

    fix custom session name

--- source/backend/src/session.php
+++ source/backend/src/session.php
@@ -2,7 +2,7 @@
 $folder = __DIR__ . '/../tmp';
 if (!file_exists($folder)) mkdir($folder, 0777, true);
 $timeout = 3600;
-$name = uniqid();
+//$name = uniqid();
 
 session_save_path($folder);
 session_set_cookie_params($timeout);
@@ -12,8 +12,8 @@ ini_set('session.cookie_lifetime', $timeout);
 ini_set('session.gc_probability', 100);
 ini_set('session.gc_divisor', 100);
 
-session_id($name);
+//session_id($name);
 ini_set('session.use_strict_mode', 0);
 
-session_name($name);
+//session_name($name);
 ini_set('session.use_strict_mode', 1);
