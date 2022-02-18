89f859dac7e6184ed19ea815875b0d6e0864bed8
dimaslanjaka <dimaslanjaka@gmail.com>
2022-02-18 11:23:36 +0700

    update

--- src-posts/Chimeraland/Pets/script.js
+++ src-posts/Chimeraland/Pets/script.js
@@ -2,16 +2,16 @@ const td = document.querySelectorAll("table#pet-tree");
 Array.from(td).forEach((el) => {
   let text = el.innerHTML;
   text = text.replace(
-    /\sfullness/gmi,
-    ' <img src="Recipes/fullness.jpg" class="img-inline-text" title="fullness" />'
+    /HP\s/gmi,
+    ' <img src="Pets/hp.webp" class="img-inline-text" title="Health Points" />'
   );
   text = text.replace(
     /ATK\s/gmi,
-    '<img src="../Recipes/attack.png" class="img-inline-text atk" title="attack" /> '
+    '<img src="Recipes/attack.png" class="img-inline-text atk" title="attack" /> '
   );
   text = text.replace(
     /DEF\s/gmi,
-    '<img src="../Recipes/defense.png" class="img-inline-text def" title="defense" /> '
+    '<img src="Recipes/defense.png" class="img-inline-text def" title="defense" /> '
   );
   el.innerHTML = text;
 });
