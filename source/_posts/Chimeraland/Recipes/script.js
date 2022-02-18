0655790343b2fdb712b1756d78c678a9afab155d
dimaslanjaka <dimaslanjaka@gmail.com>
2022-02-18 09:19:09 +0700

    update

--- source/_posts/Chimeraland/Recipes/script.js
+++ source/_posts/Chimeraland/Recipes/script.js
@@ -2,12 +2,16 @@ const td = document.querySelectorAll("article table#recipes td");
 Array.from(td).forEach((el) => {
   let text = el.innerHTML;
   text = text.replace(
-    "fullness",
-    '<img src="Recipes/fullness.jpg" class="img-inline-text" title="fullness" />'
+    /\sfullness/gmi,
+    ' <img src="Recipes/fullness.jpg" class="img-inline-text" title="fullness" />'
   );
   text = text.replace(
-    "atk",
-    '<img src="Recipes/attack.png" class="img-inline-text atk" title="attack" />'
+    /\satk/gmi,
+    ' <img src="Recipes/attack.png" class="img-inline-text atk" title="attack" />'
+  );
+  text = text.replace(
+    /\sdef/gmi,
+    ' <img src="Recipes/attack.png" class="img-inline-text atk" title="attack" />'
   );
   el.innerHTML = text;
 });
