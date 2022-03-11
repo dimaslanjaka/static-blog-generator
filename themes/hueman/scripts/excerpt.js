0e7cf228c70c6e467cc0c70271cef384a8f0332a
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-03-11 14:03:15 +0700

    Update excerpt.js

--- themes/hueman/scripts/excerpt.js
+++ themes/hueman/scripts/excerpt.js
@@ -7,21 +7,21 @@
 function get_excerpt(post) {
   var excerpt;
   if (post.excerpt) {
-    excerpt = post.excerpt.replace(/\<[^\>]+\>/g, '');
-  } else {
+    excerpt = post.excerpt.replace(/\<[^\>]+\>/g, "");
+  } else if (post.content) {
     excerpt = getOnlyText(post.content).substring(0, 200);
+  } else {
+    return getOnlyText(post);
   }
   return excerpt;
 }
 
-function getOnlyText(str){
-  const scriptgx = /<script(?:(?!\/\/)(?!\/\*)[^'"]|"(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*'|\/\/.*(?:\n)|\/\*(?:(?:.|\s))*?\*\/)*?<\/script>/gmi;
+function getOnlyText(text) {
+  let str = text;
+  const scriptgx = /<script(?:(?!\/\/)(?!\/\*)[^'"]|"(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*'|\/\/.*(?:\n)|\/\*(?:(?:.|\s))*?\*\/)*?<\/script>/gim;
   const defaultgx = /\<[^\>]+\>/gm; // default only g
-  const stylegx = /<style(?:(?!\/\/)(?!\/\*)[^'"]|"(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*'|\/\/.*(?:\n)|\/\*(?:(?:.|\s))*?\*\/)*?<\/style>/gmi;
-  str = str.replace(scriptgx, '');
-  str = str.replace(stylegx, '');
-  str = str.replace(defaultgx, '');
-  return str;
+  const stylegx = /<style(?:(?!\/\/)(?!\/\*)[^'"]|"(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*'|\/\/.*(?:\n)|\/\*(?:(?:.|\s))*?\*\/)*?<\/style>/gim;
+  return str.replace(scriptgx, "").replace(stylegx, "").replace(defaultgx, "");
 }
 
-hexo.extend.helper.register('excerpt', get_excerpt);
+hexo.extend.helper.register("excerpt", get_excerpt);
