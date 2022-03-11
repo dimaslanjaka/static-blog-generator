4b94f3e6c74b9b95ce85f7299d6231bf39658c0e
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-03-11 14:13:06 +0700

    Update excerpt.js

--- themes/hueman/scripts/excerpt.js
+++ themes/hueman/scripts/excerpt.js
@@ -1,21 +1,3 @@
-/**
- * Excerpt Helper
- * @description Get the excerpt from a post
- * @example
- *     <%- excerpt(post) %>
- */
-function get_excerpt(post) {
-  var excerpt;
-  if (post.excerpt) {
-    excerpt = post.excerpt.replace(/\<[^\>]+\>/g, "");
-  } else if (post.content) {
-    excerpt = getOnlyText(post.content).substring(0, 200);
-  } else {
-    return getOnlyText(post);
-  }
-  return excerpt;
-}
-
 function getOnlyText(text) {
   let str = text;
   const scriptgx = /<script(?:(?!\/\/)(?!\/\*)[^'"]|"(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*'|\/\/.*(?:\n)|\/\*(?:(?:.|\s))*?\*\/)*?<\/script>/gim;
@@ -24,4 +6,20 @@ function getOnlyText(text) {
   return str.replace(scriptgx, "").replace(stylegx, "").replace(defaultgx, "");
 }
 
+function get_excerpt(post) {
+  if (post.excerpt) {
+    return post.excerpt.replace(/\<[^\>]+\>/g, "");
+  } else if (post.content) {
+    return getOnlyText(post.content).substring(0, 200);
+  } else {
+    return getOnlyText(post);
+  } 
+}
+
+/**
+ * Excerpt Helper
+ * @description Get the excerpt from a post
+ * @example
+ *     <%- excerpt(post) %>
+ */
 hexo.extend.helper.register("excerpt", get_excerpt);
