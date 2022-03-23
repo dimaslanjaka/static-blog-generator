2e5c4ec045e8193744346f20ba745da9449ac09f
dimaslanjaka <dimaslanjaka@gmail.com>
2022-03-23 09:29:02 +0700

    dont run on github workflow

--- src/gulp/fix/external-img.ts
+++ src/gulp/fix/external-img.ts
@@ -1,4 +1,6 @@
+import { curly } from "node-libcurl";
 import { parsePostReturn } from "../../markdown/transformPosts";
+import crypto from "crypto";
 
 interface ImgLib {
   /**
@@ -19,10 +21,12 @@ const libraries: ImgLib = {};
 
 /**
  * Download External Images To Local
- * * Store database on `${workspaceFolder}/tmp/img.json`
+ * * Store database on `${workspaceFolder}/source/_data/external-images.json`
  * @param parse parsed
  */
 export default function downloadImg(parse: parsePostReturn) {
+  // cancel github workflow
+  if (typeof process.env.GITFLOW !== "undefined") return;
   if (parse.metadata) {
     const images = [];
     // extract photos from metadata
@@ -32,6 +36,31 @@ export default function downloadImg(parse: parsePostReturn) {
       meta.photos.forEach((v) => images.push(v));
     }
     if (meta.thumbnail) meta.photos.push(meta.thumbnail);
+    if (parse.body) {
+      const regex = /\<img\s.*?\s?src\s*=\s*['|"]?([^\s'"]+).*?\>/gim;
+      const str = parse.body;
+      let m: RegExpExecArray;
+
+      while ((m = regex.exec(str)) !== null) {
+        // This is necessary to avoid infinite loops with zero-width matches
+        if (m.index === regex.lastIndex) {
+          regex.lastIndex++;
+        }
+        if (m.length > 1) {
+          const src = m[1].trim();
+          const key = crypto.createHash("md5").update(src).digest("hex");
+          if (typeof libraries[key] == "undefined") {
+            curly
+              .get(src)
+              .then((res) => {
+                console.log(res.statusCode);
+              })
+              .catch((err) => {
+                console.log(err.message);
+              });
+          }
+        }
+      }
+    }
   }
-  return parse;
 }
