e7d9a915a74a738604ed1e3e67cf1bc817e3a4a3
dimaslanjaka <dimaslanjaka@gmail.com>
2022-03-23 20:06:42 +0700

    +verification

--- src/gulp/fix/external-img.ts
+++ src/gulp/fix/external-img.ts
@@ -160,7 +160,7 @@ export default async function downloadImg(parse: parsePostReturn) {
         return bluebird
           .all(processed)
           .each((data) => {
-            if (data.fullpath) {
+            if (data.fullpath && !data.err) {
               if (!data.err) {
                 if (parse.metadata.cover.trim() === data.url) {
                   parse.metadata.cover = data.fullpath;
