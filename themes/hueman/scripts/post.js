4d9a64785a3b67365f9851b771dcb0b60b5f1310
dimaslanjaka <dimaslanjaka@gmail.com>
2022-03-26 13:44:03 +0700

    [test][order] posts descending

--- themes/hueman/scripts/post.js
+++ themes/hueman/scripts/post.js
@@ -11,10 +11,24 @@ hexo.extend.helper.register('myParseDate', myParseDate);
 
 /**
  * List post by updated date
- * @param {object} posts
+ * @param {Array<object>} posts
  * @returns
  */
 function list_post_by_updated(posts) {
+  console.log(process.env.NODE_OPTIONS);
+  if (typeof process.env.NODE_OPTIONS == 'string' && process.env.NODE_OPTIONS.includes('max_old_space_size')) {
+    return posts.sort(function (a, b) {
+      if (a.updated && b.updated) {
+        return myParseDate(b.updated) - myParseDate(a.updated);
+      } else if (a.hasOwnProperty('updated')) {
+        return -1;
+      } else if (b.hasOwnProperty('updated')) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  }
   return posts;
 }
 
