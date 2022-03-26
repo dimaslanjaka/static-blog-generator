1b204124fd3c0b40d93460c0f5e9b933b631b154
dimaslanjaka <dimaslanjaka@gmail.com>
2022-03-26 13:14:18 +0700

    Test posts sort by updated

--- themes/hueman/scripts/post.js
+++ themes/hueman/scripts/post.js
@@ -1,10 +1,20 @@
-const console = require('hexo-log')({
+/*const console = require('hexo-log')({
   debug: false,
   silent: false,
-});
+});*/
 
+const myParseDate = (date_string) => {
+  let [y, M, d, h, m, s] = date_string.split(/[- :T]/);
+  return new Date(y, parseInt(M) - 1, d, h, parseInt(m), s.replace('Z', ''));
+};
+hexo.extend.helper.register('myParseDate', myParseDate);
+
+/**
+ * List post by updated date
+ * @param {object} posts
+ * @returns
+ */
 function list_post_by_updated(posts) {
-  console.log(Array.isArray(posts.data));
   return posts;
 }
 
