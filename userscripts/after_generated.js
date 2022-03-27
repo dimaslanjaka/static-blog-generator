cafb2ab5fc222aea67f3116f29c18d762a3423ec
dimaslanjaka <dimaslanjaka@gmail.com>
2022-03-28 01:06:10 +0700

    +validate

--- userscripts/after_generated.js
+++ userscripts/after_generated.js
@@ -2,6 +2,7 @@
 const fs = require("fs");
 const path = require("path");
 const _ = require("lodash");
+require('js-prototypes');
 const root = path.join(__dirname, "/../");
 const docs = path.join(root, "docs");
 
@@ -15,10 +16,10 @@ if (typeof "".replaceAll != "function") {
 // process docs/assets/js/quiz.js
 let quizjs = path.join(__dirname, "/../docs/assets/js/quiz.js");
 let read = fs.readFileSync(quizjs).toString();
-let regex = /#uniqid\(\)/gm;
+let regex = new RegExp('#uniqid()', 'gm');
 let uuid = _.uniqueId();
 read = read.replaceAll(regex, uuid);
 fs.writeFileSync(quizjs, read);
 
-// copy readme to docs
+// copy readme to public_dir
 fs.copyFileSync(path.join(root, "README.md"), path.join(docs, "readme.md"));
--- userscripts/validate.js
+++ userscripts/validate.js
@@ -1,23 +1,16 @@
-// validate docs
+// validate public_dir
 const fs = require("fs");
 const path = require("path");
 const root = path.join(__dirname, "/../");
 const docs = path.join(root, "docs");
 
+// if public_dir/index.html empty, throw
 const tests = [path.join(docs, "index.html"), path.join(docs, "The Legend Of Neverland/Quiz.html")];
 tests.forEach(function (test) {
   if (fs.existsSync(test)) {
     const sizes_byte = fs.statSync(test).size;
-    if (sizes_byte === 0) exitMsg(test, "File size 0");
+    if (sizes_byte === 0) throw `File size ${test} 0`;
     const content_sizes = fs.readFileSync(test).toString().trim().length;
-    if (content_sizes === 0) exitMsg("File content empty");
+    if (content_sizes === 0) throw `File content empty ${test}`;
   }
 });
-
-// eslint-disable-next-line no-unused-vars
-function exitMsg(...str) {
-  for (let i = 0, j = arguments.length; i < j; i++) {
-    console.log(arguments[i] + " ");
-  }
-  throw "";
-}
