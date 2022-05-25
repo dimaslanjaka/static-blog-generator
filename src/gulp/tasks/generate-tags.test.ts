cc05262a39fc1e7c0b4aade386843cbf0afae68e
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-26 04:57:57 +0700

    prepare fix tags generator

--- src/gulp/tasks/generate-tags.test.ts
+++ src/gulp/tasks/generate-tags.test.ts
@@ -2,5 +2,7 @@ import { write } from '../../node/filemanager';
 import { tmp } from '../../types/_config';
 import generateTags from './generate-tags';
 
-const test1 = generateTags('Blogging', 4);
-write(tmp('tags/Blogging-4.html'), test1);
+(async () => {
+  const test1 = await generateTags('Blogging', 4);
+  if (test1) write(tmp('tags/Blogging-4.html'), test1);
+})();
