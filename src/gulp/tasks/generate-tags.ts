dacef5c08bdbb5ebeeaf8a62563724c75201ad05
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-26 05:58:45 +0700

    fix tags generator

--- src/gulp/tasks/generate-tags.ts
+++ src/gulp/tasks/generate-tags.ts
@@ -1,9 +1,22 @@
+import { writeFileSync } from 'fs';
 import gulp from 'gulp';
-import { Nullable } from 'safelinkify/dist/resolveQueryUrl';
+import { join } from 'upath';
+import { excerpt } from '../../ejs/helper/excerpt';
+import { thumbnail } from '../../ejs/helper/thumbnail';
 import { pcache } from '../../node/cache';
 import CachePost from '../../node/cache-post';
+import color from '../../node/color';
 import scheduler from '../../node/scheduler';
+import modifyPost from '../../parser/post/modifyPost';
 import { postMap } from '../../parser/post/parsePost';
+import postChunksIterator from '../../parser/post/postChunksIterator';
+import {
+  array_wrap,
+  post_chunks,
+  simplifyDump
+} from '../../parser/post/postMapper';
+import config, { cwd, tmp } from '../../types/_config';
+import { renderer } from './generate-posts';
 
 //const cacheTags = new CacheFile('postTags');
 const cacheTags = pcache('tags');
@@ -15,25 +28,33 @@ const cacheTags = pcache('tags');
 export default async function generateTags(
   labelname?: string | null,
   pagenum?: number
-): Promise<Nullable<string>> {
+) {
   const tags = cacheTags.keysSync();
   for (let indexTag = 0; indexTag < tags.length; indexTag++) {
     const tagName = tags[indexTag];
-    const tag_posts = cacheTags.getSync(tagName);
-    console.log(tag_posts);
-  }
-  /*const tag_posts: { [key: string]: postMap[] } = cacheTags.getAll();
-  for (const tagname in tag_posts) {
-    if (Object.prototype.hasOwnProperty.call(tag_posts, tagname)) {
-      // specific tag label otherwise skip
-      if (labelname && tagname.toLowerCase() !== labelname.toLowerCase())
-        continue;
-      // skip non array
-      if (!tag_posts[tagname] || !Array.isArray(tag_posts[tagname])) continue;
-      const logname =
-        color['Desert Sand']('[generate][tag]') +
-        color['Wild Blue Yonder'](`[${tagname}]`);
-      const treeChunks = post_chunks(tag_posts[tagname]);
+    const tag_posts = cacheTags.getSync<postMap[]>(tagName);
+    const logname =
+      color['Desert Sand']('[generate][tag]') +
+      color['Wild Blue Yonder'](`[${tagName}]`);
+
+    // specific tag label otherwise skip
+    if (
+      typeof labelname == 'string' &&
+      labelname.trim().length > 0 &&
+      tagName.toLowerCase() !== labelname.toLowerCase()
+    ) {
+      console.log(tagName, '!==', labelname);
+      continue;
+    }
+    // skip non array
+    if (!tag_posts[tagName] || !Array.isArray(tag_posts[tagName])) continue;
+    console.log(logname, 'start');
+    for (
+      let indexTagPost = 0;
+      indexTagPost < tag_posts.length;
+      indexTagPost++
+    ) {
+      const treeChunks = post_chunks(tag_posts[tagName]);
       const parentChunks = treeChunks.chunk;
 
       for (
@@ -46,7 +67,7 @@ export default async function generateTags(
         const innerChunks = array_wrap(parentChunks[current_page]);
         const data = postChunksIterator(innerChunks, {
           current_page: current_page,
-          base: join(config.tag_dir, tagname),
+          base: join(config.tag_dir, tagName),
           parentChunks,
           treeChunks
         });
@@ -58,7 +79,7 @@ export default async function generateTags(
         );
         const pagemeta: postMap = {
           metadata: {
-            title: 'Tag: ' + tagname,
+            title: 'Tag: ' + tagName,
             description: excerpt(config),
             date: data.latestUpdated,
             updated: data.latestUpdated,
@@ -75,16 +96,16 @@ export default async function generateTags(
           }
         };
         if (current_page > 0) {
-          pagemeta.metadata.title = 'Tag: ' + tagname + ' Page ' + current_page;
+          pagemeta.metadata.title = 'Tag: ' + tagName + ' Page ' + current_page;
         }
         const merge_data = Object.assign(pagemeta, data);
         const pagedata = modifyPost(merge_data);
         const rendered = await renderer(<any>pagedata);
-        const f = await write(saveTo, rendered);
+        const f = writeFileSync(saveTo, rendered);
         console.log(logname, f);
 
         if (config.verbose) {
-          write(
+          writeFileSync(
             tmp('generateTags', data.perm_current + '.log'),
             simplifyDump(pagedata)
           );
@@ -92,7 +113,7 @@ export default async function generateTags(
         if (labelname) return rendered;
       }
     }
-  }*/
+  }
   return null;
 }
 
