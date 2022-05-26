5cd8b81332529a9553d2f07231734d24ca2b5ee9
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-27 04:37:10 +0700

    integrate nunjuck sample

--- src/parser/utility.ts
+++ src/parser/utility.ts
@@ -1,11 +1,25 @@
 /**
- * Function type
+ * Generic function type
+ *
+ * Usage
+ * * JSDOC `@var/@type` `{FunctionType<string>}` describe function return an `string`
+ * * JSDOC `@var/@type` `{FunctionType<string, number>}` describe function return an `string` with arguments type number
+ * * JSDOC `@var/@type` `{FunctionType<string, number | string>}` describe function return an `string` with variadic or single arguments type number or string
+ * @param T Return type
+ * @param U Argument type
+ * @author Dimas Lanjaka <dimaslanjaka@gmail.com> (https://www.webmanajemen.com)
+ * @license MIT
  */
-export interface FunctionType {
-  (): any;
-  (arg0: any): any;
-  (...arg0: any[]): any;
-}
+export type FunctionType<T = any, U = any> = (...arg0: U[]) => T;
+const _fn: FunctionType<string, number> = (n) => {
+  return ['x'][n];
+};
+const _fns: FunctionType<string, number | string> = (n, s) => {
+  return ['x', s][n];
+};
+const _fa: FunctionType<string, number | string> = (n, ...s) => {
+  return String([String(n), String(s)]);
+};
 
 /**
  * transform any variable type to string
@@ -16,7 +30,7 @@ export interface FunctionType {
  * const funcName = varToString({ func });
  * console.log(funcName); // func
  */
-export const varToString = (varObj: { func?: FunctionType }) =>
+export const varToString = (varObj: { func?: FunctionType<string> }) =>
   Object.keys(varObj)[0];
 
 /**
@@ -24,6 +38,6 @@ export const varToString = (varObj: { func?: FunctionType }) =>
  * @param func
  * @returns
  */
-export const getFuncName = (func: FunctionType) => {
+export const getFuncName = (func: FunctionType<string>) => {
   return func.name || varToString({ func });
 };
