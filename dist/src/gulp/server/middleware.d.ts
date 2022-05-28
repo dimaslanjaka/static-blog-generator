7b0036c29e06f011164e05e9fb17b02fe0a08234
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-28 18:40:26 +0700

    build Sat May 28 2022 18:40:26 GMT+0700 (Western Indonesia Time)

--- dist/src/gulp/server/middleware.d.ts
+++ dist/src/gulp/server/middleware.d.ts
@@ -1,5 +1,8 @@
+import Bluebird from 'bluebird';
+import { TaskFunction } from 'gulp';
 import '../tasks/generate';
 import './gen-middleware';
+export declare const middlewareCopyAssets: (...fn: TaskFunction[] | string[]) => Bluebird<unknown>;
 declare const ServerMiddleWare: import('browser-sync').Options['middleware'];
 export default ServerMiddleWare;
 export { ServerMiddleWare };
