992bb079cbc25c596e8a8b0052d6bf14ae073005
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-28 19:01:12 +0700

    fix config

--- src/gulp/tasks/dump.ts
+++ src/gulp/tasks/dump.ts
@@ -1,11 +1,18 @@
 import { writeFile } from 'fs';
 import gulp from 'gulp';
+import { DynamicObject } from '../../types';
 import config, { theme_config, tmp } from '../../types/_config';
 
 gulp.task('dump', async () => {
-  writeFile(tmp('config.json'), JSON.stringify(config, null, 2), (err) => {
-    if (!err) console.log('project config dump on', tmp('config.json'));
-  });
+  const sortedObject = (data: DynamicObject) =>
+    Object.fromEntries(Object.entries(data).sort());
+  writeFile(
+    tmp('config.json'),
+    JSON.stringify(sortedObject(config), null, 2),
+    (err) => {
+      if (!err) console.log('project config dump on', tmp('config.json'));
+    }
+  );
   writeFile(
     tmp('theme_config.json'),
     JSON.stringify(theme_config, null, 2),
