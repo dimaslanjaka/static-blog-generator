d18c1ccbab18680688f340e14d6d62e7a674f5dc
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-28 09:24:52 +0700

    fix coverage

--- src/translator/index.ts
+++ src/translator/index.ts
@@ -6,10 +6,10 @@
 // GET https://9f9f7cf85cf1-ngrok-io.translate.goog/2020/6/10/update-genshin-impact-1.6.0-1.6.1.html?_x_tr_sl=id&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=ajax
 // GET https://translate.google.com/translate_un?sl=id&tl=en&u=https://9f9f7cf85cf1.ngrok.io/2020/6/10/update-genshin-impact-1.6.0-1.6.1.html&usg=ALkJrhg-dpAhmINQHidHIs0byhWyENzuSA
 
-import { Curl, HeaderInfo } from 'node-libcurl';
-import path from 'path';
 import fs from 'fs';
 import jsdom from 'jsdom';
+import { Curl, HeaderInfo } from 'node-libcurl';
+import path from 'path';
 const { JSDOM } = jsdom;
 const cookieJarFile = path.join(__dirname, '/../../build/cookiejar.txt');
 
@@ -135,7 +135,7 @@ class Translator {
     dom.window.document.getElementById('gt-nvframe').remove();
     const head = dom.window.document.head;
     const base = head.getElementsByTagName('base');
-    Array.from(base).map((basehtml: HTMLBaseElement) => {
+    Array.from(base).map((basehtml /*: HTMLBaseElement*/) => {
       basehtml.remove();
     });
     return dom.serialize();
