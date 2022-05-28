d49b588290e6f3142dbef69af4b8ab94727a8759
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-28 17:06:21 +0700

    fix folder hashes

--- src/gulp/tasks/generate-sitemap.ts
+++ src/gulp/tasks/generate-sitemap.ts
@@ -1,17 +1,17 @@
-import Sitemap from '../../node/cache-sitemap';
-import GoogleNewsSitemap, { ClassItemType } from 'google-news-sitemap/src';
 import Bluebird from 'bluebird';
-import { cwd, join, write } from '../../node/filemanager';
-import config, { root } from '../../types/_config';
-import gulp from 'gulp';
-import { TaskCallback } from 'undertaker';
 import chalk from 'chalk';
+import GoogleNewsSitemap, { ClassItemType } from 'google-news-sitemap';
+import gulp from 'gulp';
 import 'js-prototypes';
 import moment from 'moment';
-import { renderer } from './generate-posts';
-import './sitemap';
+import { TaskCallback } from 'undertaker';
 import modifyPost from '../../markdown/transformPosts/modifyPost';
 import { postMap } from '../../markdown/transformPosts/parsePost';
+import Sitemap from '../../node/cache-sitemap';
+import { cwd, join, write } from '../../node/filemanager';
+import config, { root } from '../../types/_config';
+import { renderer } from './generate-posts';
+import './sitemap';
 
 const logname = chalk.cyanBright('[generate][sitemap]');
 const pages = new Sitemap();
