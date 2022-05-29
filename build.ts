c71d99b6f4a36297f13356df40d7dfef9fe1d589
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-29 19:41:59 +0700

    update

--- build.ts
+++ build.ts
@@ -2,11 +2,15 @@ import { spawn } from 'child_process';
 import { randomUUID } from 'crypto';
 import fse, { writeFile } from 'fs-extra';
 import { join, toUnix } from 'upath';
+import pkg from './package.json';
 import { getLatestCommitHash, git } from './src/bin/git';
+import { json_encode } from './src/node/JSON';
 
 if (!process.env['GITHUB_WORKFLOW']) {
   const uuid = randomUUID();
+  pkg.version = pkg.version + '-beta-' + uuid;
   writeFile(join(__dirname, '.guid'), uuid);
+  writeFile(join(__dirname, 'package.json'), json_encode(pkg, 2));
   // commit uuid
   git(null, 'add', '.guid').then(() => {
     git(null, 'commit', '-m', `update cache id ${uuid}`).then(() => {
