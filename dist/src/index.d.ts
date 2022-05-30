0ae6af0753c1d9b5cefde3befb38b04e22748211
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-31 05:24:00 +0700

    [undefined] build Tue May 31 2022 05:24:00 GMT+0700 (Western Indonesia Time)

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,2 +1,7 @@
+import { clean_db, clean_posts, clean_public, clean_tmp } from './gulp/tasks/clean';
+import { copyPosts } from './gulp/tasks/copy';
+import { copyAssets } from './gulp/tasks/copy/assets';
+import { gulpInlineStyle } from './gulp/tasks/copy/remove-inline-style';
 import './gulp/tasks/deploy';
 import './gulp/tasks/generate';
+export { copyPosts, copyAssets, gulpInlineStyle, clean_db, clean_posts, clean_public, clean_tmp };
