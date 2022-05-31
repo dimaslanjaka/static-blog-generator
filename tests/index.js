6a3763aa0ea5e05d0cec057e7641b610a916e0a1
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-31 11:49:57 +0700

    fix build

--- tests/index.js
+++ tests/index.js
@@ -2,4 +2,10 @@ const { test } = require('./test');
 const sbg = require('../dist/src');
 
 console.log('test using local dependencies');
-test(sbg);
+test(sbg, 'clean');
+
+console.log('clean');
+sbg.clean_db(null);
+sbg.clean_posts(null);
+sbg.clean_tmp(null);
+sbg.clean_public(null);
