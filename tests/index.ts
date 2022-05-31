90faab56042294942f7e603ea58666430988560b
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-31 14:44:25 +0700

    update test

--- tests/index.ts
+++ tests/index.ts
@@ -4,6 +4,6 @@ process.env['NODE_ENV'] = 'development';
 import sbg from '../src';
 
 console.log('test using local dependencies');
-sbg.clean_posts();
+//sbg.clean_posts();
 //sbg.copyAssets();
 sbg.copyPosts();
