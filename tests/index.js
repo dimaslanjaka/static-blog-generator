7eb50639ab2a9f9bc4162029ba3b02745f1f3d2a
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-31 08:23:27 +0700

    update test

--- tests/index.js
+++ tests/index.js
@@ -1,12 +1,25 @@
 process.cwd = () => __dirname;
 
-//const sbg = require('../dist/src');
-const sbg = require('static-blog-generator');
+const sbg = require('../dist/src');
+const sbg2 = require('static-blog-generator');
 
-// clean
-sbg.clean_db(null);
-sbg.clean_posts(null);
+console.log('test using local dependencies');
+test(sbg);
+console.log('test using remote dependencies');
+test(sbg2);
 
-// copy posts
-//sbg.copyPosts();
-//sbg.copyAssets();
+/**
+ * test runner
+ * @param {typeof sbg} sbg
+ */
+function test(sbg) {
+  // clean
+  sbg.clean_db(null);
+  sbg.clean_posts(null);
+  sbg.clean_tmp(null);
+  sbg.clean_public(null);
+
+  // copy posts
+  //sbg.copyPosts();
+  //sbg.copyAssets();
+}
