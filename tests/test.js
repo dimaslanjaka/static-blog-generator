79bb19d302a9b6f52f9294bfe1c4e98c2fc89336
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-31 11:30:23 +0700

    test

--- tests/test.js
+++ tests/test.js
@@ -4,15 +4,18 @@ process.cwd = () => __dirname;
  * test runner
  * @param {import('../dist/src')} sbg
  */
-function test(sbg) {
+function test(sbg, type) {
   // clean
-  sbg.clean_db(null);
-  sbg.clean_posts(null);
-  sbg.clean_tmp(null);
-  sbg.clean_public(null);
+  if (type === 'clean') {
+    sbg.clean_db(null);
+    sbg.clean_posts(null);
+    sbg.clean_tmp(null);
+    sbg.clean_public(null);
+  }
 
   // copy posts
-  //sbg.copyPosts();
-  //sbg.copyAssets();
+  sbg.copyPosts(null);
+  sbg.copyAssets(null);
 }
+
 module.exports = { test };
