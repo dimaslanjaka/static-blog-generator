abe952bc7dddf82e40fdab761c86da45403297e3
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-31 13:48:11 +0700

    update test

--- tests/index.js
+++ tests/index.js
@@ -1,7 +1,6 @@
 process.cwd = () => __dirname;
 
-const GulpClient = require('gulp');
 const sbg = require('../dist/src');
 
 console.log('test using local dependencies');
-GulpClient.series(sbg.copyAssets)(null);
+sbg.copyAssets();
