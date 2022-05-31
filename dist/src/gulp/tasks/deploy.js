61493f2d04a75675416751f8a17e2b6348a38ef8
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-31 10:52:27 +0700

    build 6f20737617 Tue May 31 2022 10:52:27 GMT+0700 (Western Indonesia Time)

--- dist/src/gulp/tasks/deploy.js
+++ dist/src/gulp/tasks/deploy.js
@@ -8,5 +8,7 @@ var _config_1 = __importDefault(require("../../types/_config"));
 require("./deploy/firebase");
 var git_1 = require("./deploy/git");
 gulp_1.default.task('deploy-git', git_1.deployerGit);
-gulp_1.default.task('deploy', gulp_1.default.series('deploy-' + _config_1.default.deploy.type));
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZGVwbG95LmpzIiwic291cmNlUm9vdCI6Ii4vc3JjLyIsInNvdXJjZXMiOlsic3JjL2d1bHAvdGFza3MvZGVwbG95LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsOENBQXdCO0FBQ3hCLGdFQUF5QztBQUN6Qyw2QkFBMkI7QUFDM0Isb0NBQTJDO0FBRTNDLGNBQUksQ0FBQyxJQUFJLENBQUMsWUFBWSxFQUFFLGlCQUFXLENBQUMsQ0FBQztBQUNyQyxjQUFJLENBQUMsSUFBSSxDQUFDLFFBQVEsRUFBRSxjQUFJLENBQUMsTUFBTSxDQUFDLFNBQVMsR0FBRyxpQkFBTSxDQUFDLE1BQU0sQ0FBQyxJQUFJLENBQUMsQ0FBQyxDQUFDIn0=
+if ('deploy' in _config_1.default && 'type' in _config_1.default.deploy) {
+    gulp_1.default.task('deploy', gulp_1.default.series('deploy-' + _config_1.default.deploy.type));
+}
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZGVwbG95LmpzIiwic291cmNlUm9vdCI6Ii4vc3JjLyIsInNvdXJjZXMiOlsic3JjL2d1bHAvdGFza3MvZGVwbG95LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsOENBQXdCO0FBQ3hCLGdFQUF5QztBQUN6Qyw2QkFBMkI7QUFDM0Isb0NBQTJDO0FBRTNDLGNBQUksQ0FBQyxJQUFJLENBQUMsWUFBWSxFQUFFLGlCQUFXLENBQUMsQ0FBQztBQUNyQyxJQUFJLFFBQVEsSUFBSSxpQkFBTSxJQUFJLE1BQU0sSUFBSSxpQkFBTSxDQUFDLE1BQU0sRUFBRTtJQUNqRCxjQUFJLENBQUMsSUFBSSxDQUFDLFFBQVEsRUFBRSxjQUFJLENBQUMsTUFBTSxDQUFDLFNBQVMsR0FBRyxpQkFBTSxDQUFDLE1BQU0sQ0FBQyxJQUFJLENBQUMsQ0FBQyxDQUFDO0NBQ2xFIn0=
