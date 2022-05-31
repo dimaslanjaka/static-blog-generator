cd50a21e5d1e02427b9f24276d535cbac5b44e4d
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-31 17:23:51 +0700

    build 7370baab0d 2022-05-31 17:23:34 PM GMT+07:00 WIB

--- dist/src/gulp/tasks/deploy/index.js
+++ dist/src/gulp/tasks/deploy/index.js
@@ -6,13 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var gulp_1 = __importDefault(require("gulp"));
 var _config_1 = __importDefault(require("../../../types/_config"));
 require("./firebase");
-var firebase_1 = __importDefault(require("./firebase"));
+var firebase_1 = require("./firebase");
 var git_1 = require("./git");
 gulp_1.default.task('deploy-git', git_1.deployerGit);
-gulp_1.default.task('deploy-firebase', firebase_1.default);
+gulp_1.default.task('deploy-firebase', firebase_1.deployFirebase);
 if ('deploy' in _config_1.default &&
     typeof _config_1.default.deploy == 'object' &&
     'type' in _config_1.default.deploy) {
     gulp_1.default.task('deploy', gulp_1.default.series('deploy-' + _config_1.default['deploy']['type']));
 }
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiLi9zcmMvIiwic291cmNlcyI6WyJzcmMvZ3VscC90YXNrcy9kZXBsb3kvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSw4Q0FBd0I7QUFDeEIsbUVBQTRDO0FBQzVDLHNCQUFvQjtBQUNwQix3REFBd0M7QUFDeEMsNkJBQW9DO0FBRXBDLGNBQUksQ0FBQyxJQUFJLENBQUMsWUFBWSxFQUFFLGlCQUFXLENBQUMsQ0FBQztBQUNyQyxjQUFJLENBQUMsSUFBSSxDQUFDLGlCQUFpQixFQUFFLGtCQUFjLENBQUMsQ0FBQztBQUU3QyxJQUNFLFFBQVEsSUFBSSxpQkFBTTtJQUNsQixPQUFPLGlCQUFNLENBQUMsTUFBTSxJQUFJLFFBQVE7SUFDaEMsTUFBTSxJQUFJLGlCQUFNLENBQUMsTUFBTSxFQUN2QjtJQUNBLGNBQUksQ0FBQyxJQUFJLENBQUMsUUFBUSxFQUFFLGNBQUksQ0FBQyxNQUFNLENBQUMsU0FBUyxHQUFHLGlCQUFNLENBQUMsUUFBUSxDQUFDLENBQUMsTUFBTSxDQUFDLENBQUMsQ0FBQyxDQUFDO0NBQ3hFIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiLi9zcmMvIiwic291cmNlcyI6WyJzcmMvZ3VscC90YXNrcy9kZXBsb3kvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSw4Q0FBd0I7QUFDeEIsbUVBQTRDO0FBQzVDLHNCQUFvQjtBQUNwQix1Q0FBNEM7QUFDNUMsNkJBQW9DO0FBRXBDLGNBQUksQ0FBQyxJQUFJLENBQUMsWUFBWSxFQUFFLGlCQUFXLENBQUMsQ0FBQztBQUNyQyxjQUFJLENBQUMsSUFBSSxDQUFDLGlCQUFpQixFQUFFLHlCQUFjLENBQUMsQ0FBQztBQUU3QyxJQUNFLFFBQVEsSUFBSSxpQkFBTTtJQUNsQixPQUFPLGlCQUFNLENBQUMsTUFBTSxJQUFJLFFBQVE7SUFDaEMsTUFBTSxJQUFJLGlCQUFNLENBQUMsTUFBTSxFQUN2QjtJQUNBLGNBQUksQ0FBQyxJQUFJLENBQUMsUUFBUSxFQUFFLGNBQUksQ0FBQyxNQUFNLENBQUMsU0FBUyxHQUFHLGlCQUFNLENBQUMsUUFBUSxDQUFDLENBQUMsTUFBTSxDQUFDLENBQUMsQ0FBQyxDQUFDO0NBQ3hFIn0=
