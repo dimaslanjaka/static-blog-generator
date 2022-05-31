09179e0b12bd2f12eabed32a201c92effd398f48
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-31 13:22:13 +0700

    fix test

--- tests/index.js
+++ tests/index.js
@@ -3,9 +3,6 @@ process.cwd = () => __dirname;
 const sbg = require('../dist/src');
 
 console.log('test using local dependencies');
-
-console.log('clean');
-sbg.clean_db(null);
-sbg.clean_posts(null);
-sbg.clean_tmp(null);
-sbg.clean_public(null);
+sbg.copyAssets().then((msg) => {
+  console.log('ers', msg);
+});
