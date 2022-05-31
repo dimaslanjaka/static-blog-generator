8ced267b3196a6f10981270afd7d8b1a5bcfc5c9
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-31 11:44:30 +0700

    build f129228424 Tue May 31 2022 11:44:30 GMT+0700 (Western Indonesia Time)

--- dist/src/gulp/tasks/deploy.js
+++ dist/src/gulp/tasks/deploy.js
@@ -8,7 +8,9 @@ var _config_1 = __importDefault(require("../../types/_config"));
 require("./deploy/firebase");
 var git_1 = require("./deploy/git");
 gulp_1.default.task('deploy-git', git_1.deployerGit);
-if ('deploy' in _config_1.default && 'type' in _config_1.default.deploy) {
-    gulp_1.default.task('deploy', gulp_1.default.series('deploy-' + _config_1.default.deploy.type));
+if ('deploy' in _config_1.default &&
+    typeof _config_1.default.deploy == 'object' &&
+    'type' in _config_1.default.deploy) {
+    gulp_1.default.task('deploy', gulp_1.default.series('deploy-' + _config_1.default['deploy']['type']));
 }
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZGVwbG95LmpzIiwic291cmNlUm9vdCI6Ii4vc3JjLyIsInNvdXJjZXMiOlsic3JjL2d1bHAvdGFza3MvZGVwbG95LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsOENBQXdCO0FBQ3hCLGdFQUF5QztBQUN6Qyw2QkFBMkI7QUFDM0Isb0NBQTJDO0FBRTNDLGNBQUksQ0FBQyxJQUFJLENBQUMsWUFBWSxFQUFFLGlCQUFXLENBQUMsQ0FBQztBQUNyQyxJQUFJLFFBQVEsSUFBSSxpQkFBTSxJQUFJLE1BQU0sSUFBSSxpQkFBTSxDQUFDLE1BQU0sRUFBRTtJQUNqRCxjQUFJLENBQUMsSUFBSSxDQUFDLFFBQVEsRUFBRSxjQUFJLENBQUMsTUFBTSxDQUFDLFNBQVMsR0FBRyxpQkFBTSxDQUFDLE1BQU0sQ0FBQyxJQUFJLENBQUMsQ0FBQyxDQUFDO0NBQ2xFIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZGVwbG95LmpzIiwic291cmNlUm9vdCI6Ii4vc3JjLyIsInNvdXJjZXMiOlsic3JjL2d1bHAvdGFza3MvZGVwbG95LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsOENBQXdCO0FBQ3hCLGdFQUF5QztBQUN6Qyw2QkFBMkI7QUFDM0Isb0NBQTJDO0FBRTNDLGNBQUksQ0FBQyxJQUFJLENBQUMsWUFBWSxFQUFFLGlCQUFXLENBQUMsQ0FBQztBQUNyQyxJQUNFLFFBQVEsSUFBSSxpQkFBTTtJQUNsQixPQUFPLGlCQUFNLENBQUMsTUFBTSxJQUFJLFFBQVE7SUFDaEMsTUFBTSxJQUFJLGlCQUFNLENBQUMsTUFBTSxFQUN2QjtJQUNBLGNBQUksQ0FBQyxJQUFJLENBQUMsUUFBUSxFQUFFLGNBQUksQ0FBQyxNQUFNLENBQUMsU0FBUyxHQUFHLGlCQUFNLENBQUMsUUFBUSxDQUFDLENBQUMsTUFBTSxDQUFDLENBQUMsQ0FBQyxDQUFDO0NBQ3hFIn0=
