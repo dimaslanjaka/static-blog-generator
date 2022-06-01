c8e567c57e0897de7f4f4a34a03a040483ad15d6
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-01 22:36:57 +0700

    build 885567fbdd 2022-06-01 22:36:27 PM GMT+07:00 WIB

--- dist/src/a-core.js
+++ dist/src/a-core.js
@@ -1,15 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs_extra_1 = require("fs-extra");
-var upath_1 = require("upath");
+var fs_1 = require("fs");
+var path_1 = require("path");
 // [task] generate empty config if not exists
 [
-    (0, upath_1.join)(__dirname, 'types/_config_project.json'),
-    (0, upath_1.join)(__dirname, 'types/_config_theme.json'),
-    (0, upath_1.join)(__dirname, 'types/_config_hashes.json')
+    (0, path_1.join)(__dirname, 'types/_config_project.json'),
+    (0, path_1.join)(__dirname, 'types/_config_theme.json'),
+    (0, path_1.join)(__dirname, 'types/_config_hashes.json')
 ].forEach(function (path) {
-    if (!(0, fs_extra_1.existsSync)(path)) {
-        (0, fs_extra_1.writeFileSync)(path, '{}');
+    if (!(0, fs_1.existsSync)(path)) {
+        (0, fs_1.writeFileSync)(path, '{}');
     }
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYS1jb3JlLmpzIiwic291cmNlUm9vdCI6Ii4vc3JjLyIsInNvdXJjZXMiOlsic3JjL2EtY29yZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLHFDQUFxRDtBQUNyRCwrQkFBNkI7QUFFN0IsNkNBQTZDO0FBQzdDO0lBQ0UsSUFBQSxZQUFJLEVBQUMsU0FBUyxFQUFFLDRCQUE0QixDQUFDO0lBQzdDLElBQUEsWUFBSSxFQUFDLFNBQVMsRUFBRSwwQkFBMEIsQ0FBQztJQUMzQyxJQUFBLFlBQUksRUFBQyxTQUFTLEVBQUUsMkJBQTJCLENBQUM7Q0FDN0MsQ0FBQyxPQUFPLENBQUMsVUFBQyxJQUFJO0lBQ2IsSUFBSSxDQUFDLElBQUEscUJBQVUsRUFBQyxJQUFJLENBQUMsRUFBRTtRQUNyQixJQUFBLHdCQUFhLEVBQUMsSUFBSSxFQUFFLElBQUksQ0FBQyxDQUFDO0tBQzNCO0FBQ0gsQ0FBQyxDQUFDLENBQUMifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYS1jb3JlLmpzIiwic291cmNlUm9vdCI6Ii4vc3JjLyIsInNvdXJjZXMiOlsic3JjL2EtY29yZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLHlCQUErQztBQUMvQyw2QkFBNEI7QUFFNUIsNkNBQTZDO0FBQzdDO0lBQ0UsSUFBQSxXQUFJLEVBQUMsU0FBUyxFQUFFLDRCQUE0QixDQUFDO0lBQzdDLElBQUEsV0FBSSxFQUFDLFNBQVMsRUFBRSwwQkFBMEIsQ0FBQztJQUMzQyxJQUFBLFdBQUksRUFBQyxTQUFTLEVBQUUsMkJBQTJCLENBQUM7Q0FDN0MsQ0FBQyxPQUFPLENBQUMsVUFBQyxJQUFJO0lBQ2IsSUFBSSxDQUFDLElBQUEsZUFBVSxFQUFDLElBQUksQ0FBQyxFQUFFO1FBQ3JCLElBQUEsa0JBQWEsRUFBQyxJQUFJLEVBQUUsSUFBSSxDQUFDLENBQUM7S0FDM0I7QUFDSCxDQUFDLENBQUMsQ0FBQyJ9
