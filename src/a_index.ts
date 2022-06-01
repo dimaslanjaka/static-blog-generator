0e96a9f95e650d096d1c8a72081d5761d883b1b8
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-01 22:04:45 +0700

    generate json dynamically

---
+++ src/a_index.ts
@@ -0,0 +1,16 @@
+import { existsSync, writeFileSync } from 'fs';
+import { join } from 'path';
+
+// [task] generate empty config if not exists
+[
+  join(__dirname, 'types/_config_project.json'),
+  join(__dirname, 'types/_config_theme.json'),
+  join(__dirname, 'types/_config_hashes.json')
+].forEach((path) => {
+  if (!existsSync(path)) {
+    writeFileSync(path, '{}');
+  }
+});
+
+// declare require types
+export declare function require<T>(name: string): T;
