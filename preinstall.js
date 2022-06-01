0282b644de22e4041f2008f9e141e5886820c3bf
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-01 15:01:35 +0700

    update module resolutions

---
+++ preinstall.js
@@ -0,0 +1,15 @@
+// git submodule update -i -r
+const { spawn } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+if (fs.existsSync(path.join(__dirname, '.git'))) {
+  console.log('installing from github repository');
+  spawn('git', ['submodule', '-i', '-r'], {
+    cwd: __dirname,
+    shell: true,
+    stdio: 'inherit'
+  });
+} else {
+  console.log('installing from npm packages');
+}
