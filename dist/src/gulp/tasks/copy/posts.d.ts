33b2ff91d580c26b7a533e0fa3a47ab0dc9f6260
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-03 21:48:22 +0700

    build 19212226d8 2022-06-03 21:47:55 PM GMT+07:00 WIB

--- dist/src/gulp/tasks/copy/posts.d.ts
+++ dist/src/gulp/tasks/copy/posts.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="node" />
 import { TaskCallback } from 'undertaker';
+import { parsePost } from '../../../parser/post/parsePost';
 import './assets';
 /**
  * copy posts from `src-posts` to config.source_dir {@link config.source_dir}
@@ -8,8 +9,8 @@ import './assets';
  * @param cpath custom copy, only copy post with this key
  * @returns
  */
-export declare const copyPosts: (_done?: TaskCallback, cpath?: string) => NodeJS.ReadWriteStream;
+export declare const copyPosts: (_done?: TaskCallback, cpath?: string, options?: Partial<Parameters<typeof parsePost>[2]>) => NodeJS.ReadWriteStream;
 /**
  * @see {@link copyPosts}
  */
-export declare const copy_posts: (_done?: TaskCallback, cpath?: string) => NodeJS.ReadWriteStream;
+export declare const copy_posts: (_done?: TaskCallback, cpath?: string, options?: Partial<Parameters<typeof parsePost>[2]>) => NodeJS.ReadWriteStream;
