162297de17dce8ef27b9b49773b93c677b025271
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-04 02:28:12 +0700

    merge git to node source function

--- src/node/spawner.ts
+++ src/node/spawner.ts
@@ -7,11 +7,74 @@ import {
   SpawnOptions
 } from 'child_process';
 import process from 'process';
+import { Readable } from 'stream';
 import scheduler from './scheduler';
 
 class spawner {
   static children: ChildProcessWithoutNullStreams[] = [];
   private static onExit = false;
+
+  /**
+   * promises spawn
+   * @param options
+   * @param cmd
+   * @param args
+   * @returns
+   * @example
+   * spawner.promise({}, 'git', 'log', '-n', '1').then((res)=> console.log(res))
+   */
+  static promise(
+    options: null | SpawnOptions = null,
+    cmd: string,
+    ...args: string[]
+  ) {
+    return new Promise(
+      (
+        resolve: (args: {
+          code: number;
+          stdout: string[] | Readable;
+          stderr: any;
+        }) => any,
+        reject: (args: { args: string[]; err: Error }) => any
+      ) => {
+        if (options === null)
+          options = {
+            cwd: __dirname,
+            stdio: 'inherit'
+          };
+        const stdouts: string[] = [];
+        const stderrs: string[] = [];
+        const child = spawn(cmd, args, options);
+        // use event hooks to provide a callback to execute when data are available:
+        if (child.stdout !== null && child.stdout !== undefined)
+          child.stdout.on('data', function (data) {
+            stdouts.push(data.toString().trim());
+          });
+        if (child.stderr !== null && child.stderr !== undefined)
+          child.stderr.on('data', function (data) {
+            stderrs.push(data.toString().trim());
+          });
+        child.on('close', function (code) {
+          // Should probably be 'exit', not 'close'
+          // *** Process completed
+          return resolve({
+            code: code,
+            stdout: stdouts.length > 0 ? stdouts : child.stdout,
+            stderr:
+              stderrs.length > 0
+                ? stderrs
+                : stdouts.length === 0
+                ? child.stderr
+                : null
+          });
+        });
+        child.on('error', function (err) {
+          // *** Process creation failed
+          return reject({ args: args, err: err });
+        });
+      }
+    );
+  }
 
   /**
    * Spawn Commands
