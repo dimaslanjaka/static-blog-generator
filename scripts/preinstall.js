498baf914389af77415cc6d90eca5b106f3eec4b
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-04 02:18:26 +0700

    fix root

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -3,15 +3,22 @@ const { spawn } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
-if (fs.existsSync(path.join(__dirname, '.git'))) {
+const root = fs.join(__dirname, '..');
+
+if (fs.existsSync(path.join(root, '.git'))) {
   console.log('installing from github repository');
+  spawn('git', ['pull', '--all'], {
+    cwd: root,
+    shell: true,
+    stdio: 'inherit'
+  });
   spawn('git', ['submodule', 'update', '-i', '-r'], {
-    cwd: __dirname,
+    cwd: root,
     shell: true,
     stdio: 'inherit'
   });
   spawn('git', ['submodule', 'update', '--init', '--recursive'], {
-    cwd: path.join(__dirname, 'packages/hexo-post-parser'),
+    cwd: path.join(root, 'packages/hexo-post-parser'),
     shell: true,
     stdio: 'inherit'
   });
