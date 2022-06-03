f8d1b596072576bfb61c18abafc8f69403b6f75f
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-03 15:16:51 +0700

    build 1f546a0231 2022-06-03 15:15:03 PM GMT+07:00 WIB

--- dist/src/node/cache-post.d.ts
+++ dist/src/node/cache-post.d.ts
@@ -1,11 +1,10 @@
-import { postMap } from '../parser/post/parsePost';
 import { mergedPostMap } from '../parser/post/postMapper';
 export declare type postResult = Partial<mergedPostMap>;
 export declare class CachePost {
     constructor();
     set(key: string, value: any): this;
     get<T>(key: string): T;
-    getKeys(): any[];
+    getKeys(): string[];
     getAll(): postMap[];
     /**
      * get total posts
@@ -36,5 +35,5 @@ export declare function getTotalPosts(): number;
  * @param identifier cached result
  * @returns
  */
-export declare function getRandomPosts(max?: number, identifier?: string): Partial<mergedPostMap>[];
+export declare function getRandomPosts(max?: number, identifier?: string): Partial<any>[];
 export declare const Post: typeof CachePost;
