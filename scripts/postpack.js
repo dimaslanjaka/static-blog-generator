ce2b48b195c71c77f372544483749c30e77ac3d9
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-04 02:15:22 +0700

    add release

--- scripts/postpack.js
+++ scripts/postpack.js
@@ -1,7 +1,22 @@
-const { existsSync } = require('fs');
+const { existsSync, writeFileSync, mkdirSync } = require('fs');
+const { copySync } = require('fs-extra');
 const { join } = require('path');
 const pkg = require('../package.json');
-const filename = pkg.name + '-' + pkg.version + '.tgz';
-const filepath = join(__dirname, filename);
+const { hideBin } = require('yargs/helpers');
+const yargs = require('yargs');
+const argv = yargs(hideBin(process.argv)).argv;
 
-console.log(filepath, existsSync(filepath));
+const filename = pkg.name + '-' + pkg.version + '.tgz';
+const filepath = join(process.cwd(), filename);
+
+const tmp = join(__dirname, 'tmp');
+if (!existsSync(tmp)) mkdirSync(tmp, { recursive: true });
+if (existsSync(filepath)) {
+  const isbeta = filepath.includes('-beta-');
+  let newPath = join(process.cwd(), 'release/production.tgz');
+  if (isbeta) {
+    newPath = join(process.cwd(), 'release/development.tgz');
+  }
+  copySync(filepath, newPath, { overwrite: true });
+}
+writeFileSync(join(tmp, 'postpack.log'), JSON.stringify(argv));
