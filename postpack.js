afb3d217dfe2a14f3da97c2d3fc30a2d5a9998d4
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-03 07:36:08 +0700

    migrate to yarn v2

---
+++ postpack.js
@@ -0,0 +1,7 @@
+const { existsSync } = require('fs');
+const { join } = require('path');
+const pkg = require('./package.json');
+const filename = pkg.name + '-' + pkg.version + '.tgz';
+const filepath = join(__dirname, filename);
+
+console.log(filepath, existsSync(filepath));
