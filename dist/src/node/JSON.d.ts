614805b19f5150770ce0cb4f02622cf80a2031f1
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-03 17:30:50 +0700

    refresh build

--- dist/src/node/JSON.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="node/JSON" />
-/**
- * json_encode PHP equivalent
- * * support circular refs
- * @param data
- * @returns
- */
-export declare function json_encode<T extends Record<string, unknown> | any[]>(data: T, indent?: number): string;
