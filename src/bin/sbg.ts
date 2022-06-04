01a75b2638bc398a03b02c2d857f3c28688bbe23
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-04 13:57:09 +0700

    test dynamic task runner

--- src/bin/sbg.ts
+++ src/bin/sbg.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import { TaskFunctionWrapped } from 'undertaker';
 import yargs from 'yargs';
 import gulp from '../../gulpfile';
 
@@ -8,8 +9,33 @@ const tasks = argv['_'];
 
 //console.log(getConfig().verbose, getConfig().generator.cache);
 
+const taskswrapper: TaskFunctionWrapped[] = [];
 for (let i = 0; i < tasks.length; i++) {
   const taskname = tasks[i];
   const taskfn = gulp.task(taskname);
-  console.log(typeof taskfn);
+  //console.log('typeof task', taskname, typeof taskfn);
+  if (typeof taskfn == 'function') {
+    taskswrapper.push(taskfn);
+  }
+}
+
+let keeprunning = true;
+while (keeprunning !== false) {
+  const run = taskswrapper[0](null);
+  taskswrapper.shift();
+  if (taskswrapper.length > 0) {
+    if (typeof run['then'] === 'function') {
+      run['then'](() => {
+        console.log('done');
+      });
+    } else {
+      if (typeof run['on'] === 'function') {
+        run['on']('end', function () {
+          console.log('end');
+        });
+      }
+    }
+  } else {
+    keeprunning = false;
+  }
 }
