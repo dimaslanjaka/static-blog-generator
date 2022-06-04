1058065c5268e94117dd3624eda8f99f8b601aba
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-04 13:33:18 +0700

    build d74e1d33f0 2022-06-04 13:33:00 PM GMT+07:00 WIB

---
+++ dist/src/bin/sbg.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var yargs_1 = __importDefault(require("yargs"));
+var argv = (0, yargs_1.default)(process.argv.slice(2)).argv;
+console.log(argv);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2JnLmpzIiwic291cmNlUm9vdCI6Ii4vc3JjLyIsInNvdXJjZXMiOlsic3JjL2Jpbi9zYmcudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBRUEsZ0RBQTBCO0FBRTFCLElBQU0sSUFBSSxHQUFHLElBQUEsZUFBSyxFQUFDLE9BQU8sQ0FBQyxJQUFJLENBQUMsS0FBSyxDQUFDLENBQUMsQ0FBQyxDQUFDLENBQUMsSUFBSSxDQUFDO0FBQy9DLE9BQU8sQ0FBQyxHQUFHLENBQUMsSUFBSSxDQUFDLENBQUMifQ==
