6596cbde8379f56d313c516c57ff70630fb660bf
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-04 13:39:55 +0700

    log args

--- src/bin/sbg.ts
+++ src/bin/sbg.ts
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
 import yargs from 'yargs';
+import { getConfig } from '../types/_config';
 
 const argv = yargs(process.argv.slice(2)).argv;
 const tasks = argv['_'];
 
-console.log(tasks, argv);
+console.log(getConfig().verbose, getConfig().generator.cache);
