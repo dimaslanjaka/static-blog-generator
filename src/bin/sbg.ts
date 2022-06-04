49be752ab2bb3501c776dd70122637c37922271c
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-04 13:41:44 +0700

    log task function

--- src/bin/sbg.ts
+++ src/bin/sbg.ts
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 
+import gulp from 'gulp';
 import yargs from 'yargs';
-import { getConfig } from '../types/_config';
 
 const argv = yargs(process.argv.slice(2)).argv;
 const tasks = argv['_'];
 
-console.log(getConfig().verbose, getConfig().generator.cache);
+//console.log(getConfig().verbose, getConfig().generator.cache);
+
+for (let i = 0; i < tasks.length; i++) {
+  const task = tasks[i];
+
+  const fn = gulp.series(task);
+  console.log(typeof fn);
+}
