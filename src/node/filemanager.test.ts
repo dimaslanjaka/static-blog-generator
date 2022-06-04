a2ea7d4498119ca8210bb3dd9e8931250e7e355b
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-04 07:04:39 +0700

    fix `globSrc`, +minimatch advanced filter

--- src/node/filemanager.test.ts
+++ src/node/filemanager.test.ts
@@ -1,4 +1,12 @@
 import { post_public_dir } from '../types/_config';
 import { globSrc } from './filemanager';
 
-globSrc('**/*', { ignore: 'p', cwd: post_public_dir });
+const ignore = ['codeblock.*', 'shortcodes', 'dummy'];
+
+globSrc('**/*', {
+  ignore: ['unit', 'shortcodes'],
+  cwd: post_public_dir,
+  use: 'minimatch'
+}).then((_files) => {
+  console.log(_files);
+});
