a0647be8d12caecf26633a595ffdce83ef6afd29
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-04 13:40:09 +0700

    build e8ce51a624 2022-06-04 13:39:51 PM GMT+07:00 WIB

--- dist/src/bin/sbg.js
+++ dist/src/bin/sbg.js
@@ -5,7 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var yargs_1 = __importDefault(require("yargs"));
+var _config_1 = require("../types/_config");
 var argv = (0, yargs_1.default)(process.argv.slice(2)).argv;
 var tasks = argv['_'];
-console.log(tasks, argv);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2JnLmpzIiwic291cmNlUm9vdCI6Ii4vc3JjLyIsInNvdXJjZXMiOlsic3JjL2Jpbi9zYmcudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBRUEsZ0RBQTBCO0FBRTFCLElBQU0sSUFBSSxHQUFHLElBQUEsZUFBSyxFQUFDLE9BQU8sQ0FBQyxJQUFJLENBQUMsS0FBSyxDQUFDLENBQUMsQ0FBQyxDQUFDLENBQUMsSUFBSSxDQUFDO0FBQy9DLElBQU0sS0FBSyxHQUFHLElBQUksQ0FBQyxHQUFHLENBQUMsQ0FBQztBQUV4QixPQUFPLENBQUMsR0FBRyxDQUFDLEtBQUssRUFBRSxJQUFJLENBQUMsQ0FBQyJ9
+console.log((0, _config_1.getConfig)().verbose, (0, _config_1.getConfig)().generator.cache);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2JnLmpzIiwic291cmNlUm9vdCI6Ii4vc3JjLyIsInNvdXJjZXMiOlsic3JjL2Jpbi9zYmcudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBRUEsZ0RBQTBCO0FBQzFCLDRDQUE2QztBQUU3QyxJQUFNLElBQUksR0FBRyxJQUFBLGVBQUssRUFBQyxPQUFPLENBQUMsSUFBSSxDQUFDLEtBQUssQ0FBQyxDQUFDLENBQUMsQ0FBQyxDQUFDLElBQUksQ0FBQztBQUMvQyxJQUFNLEtBQUssR0FBRyxJQUFJLENBQUMsR0FBRyxDQUFDLENBQUM7QUFFeEIsT0FBTyxDQUFDLEdBQUcsQ0FBQyxJQUFBLG1CQUFTLEdBQUUsQ0FBQyxPQUFPLEVBQUUsSUFBQSxtQkFBUyxHQUFFLENBQUMsU0FBUyxDQUFDLEtBQUssQ0FBQyxDQUFDIn0=
