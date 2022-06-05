583d8a5fbf1f017af22b77f1df9128ef4e186ad2
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-06-05 14:34:44 +0700

    Update sbg.ts

--- src/bin/sbg.ts
+++ src/bin/sbg.ts
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+// compiled location in dist/src/bin/sbg.js
+
 import { TaskFunctionWrapped } from 'undertaker';
 import yargs from 'yargs';
 import gulp from '../../gulpfile';
@@ -12,22 +14,26 @@ indexing();
 
 //console.log(getConfig().verbose, getConfig().generator.cache);
 
-const taskswrapper: TaskFunctionWrapped[] = [];
-for (let i = 0; i < tasks.length; i++) {
-  const taskname = tasks[i];
-  const taskfn = gulp.task(taskname);
-  //console.log('typeof task', taskname, typeof taskfn);
-  if (typeof taskfn == 'function') {
-    taskswrapper.push(taskfn);
-  }
-}
+gulp.series(...tasks)(null);
 
-let keeprunning = true;
-while (keeprunning !== false) {
-  if (taskswrapper.length > 0) {
-    taskswrapper[0](null);
-    taskswrapper.shift();
-  } else {
-    keeprunning = false;
+function _loop_method() {
+  const taskswrapper: TaskFunctionWrapped[] = [];
+  for (let i = 0; i < tasks.length; i++) {
+    const taskname = tasks[i];
+    const taskfn = gulp.task(taskname);
+    //console.log('typeof task', taskname, typeof taskfn);
+    if (typeof taskfn == "function") {
+      taskswrapper.push(taskfn);
+    }
+  }
+
+  let keeprunning = true;
+  while (keeprunning !== false) {
+    if (taskswrapper.length > 0) {
+      taskswrapper[0](null);
+      taskswrapper.shift();
+    } else {
+      keeprunning = false;
+    }
   }
 }
