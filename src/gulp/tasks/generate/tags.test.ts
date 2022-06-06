75f5387ffa176ac03ab58ebb5358ded26ebe4bf1
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-06 10:34:01 +0700

    fix param

--- src/gulp/tasks/generate/tags.test.ts
+++ src/gulp/tasks/generate/tags.test.ts
@@ -1,20 +1,20 @@
 import { join } from 'upath';
 import color from '../../../node/color';
 import { write } from '../../../node/filemanager';
-import { getChunkOf } from './tags';
+import { getChunkOf } from './getChunkOf';
 
 (async () => {
   const tagname = 'dummy';
   const saveFolder = join(__dirname, 'tmp/tags');
 
-  const test = await getChunkOf();
+  const test = getChunkOf('tag');
   write(join(saveFolder, 'all.json'), test);
   console.log(
     color['Vivid Violet']('test result'),
     typeof test == 'string' && Object.keys(test).length > 0
   );
 
-  const test2 = await getChunkOf(tagname);
+  const test2 = getChunkOf('tag', tagname);
   console.log(
     color['Vivid Violet']('test2 result'),
     typeof test2 == 'string' && Object.keys(test2).length > 0
