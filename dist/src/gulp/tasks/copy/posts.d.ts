65e79524e0de30e3c075df2e6f379c7984a3e3e5
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-06 11:07:42 +0700

    [d815561] build 2022-06-06 11:07:37 AM GMT+07:00 WIB

--- dist/src/gulp/tasks/copy/posts.d.ts
+++ dist/src/gulp/tasks/copy/posts.d.ts
@@ -6,11 +6,11 @@ import './assets';
  * copy posts from `src-posts` to config.source_dir {@link config.source_dir}
  * @description copy, parsing shortcodes, render html body, etc from src-posts to source_dir
  * @summary copy from src-posts to source/_posts
- * @param cpath custom copy, only copy post with this key
+ * @param customPaths custom copy, only copy post with this key
  * @returns
  */
-export declare const copyPosts: (_done?: TaskCallback, cpath?: string, options?: Partial<Parameters<typeof parsePost>[2]>) => NodeJS.ReadWriteStream;
+export declare const copyPosts: (_done?: TaskCallback, customPaths?: string | string[], options?: Partial<Parameters<typeof parsePost>[2]>) => NodeJS.ReadWriteStream;
 /**
  * @see {@link copyPosts}
  */
-export declare const copy_posts: (_done?: TaskCallback, cpath?: string, options?: Partial<Parameters<typeof parsePost>[2]>) => NodeJS.ReadWriteStream;
+export declare const copy_posts: (_done?: TaskCallback, customPaths?: string | string[], options?: Partial<Parameters<typeof parsePost>[2]>) => NodeJS.ReadWriteStream;
