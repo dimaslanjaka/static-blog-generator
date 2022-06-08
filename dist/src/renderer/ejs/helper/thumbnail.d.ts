c87ede09e4a299559c6c1f8b529d8268dab6e05e
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-08 15:11:48 +0700

    build from @https://github.com/dimaslanjaka/static-blog-generator/commit/cc8dc18

--- dist/src/renderer/ejs/helper/thumbnail.d.ts
+++
@@ -1,9 +0,0 @@
-import { postMap } from '../../../parser/post/parsePost';
-/**
- * Thumbnail Helper
- * @description Get the thumbnail url from a post
- * @param {object} post
- * @example
- *     <%- thumbnail(post) %>
- */
-export declare function thumbnail(post: postMap['metadata']): string;
