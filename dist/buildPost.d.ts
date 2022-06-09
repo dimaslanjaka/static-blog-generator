068f8a0bdb3be9e540c43a08cebe16425d0a7fc6
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-06-10 04:09:22 +0700

    build Fri Jun 10 2022 04:09:21 GMT+0700 (Western Indonesia Time)

--- dist/buildPost.d.ts
+++ dist/buildPost.d.ts
@@ -1,8 +1,8 @@
-import { postMap } from './parsePost';
+import { postMap } from './types/postMap';
 /**
  * Rebuild {@link parsePost} result to markdown post back
  * @param parsed parsed post return {@link parsePost}
  * @returns
  */
-export declare function buildPost(parsed: Partial<postMap>): any;
+export declare function buildPost(parsed: Partial<postMap>): string;
 export default buildPost;
