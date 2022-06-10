73e897655d25eecce736bc972c24c49cb6f8be05
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-10 18:24:35 +0700

    add windows bin

---
+++ bin/install-repo.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+npm un hexo-post-parser persistent-cache sbg-themes safelinkify
+npm i git+https://github.com/dimaslanjaka/persistent-cache#improve2
+npm i git+https://github.com/dimaslanjaka/safelink#master
+npm i git+https://github.com/dimaslanjaka/hexo-post-parser#master
+npm i git+https://github.com/dimaslanjaka/persistent-cache#improve2
+npm i git+https://github.com/dimaslanjaka/google-news-sitemap#master
+npm install -O https://github.com/dimaslanjaka/sbg-themes.git
