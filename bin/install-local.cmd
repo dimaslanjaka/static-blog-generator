40b2b86048cae262ca1ece77936f1c004ca2016c
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-10 18:24:35 +0700

    add windows bin

---
+++ bin/install-local.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+npm un hexo-post-parser persistent-cache sbg-themes safelinkify
+npm i file:../persistent-cache
+npm i file:../safelink
+npm i file:../hexo-post-parser
+npm i file:../persistent-cache
+npm i file:../google-news-sitemap
+npm install -O https://github.com/dimaslanjaka/sbg-themes.git
