68b86a34d9ed8586aaaf39afa9a96af507df9c39
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-10 14:56:59 +0700

    [5e22fb9] build 2022-06-10 14:56:53 PM GMT+07:00 WIB

--- dist/src/parser/yaml.d.ts
+++ dist/src/parser/yaml.d.ts
@@ -4,3 +4,9 @@
  * @returns
  */
 export declare function yamlParse<T extends Record<string, unknown>>(target: string): T;
+/**
+ * build object to yaml
+ * @param obj
+ * @returns
+ */
+export declare function yamlBuild(obj: Record<string, any>): string;
