44a01dc3b4c02001733592f70113640f49225510
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-13 06:15:55 +0700

    add interface generatorCopy

--- src/types/_config.default.ts
+++ src/types/_config.default.ts
@@ -117,8 +117,15 @@ export const default_config: Partial<SBGConfig> = {
 };
 
 export interface Generator {
+  [key: string]: any;
+  copy?: GeneratorCopy;
   cache?: boolean;
   type?: 'hexo' | 'jekyll' | 'sbg';
+}
+
+export interface GeneratorCopy {
+  [key: string]: any;
+  space?: boolean;
 }
 
 export interface SBGConfig {
