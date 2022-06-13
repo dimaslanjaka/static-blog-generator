db0a396c1f9331c576f83f7fa848cd5ef3eec00a
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-13 15:56:52 +0700

    [5c7eaf7] build 2022-06-13 15:56:46 PM GMT+07:00 WIB

--- dist/src/types/_config.default.d.ts
+++ dist/src/types/_config.default.d.ts
@@ -1,8 +1,14 @@
 import '../a-core';
 export declare const default_config: Partial<SBGConfig>;
 export interface Generator {
+    [key: string]: any;
+    copy?: GeneratorCopy;
     cache?: boolean;
     type?: 'hexo' | 'jekyll' | 'sbg';
+}
+export interface GeneratorCopy {
+    [key: string]: any;
+    space?: boolean;
 }
 export interface SBGConfig {
     [key: string]: any;
