28e0951acfd69138831080765a44a1b3eb5410db
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-13 18:37:31 +0700

    apply new method

--- src/gulp/tasks/copy/assets.test.ts
+++ src/gulp/tasks/copy/assets.test.ts
@@ -1,5 +1,13 @@
-import { copyAssets } from './assets';
-
+/*
 copyAssets(['shortcode']).then((copied) => {
   console.log(copied);
 });
+*/
+
+import { globSrc } from '../../../node/filemanager';
+import config, { post_source_dir } from '../../../types/_config';
+
+globSrc('*/**', {
+  cwd: post_source_dir,
+  ignore: config.exclude.concat(['**/*.md'])
+}).then((files) => console.log(files));
