88ab5439b16a9336874d29e4093a0080103c4cc9
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-14 14:07:14 +0700

    fixed copying post with space in path option

--- src/gulp/tasks/copy/posts.test.ts
+++ src/gulp/tasks/copy/posts.test.ts
@@ -1,4 +1,6 @@
 import { copyPosts } from './posts';
 
-//copyPosts(null, 'src-posts/Tests/unit/elements.md');
-copyPosts(null, 'src-posts/folder with space/file post with space.md');
+//copyPosts(null, 'src-posts/Tests/unit/elements.md', {cache:false});
+copyPosts(null, 'folder with space/file post with space.md', {
+  cache: false
+});
