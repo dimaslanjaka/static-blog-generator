d05bdd7e6742edc02321f2c128e3c0bc046560a9
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-14 16:09:19 +0700

    [cd3844c] build 2022-06-14 16:09:12 PM GMT+07:00 WIB

--- dist/src/node/color.d.ts
+++ dist/src/node/color.d.ts
@@ -130,7 +130,6 @@ declare const color: {
     'Wild Strawberry': chalk.Chalk;
     'Wild Watermelon': chalk.Chalk;
     Wisteria: chalk.Chalk;
-    yellow: chalk.Chalk;
     Yellow: chalk.Chalk;
     'Yellow Green': chalk.Chalk;
     'Yellow Orange': chalk.Chalk;
