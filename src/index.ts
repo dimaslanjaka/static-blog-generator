7151752b1ae7d0b6ade5880bc4de4bdf924d2f55
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-06-14 12:48:27 +0700

    export generate post id

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import './a_index';
 import { buildPost } from './buildPost';
 import { parsePost } from './parsePost';
+export { generatePostId } from './generatePostId';
 export { parsePermalink } from './parsePermalink';
 export { DeepPartial, Nullable, ParseOptions } from './parsePost';
 export { postMap } from './types/postMap';
