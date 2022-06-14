3bddf6d7db3d2793b06a1ec0ab7844da6a63a26a
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-06-14 12:45:46 +0700

    add custom identifier

--- src/generatePostId.ts
+++ src/generatePostId.ts
@@ -6,18 +6,22 @@ import { postMeta } from './types/postMeta';
  * @param meta
  * @returns
  */
-export function generatePostId(meta: postMeta) {
+export function generatePostId(meta: postMeta | string) {
   let id: string;
-  if ('title' in meta && 'webtitle' in meta) {
-    id = meta.title + meta.webtitle;
-  } else if ('description' in meta) {
-    id = meta.description;
-  } else if ('subtitle' in meta) {
-    id = meta.subtitle;
-  } else if ('excerpt' in meta) {
-    id = meta.excerpt;
-  } else if ('title' in meta) {
-    id = meta.title;
+  if (typeof meta === 'object') {
+    if ('title' in meta && 'webtitle' in meta) {
+      id = meta.title + meta.webtitle;
+    } else if ('description' in meta) {
+      id = meta.description;
+    } else if ('subtitle' in meta) {
+      id = meta.subtitle;
+    } else if ('excerpt' in meta) {
+      id = meta.excerpt;
+    } else if ('title' in meta) {
+      id = meta.title;
+    }
+  } else if (typeof meta === 'string') {
+    id = meta;
   }
 
   return uuidv4(id);
