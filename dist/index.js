87370f291e0061b89d0c95a5c6b8a5970b26408c
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-06-14 12:48:57 +0700

    build Tue Jun 14 2022 12:48:57 GMT+0700 (Western Indonesia Time)

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.obj = exports.buildPost = exports.parsePost = exports.parsePermalink = void 0;
+exports.obj = exports.buildPost = exports.parsePost = exports.parsePermalink = exports.generatePostId = void 0;
 require("./a_index");
 const buildPost_1 = require("./buildPost");
 Object.defineProperty(exports, "buildPost", { enumerable: true, get: function () { return buildPost_1.buildPost; } });
 const parsePost_1 = require("./parsePost");
 Object.defineProperty(exports, "parsePost", { enumerable: true, get: function () { return parsePost_1.parsePost; } });
+var generatePostId_1 = require("./generatePostId");
+Object.defineProperty(exports, "generatePostId", { enumerable: true, get: function () { return generatePostId_1.generatePostId; } });
 var parsePermalink_1 = require("./parsePermalink");
 Object.defineProperty(exports, "parsePermalink", { enumerable: true, get: function () { return parsePermalink_1.parsePermalink; } });
 exports.obj = { parsePost: parsePost_1.parsePost, buildPost: buildPost_1.buildPost };
