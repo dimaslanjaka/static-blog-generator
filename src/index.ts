eaa9906458bde1fcd14a0cd25d337cd59c1d6b14
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-06-14 11:56:40 +0700

    export parse permalink

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import './a_index';
 import { buildPost } from './buildPost';
 import { parsePost } from './parsePost';
-
-export const obj = { parsePost };
+export { parsePermalink } from './parsePermalink';
 export { DeepPartial, Nullable, ParseOptions } from './parsePost';
 export { postMap } from './types/postMap';
 export { postMeta } from './types/postMeta';
 export { parsePost, buildPost };
+
+export const obj = { parsePost, buildPost };
+
 export default obj;
