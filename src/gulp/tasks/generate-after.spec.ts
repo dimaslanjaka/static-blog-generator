288a56ccbb25d523abdf34a1360ae261babc3d4d
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-14 16:24:10 +0700

    add test case

--- src/gulp/tasks/generate-after.spec.ts
+++ src/gulp/tasks/generate-after.spec.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import 'mocha';
 import { filter_external_links } from './generate-after';
 
-describe('Test Safelink', async () => {
+describe('Test Safelink (filter_external_links)', async () => {
   it('check external link', () => {
     const test = filter_external_links('http://google.com');
     expect(test.href).to.be.a('string');
@@ -11,6 +11,12 @@ describe('Test Safelink', async () => {
   });
   it('check internal link', () => {
     const test = filter_external_links('http://webmanajemen.com');
+    expect(test.href).to.be.a('string');
+    expect(test.href).to.match(/(?:webmanajemen)/);
+    expect(test.internal).to.be.true;
+  });
+  it('check invalid link', () => {
+    const test = filter_external_links('http://webmanajemen.com">invalid');
     expect(test.href).to.be.a('string');
     expect(test.href).to.match(/(?:webmanajemen)/);
     expect(test.internal).to.be.true;
