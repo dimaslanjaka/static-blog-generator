c9eec6c6c4c2f4eb450828332579e01989241205
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-14 13:14:37 +0700

    fix permalink of post with spaces

--- src/gulp/tasks/copy/posts.test.ts
+++ src/gulp/tasks/copy/posts.test.ts
@@ -1,3 +1,4 @@
 import { copyPosts } from './posts';
 
-copyPosts(null, 'src-posts/Tests/unit/elements.md');
+//copyPosts(null, 'src-posts/Tests/unit/elements.md');
+copyPosts(null, 'src-posts/folder with space/file post with space.md');
