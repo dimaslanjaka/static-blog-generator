ecbf7b699922b99c77ca4ddbc9cbc4705be63be8
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-15 08:02:05 +0700

    add test

---
+++ src/renderer/ejs/EJSRenderer.test.ts
@@ -0,0 +1,18 @@
+import { join } from 'upath';
+import { inspect } from 'util';
+import { write } from '../../node/filemanager';
+import parsePost from '../../parser/post/parsePost';
+import { post_source_dir } from '../../types/_config';
+import { EJSRenderer } from './EJSRenderer';
+
+(async () => {
+  const path = join(post_source_dir, 'with-ejs-function-in-body.md');
+  const parse = await parsePost(path, null, { cache: false });
+  const data = Object.assign(
+    {},
+    { page: Object.assign({}, parse, parse.metadata) }
+  );
+  write(join(__dirname, 'tmp', 'data-test.log'), inspect(data));
+  const rendered = await EJSRenderer(parse, data);
+  console.log(rendered);
+})();
