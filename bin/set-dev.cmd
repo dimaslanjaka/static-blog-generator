b7ff23e25c988b2f4eb36fd379d741e90d56eabe
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-16 07:39:17 +0700

    code-runner set use as development by default

--- bin/set-dev.cmd
+++ bin/set-dev.cmd
@@ -1,3 +1,6 @@
-@echo off
+@ECHO OFF
 
-cross-env NODE_ENV=development %*
+:: source: https://stackoverflow.com/a/21606502/6404439
+:: cross-env: https://github.com/kentcdodds/cross-env#cross-env-vs-cross-env-shell
+
+cross-env-shell NODE_ENV=development %*
