f48adcf919f7237d8c470f2a022405475f6219c2
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-16 09:21:13 +0700

    add dump

--- src/renderer/ejs/EJSRenderer.test.ts
+++ src/renderer/ejs/EJSRenderer.test.ts
@@ -2,18 +2,27 @@ import { join } from 'upath';
 import { inspect } from 'util';
 import { write } from '../../node/filemanager';
 import parsePost from '../../parser/post/parsePost';
+import { fnWrap } from '../../parser/utility';
 import { post_source_dir } from '../../types/_config';
 import { EJSRenderer } from './EJSRenderer';
 
-(async () => {
-  const path = join(post_source_dir, 'with-ejs-function-in-body.md');
-  const parse = await parsePost(path, null, { cache: false });
-  const data = Object.assign(
-    {},
-    { page: Object.assign({}, parse, parse.metadata) }
-  );
-  await write(join(__dirname, 'tmp', 'data-test.log'), inspect(data));
-  const rendered = await EJSRenderer(parse, data);
-  const s = await write(join(__dirname, 'tmp', 'result-test.html'), rendered);
-  console.log(s);
-})();
+fnWrap(_renderSrc);
+
+async function _renderSrc() {
+  {
+    const sourcePath = join(post_source_dir, 'with-ejs-function-in-body.md');
+    const parsed = await parsePost(sourcePath, null, { cache: false });
+    await write(join(__dirname, 'tmp', 'body-test.md'), parsed.body);
+    const data = Object.assign(
+      {},
+      { page: Object.assign({}, parsed, parsed.metadata) }
+    );
+    await write(join(__dirname, 'tmp', 'data-test.log'), inspect(data));
+    const rendered = await EJSRenderer(parsed, data);
+    const savedTo = await write(
+      join(__dirname, 'tmp', 'rest-result.html'),
+      rendered
+    );
+    console.log({ sourcePath, savedTo });
+  }
+}
