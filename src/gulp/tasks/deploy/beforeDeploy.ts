a89b3e9d5f96720a49a7508de277d3be7e94808c
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-20 08:30:36 +0700

    fix deployer before push

--- src/gulp/tasks/deploy/beforeDeploy.ts
+++ src/gulp/tasks/deploy/beforeDeploy.ts
@@ -1,8 +1,22 @@
 import * as fs from 'fs';
 import * as path from 'upath';
-import { spawn } from 'child_process';
+import spawner from '../../../node/spawner';
 
-export function beforeDeploy (cwd:string) {
+/**
+ * process before push
+ * @param cwd current working directory
+ */
+export async function beforeDeploy(cwd: string) {
   const pkg = path.join(cwd, 'package.json');
-  if (fs.existsSync(pkg)) spawn('npm', ['install', '--production', '--omit=dev'], {cwd});
+  if (fs.existsSync(pkg)) {
+    return await spawner.promise(
+      {
+        cwd
+      },
+      'npm',
+      'install',
+      '--production',
+      '--omit=dev'
+    );
+  }
 }
