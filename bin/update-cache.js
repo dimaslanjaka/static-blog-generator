34332f65027bca9b50b2882484bc147072c644fa
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-06-23 06:55:29 +0700

    add catch
    Signed-off-by: Dimas Lanjaka <dimaslanjaka@gmail.com>

--- bin/update-cache.js
+++ bin/update-cache.js
@@ -3,7 +3,15 @@ const md5File = require('md5-file');
 const { join } = require('path');
 const { spawn } = require('hexo-util');
 
-const hash = md5File.sync('package-lock.json');
+const hash = md5File.sync(join(__dirname, '../', 'package-lock.json'));
 //console.log(`MD5 sum: ${hash}`);
 writeFileSync(join(__dirname, '../.guid'), hash);
-spawn('git', ['add', '.guid']).then(() => spawn('git', ['commit', '-m', 'update cache']));
+spawn('git', ['add', '.guid'])
+  .then(() =>
+    spawn('git', ['commit', '-m', 'update cache']).catch((e) => {
+      //
+    })
+  )
+  .catch((e) => {
+    //
+  });
