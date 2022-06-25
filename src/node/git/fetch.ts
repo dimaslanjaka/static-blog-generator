f8a7cee42c51d04bd9648a5915db11df7ac0f1d0
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-25 17:27:01 +0700

    add `all` param

--- src/node/git/fetch.ts
+++ src/node/git/fetch.ts
@@ -4,19 +4,35 @@ import path from 'path';
 /**
  * git fetch
  * @param gitDir path location to `.git` folder
+ * @param all git fetch --all
  * @example
  * gitFetch(path.resolve(process.cwd(), '.git'))
  */
 export default async function gitFetch(
-  gitDir = path.resolve(process.cwd(), '.git')
+  gitDir = path.resolve(process.cwd(), '.git'),
+  all = false
 ) {
   const repo = await nodegit.Repository.open(gitDir);
-  await repo.fetch('origin', {
-    callbacks: {
-      credentials: function (_url_1: any, userName: any) {
-        //console.log(url, userName);
-        return nodegit.Cred.sshKeyFromAgent(userName);
+
+  if (all) {
+    await repo.fetchAll({
+      callbacks: {
+        credentials: function (_url: any, userName: string) {
+          return nodegit.Cred.sshKeyFromAgent(userName);
+        },
+        certificateCheck: function () {
+          return 0;
+        }
       }
-    }
-  });
+    });
+  } else {
+    await repo.fetch('origin', {
+      callbacks: {
+        credentials: function (_url_1: any, userName: string) {
+          //console.log(url, userName);
+          return nodegit.Cred.sshKeyFromAgent(userName);
+        }
+      }
+    });
+  }
 }
