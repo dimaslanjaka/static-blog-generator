09532fae402f6968370589b59c4c2491690884d1
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-25 17:16:26 +0700

    [git] + status getter

---
+++ src/node/git/get-status.ts
@@ -0,0 +1,40 @@
+import nodegit from 'nodegit';
+import path from 'path';
+
+/**
+ * get status git
+ * @param gitDir
+ */
+export default async function gitGetStatus(
+  gitDir = path.resolve(process.cwd(), '.git')
+) {
+  const repo = await nodegit.Repository.open(gitDir);
+  const statuses = await repo.getStatus();
+
+  const results: string[] = [];
+  statuses.forEach(function (file) {
+    results.push(file.path() + ' ' + statusToText(file));
+  });
+  return results;
+}
+
+function statusToText(status: nodegit.StatusFile) {
+  const words = [];
+  if (status.isNew()) {
+    words.push('NEW');
+  }
+  if (status.isModified()) {
+    words.push('MODIFIED');
+  }
+  if (status.isTypechange()) {
+    words.push('TYPECHANGE');
+  }
+  if (status.isRenamed()) {
+    words.push('RENAMED');
+  }
+  if (status.isIgnored()) {
+    words.push('IGNORED');
+  }
+
+  return words.join(' ');
+}
