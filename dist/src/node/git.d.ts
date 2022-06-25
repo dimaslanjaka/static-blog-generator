c8f2568c746ca4ed0ac03541ea1f74f8fe53c102
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-25 12:58:31 +0700

    [6f39b55] build 2022-06-25 12:58:26 PM GMT+07:00 WIB

--- dist/src/node/git.d.ts
+++ dist/src/node/git.d.ts
@@ -3,11 +3,10 @@
 import { SpawnOptions } from 'child_process';
 /**
  * git command
- * @param options git argument or spawn options
- * @param args git variadic arguments
+ * @param args
  * @returns
  */
-export declare function git(options?: null | string | SpawnOptions, ...args: string[]): Promise<{
+export declare function git(options?: null | SpawnOptions, ...args: string[]): Promise<{
     code: number;
     stdout: string[] | import("stream").Readable;
     stderr: any;
