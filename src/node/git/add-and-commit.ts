4a19303de4042bc06c8a2bdb5cb46e25f20bf1c6
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-28 18:26:40 +0700

    -nodegit

--- src/node/git/add-and-commit.ts
+++ src/node/git/add-and-commit.ts
@@ -1,42 +1,24 @@
-import nodegit from 'nodegit';
+import Bluebird from 'bluebird';
 import path from 'path';
-import { configDeploy } from '../../types/_config';
+import { modMoment } from '../../renderer/helpers/date';
+import spawner from '../spawner';
 
 export default async function gitAddCommit(
   gitDir = path.resolve(process.cwd(), '.git'),
   filePath: string,
   messages: string
 ) {
-  const repo = await nodegit.Repository.open(gitDir);
-  const index = await repo.refreshIndex();
-  if (filePath) {
-    index.addByPath(filePath);
-  } else {
-    index.addAll();
-  }
-
-  // this will write both files to the index
-  await index.write();
-
-  const oid = await index.writeTree();
-
-  const parent = await repo.getHeadCommit();
-
-  const username = configDeploy.user || configDeploy.username || 'Foo bar';
-  const author = nodegit.Signature.now(
-    username,
-    configDeploy.email || configDeploy.mail || 'foo@bar.com'
+  const add = await spawner.promise(
+    { cwd: gitDir },
+    'git',
+    'add',
+    filePath ? filePath : '-A'
   );
-  const committer = nodegit.Signature.now(username, username + '@github.com');
-
-  const commitId = await repo.createCommit(
-    'HEAD',
-    author,
-    committer,
-    messages,
-    oid,
-    [parent]
+  const commit = await spawner.promise(
+    { cwd: gitDir },
+    'git',
+    'commit',
+    typeof messages === 'string' ? messages : 'update ' + modMoment()
   );
-
-  console.log('New Commit: ', commitId);
+  return Bluebird.all([add, commit]);
 }
