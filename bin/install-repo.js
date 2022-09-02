8819aed731ea4bc2e4bb5240eceeefd5e3c6f237
dimaslanjaka <dimaslanjaka@gmail.com>
2022-09-02 12:54:28 +0700

    fix install local and repo builder

--- bin/install-repo.js
+++ bin/install-repo.js
@@ -1,6 +1,13 @@
-const { spawn } = require('child_process');
-const { join } = require('path');
+const spawnerBin = require('./spawner-bin');
 
-var isWin = process.platform === 'win32';
-const file = 'install-repo' + (isWin ? '.cmd' : '');
-spawn('./bin/' + file, { cwd: join(__dirname, '../') });
+const commands = `npm un hexo-post-parser persistent-cache sbg-themes safelinkify
+npm i git+https://github.com/dimaslanjaka/persistent-cache#improve2
+npm i git+https://github.com/dimaslanjaka/safelink#master
+npm i git+https://github.com/dimaslanjaka/hexo-post-parser#master
+npm i git+https://github.com/dimaslanjaka/persistent-cache#improve2
+npm i git+https://github.com/dimaslanjaka/google-news-sitemap#master
+npm install -O https://github.com/dimaslanjaka/sbg-themes.git
+npm i -D git+https://github.com/dimaslanjaka/nodejs-package-types#through2
+node scripts/preinstall.js`;
+
+spawnerBin(commands);
