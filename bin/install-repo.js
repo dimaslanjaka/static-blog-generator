885f7eaa300ac74279ce96725dad94abc3669a8f
dimaslanjaka <dimaslanjaka@gmail.com>
2022-09-02 12:30:38 +0700

    fix spawn argument

--- bin/install-repo.js
+++ bin/install-repo.js
@@ -2,5 +2,5 @@ const { spawn } = require('child_process');
 const { join } = require('path');
 
 var isWin = process.platform === 'win32';
-const file = 'install-repo' + isWin ? '.cmd' : '';
+const file = 'install-repo' + (isWin ? '.cmd' : '');
 spawn('./bin/' + file, { cwd: join(__dirname, '../') });
