171a41fc0102de11c7f265e31294d55ea7344a06
dimaslanjaka <dimaslanjaka@gmail.com>
2022-09-02 12:29:03 +0700

    use nodejs to run update

---
+++ bin/install-repo.js
@@ -0,0 +1,6 @@
+const { spawn } = require('child_process');
+const { join } = require('path');
+
+var isWin = process.platform === 'win32';
+const file = 'install-repo' + isWin ? '.cmd' : '';
+spawn('./bin/' + file, { cwd: join(__dirname, '../') });
