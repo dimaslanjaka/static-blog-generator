bf20dee9aebe3a70c4f79f5c9bbd89423ae5834a
dimaslanjaka <dimaslanjaka@gmail.com>
2022-09-02 12:03:41 +0700

    update @types/through2 and insert to updater

--- bin/install-repo.cmd
+++ bin/install-repo.cmd
@@ -7,4 +7,5 @@ npm i git+https://github.com/dimaslanjaka/hexo-post-parser#master
 npm i git+https://github.com/dimaslanjaka/persistent-cache#improve2
 npm i git+https://github.com/dimaslanjaka/google-news-sitemap#master
 npm install -O https://github.com/dimaslanjaka/sbg-themes.git
+npm i -D git+https://github.com/dimaslanjaka/nodejs-package-types#through2
 node scripts/preinstall.js
