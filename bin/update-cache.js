8af7c9462675af0baed3bb5170dbc5b6198d93a9
dimaslanjaka <dimaslanjaka@gmail.com>
2022-09-02 16:23:39 +0700

    update

--- bin/update-cache.js
+++ bin/update-cache.js
@@ -8,7 +8,7 @@ const hash = md5File.sync(join(__dirname, '../', 'package-lock.json'));
 writeFileSync(join(__dirname, '../.guid'), hash);
 spawn('git', ['add', '.guid'])
   .then(() =>
-    spawn('git', ['commit', '-m', 'update cache']).catch((e) => {
+    spawn('git', ['commit', '-m', 'update cache ' + new Date()]).catch((e) => {
       if (e instanceof Error) console.log(e.message);
     })
   )
