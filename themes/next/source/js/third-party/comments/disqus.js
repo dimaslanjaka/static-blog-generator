29c164b3514bff182c476d7f86721357f434e1ad
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-06 12:18:10 +0700

    update

--- themes/next/source/js/third-party/comments/disqus.js
+++ themes/next/source/js/third-party/comments/disqus.js
@@ -1,12 +1,14 @@
 /* global NexT, CONFIG, DISQUS */
 
 document.addEventListener('page:loaded', () => {
-
   if (CONFIG.disqus.count) {
     const loadCount = () => {
-      NexT.utils.getScript(`https://${CONFIG.disqus.shortname}.disqus.com/count.js`, {
-        attributes: { id: 'dsq-count-scr' }
-      });
+      NexT.utils.getScript(
+        `https://${CONFIG.disqus.shortname}.disqus.com/count.js`,
+        {
+          attributes: { id: 'dsq-count-scr' }
+        }
+      );
     };
 
     // defer loading until the whole page loading is completed
@@ -17,8 +19,8 @@ document.addEventListener('page:loaded', () => {
     // `disqus_config` should be a global variable
     // See https://help.disqus.com/en/articles/1717084-javascript-configuration-variables
     window.disqus_config = function() {
-      this.page.url = CONFIG.page.permalink;
-      this.page.identifier = CONFIG.page.path;
+      this.page.url = CONFIG.page.permalink.replace(/%20/g, ' ');
+      this.page.identifier = decodeURIComponent(CONFIG.page.path);
       this.page.title = CONFIG.page.title;
       if (CONFIG.disqus.i18n.disqus !== 'disqus') {
         this.language = CONFIG.disqus.i18n.disqus;
@@ -31,11 +33,13 @@ document.addEventListener('page:loaded', () => {
           config: window.disqus_config
         });
       } else {
-        NexT.utils.getScript(`https://${CONFIG.disqus.shortname}.disqus.com/embed.js`, {
-          attributes: { dataset: { timestamp: '' + +new Date() } }
-        });
+        NexT.utils.getScript(
+          `https://${CONFIG.disqus.shortname}.disqus.com/embed.js`,
+          {
+            attributes: { dataset: { timestamp: '' + +new Date() } }
+          }
+        );
       }
     });
   }
-
 });
