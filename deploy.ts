c821a596768a11b014cc5c82ae1ffc49d1b56aae
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-09 04:27:15 +0700

    update Sun Oct 09 2022 04:27:14 GMT+0700 (Western Indonesia Time)

--- deploy.ts
+++ deploy.ts
@@ -1,6 +1,8 @@
+import ansiColors from 'ansi-colors';
 import { existsSync } from 'fs';
 import { gitHelper } from 'git-command-helper/src';
 import gulp from 'gulp';
+import moment from 'moment-timezone';
 import { getConfig } from 'static-blog-generator';
 import { TaskCallback } from 'undertaker';
 import { join } from 'upath';
@@ -26,13 +28,62 @@ async function pull(done?: TaskCallback) {
 
 async function status(done?: TaskCallback) {
   const { github } = deployConfig();
-  await github.status().then((statuses) => {
-    console.log(statuses);
+  github.status().then((statuses) => {
+    statuses.map((item) => {
+      let str = '';
+
+      switch (item.changes) {
+        case 'deleted':
+          str += ansiColors.red(item.changes);
+          break;
+        case 'added':
+          str += ansiColors.green(item.changes);
+          break;
+        case 'modified':
+          str += ansiColors.yellow(item.changes);
+          break;
+        case 'untracked':
+          str += ansiColors.grey(item.changes);
+          break;
+        default:
+          str += item.changes;
+          break;
+      }
+
+      str += ' ';
+      str += item.path;
+      console.log(str);
+    });
+
     if (typeof done === 'function') done();
   });
 }
 
+async function commit() {
+  const { github, config } = deployConfig();
+  const now = moment().tz(config.timezone).format('LLL');
+  if (github.submodule.hasSubmodule()) {
+    const info = github.submodule.get();
+    const commitSubmodule = async (sub: typeof info[number]) => {
+      const submodule = new gitHelper(sub.root);
+      await submodule.addAndCommit('-A', `update ${sub.path} ${now}`, 'am');
+      submodule.status().then(console.log);
+    };
+    while (info.length > 0) {
+      try {
+        commitSubmodule(info[0]);
+      } catch {
+        //
+      }
+      info.shift();
+    }
+  }
+  await github.add('-A');
+  await github.commit('update site ' + now);
+}
+
 gulp.task('status', status);
+gulp.task('commit', commit);
 gulp.task('pull', pull);
 
 export function deployConfig() {
