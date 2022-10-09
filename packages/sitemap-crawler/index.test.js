372fe591b9b810558efc0884f29eed42c640f3cc
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-10 06:02:27 +0700

    update sitemap crawler

--- packages/sitemap-crawler/index.test.js
+++ packages/sitemap-crawler/index.test.js
@@ -1,15 +1,19 @@
 const siteMap = require('./index');
-const links = [
-  'http://www.amazon.com/',
-  'https://en.wikipedia.org/'
-];
+const fs = require('fs');
+const { join } = require('path');
+const links = ['http://www.amazon.com/', 'https://en.wikipedia.org/'];
 
 const opts = {
-  isProgress : true,
-  isLog : true
+  isProgress: true,
+  isLog: true
 };
 
 siteMap(links, opts, (err, res) => {
-  require('fs').writeFileSync('test.json', JSON.stringify(res, null, 2));
+  if (!fs.existsSync(join(__dirname, 'tmp')))
+    fs.mkdirSync(join(__dirname, 'tmp'));
+  fs.writeFileSync(
+    join(__dirname, 'tmp/test.json'),
+    JSON.stringify(res, null, 2)
+  );
   console.log('finish');
 });
