86475f7d352a74f760f94cca571ba8e73540c3bc
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-11 05:23:01 +0700

    update

---
+++ gulp.safelink.ts
@@ -0,0 +1,84 @@
+import { readdirSync, statSync } from 'fs';
+import gulp from 'gulp';
+import dom from 'gulp-dom';
+import { join } from 'path';
+import sf from 'safelinkify';
+import { getConfig } from 'static-blog-generator';
+import { TaskCallback } from 'undertaker';
+import { deployConfig } from './deploy';
+
+const config = getConfig();
+const { deployDir } = deployConfig();
+const configSafelink = Object.assign(
+  { enable: false },
+  config.external_link.safelink
+);
+const safelink = new sf.safelink({
+  redirect: [config.external_link.safelink.redirect],
+  password: config.external_link.safelink.password,
+  type: config.external_link.safelink.type
+});
+const internal_links = [
+  ...config.external_link.exclude,
+  new URL(config.url).host,
+  'www.webmanajemen.com',
+  'https://github.com/dimaslanjaka',
+  '/dimaslanjaka1',
+  'dimaslanjaka.github.io'
+];
+
+// safelinkify the deploy folder
+gulp.task('safelink', iterate);
+
+function iterate(_done?: TaskCallback) {
+  const paths = readdirSync(deployDir)
+    .map((path) => join(deployDir, path))
+    .filter((path) => statSync(path).isDirectory())
+    .map((path) => safelinkProcess(null, path));
+  return Promise.all(paths);
+}
+
+function safelinkProcess(_done?: TaskCallback, cwd = deployDir) {
+  return new Promise((resolve) => {
+    cwd = cwd || deployDir;
+    gulp
+      .src(['**/*.{html,htm}'], { cwd })
+      .pipe(
+        dom(function () {
+          //https://github.com/trygve-lie/gulp-dom
+          //this.querySelectorAll('body')[0].setAttribute('data-version', '1.0');
+          const elements = Array.from(this.querySelectorAll('a'));
+          if (configSafelink.enable) {
+            for (let i = 0; i < elements.length; i++) {
+              const a = elements[i];
+              const href = String(a['href']).trim();
+              if (new RegExp('^https?://').test(href)) {
+                /**
+                 * match host
+                 */
+                const matchHost = internal_links.includes(new URL(href).host);
+                /**
+                 * match url
+                 */
+                const matchHref = internal_links.includes(href);
+                if (!matchHref) {
+                  a.setAttribute('rel', 'nofollow noopener noreferer');
+                }
+                if (!matchHost && !matchHref) {
+                  const safelinkPath = safelink.encodeURL(href);
+                  if (
+                    typeof safelinkPath == 'string' &&
+                    safelinkPath.length > 0
+                  ) {
+                    a.setAttribute('href', safelinkPath);
+                  }
+                }
+              }
+            }
+          }
+        })
+      )
+      .pipe(gulp.dest(cwd))
+      .once('end', () => resolve(null));
+  });
+}
