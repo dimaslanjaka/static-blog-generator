31ecc2f9c54dd0dda788aea0fe318fad88436028
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-13 04:41:44 +0700

    update

--- gulp.watch-post.ts
+++ gulp.watch-post.ts
@@ -1,10 +1,14 @@
+import frontMatter from 'front-matter';
+import { writeFileSync } from 'fs';
 import gulp from 'gulp';
-import { buildPost, parsePost } from 'hexo-post-parser';
-import { getConfig } from 'static-blog-generator';
+import { buildPost, parsePost, postMap } from 'hexo-post-parser';
+import moment from 'moment-timezone';
+import { getConfig, scheduler } from 'static-blog-generator';
 import through2 from 'through2';
-import { extname, join } from 'upath';
+import { TaskCallback } from 'undertaker';
+import { extname, join, toUnix } from 'upath';
 
-gulp.task('watch-post', function (done) {
+function watchPost(done: TaskCallback) {
   const sourceDir = join(__dirname, 'src-posts');
   //const destDir = join(__dirname, 'source/_posts');
   const watcher = gulp.watch(['**/*'], { cwd: sourceDir });
@@ -14,14 +18,11 @@ gulp.task('watch-post', function (done) {
   watcher.on('add', (path) => {
     _copySingle(path);
   });
+  watcher.once('close', done);
+}
 
-  const close = () => {
-    watcher.close();
-    done();
-  };
-  process.on('SIGINT', close);
-  process.on('SIGTERM', close);
-});
+gulp.task('watch-post', watchPost);
+gulp.task('watch-posts', watchPost);
 
 const _copySingle = (identifier: string, callback?: CallableFunction) => {
   const sourceDir = join(__dirname, 'src-posts');
@@ -37,6 +38,7 @@ const _copySingle = (identifier: string, callback?: CallableFunction) => {
         ///fileList.push(file.path);
         if (file.isNull()) return next();
         if (file.extname === '.md') {
+          const config = <any>getConfig();
           const parse = await parsePost(file.path, {
             shortcodes: {
               youtube: true,
@@ -49,14 +51,57 @@ const _copySingle = (identifier: string, callback?: CallableFunction) => {
               codeblock: true
             },
             cache: false,
-            config: <any>getConfig(),
+            config,
             formatDate: true,
             fix: true,
             sourceFile: file.path
           });
-          const build = buildPost(parse);
-          file.contents = Buffer.from(build);
-          return next(null, file);
+          if (parse) {
+            // update post modified
+            const oriUp = parse.metadata.updated;
+            const oriPath = file.path;
+            parse.metadata.updated = moment()
+              .tz(config.timezone || 'UTC')
+              .format();
+            /*parse.metadata.date = moment(String(parse.metadata.date))
+              .tz(config.timezone || 'UTC')
+              .format();*/
+            const post = frontMatter<Record<string, any>>(
+              String(file.contents)
+            );
+            if ('updated' in post.attributes === false) {
+              post.attributes.updated = parse.metadata.updated;
+            }
+            post.attributes.updated = parse.metadata.updated;
+            post.attributes.date = parse.metadata.date;
+            const rBuild: postMap = {
+              metadata: <any>post.attributes,
+              body: post.body,
+              content: post.body,
+              config
+            };
+
+            // update original source post after process ends
+            scheduler.add(oriPath, function () {
+              const rebuild = buildPost(rBuild);
+              //writeFileSync(join(__dirname, 'tmp/rebuild.md'), rebuild);
+              console.log(
+                'write to',
+                toUnix(oriPath).replace(toUnix(process.cwd()), ''),
+                oriUp,
+                '->',
+                post.attributes.updated
+              );
+              writeFileSync(oriPath, rebuild); // write original post
+            });
+
+            const build = buildPost(parse);
+            file.contents = Buffer.from(build);
+            return next(null, file);
+          } else {
+            console.log('cannot parse', file.path);
+            return next(null);
+          }
         } else if (file.isDirectory()) {
           return next(null, file);
         }
