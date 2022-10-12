8683dfd136092044c2f28e9d73b10b05388b3e52
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-12 20:31:06 +0700

    [967822d48] build 2022-10-12 20:30:36 PM GMT+07:00 WIB

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -3,6 +3,7 @@ import './gulp/server';
 import './gulp/tasks/copy';
 import './gulp/tasks/deploy';
 import './gulp/tasks/generate';
+export { bindProcessExit } from './node/scheduler';
 export { clean_db, clean_posts, clean_public, clean_tmp } from './gulp/tasks/clean';
 export { copyAssets } from './gulp/tasks/copy/assets';
 export { copyPosts } from './gulp/tasks/copy/posts';
