27fb07f949298d8de640f3bf4f89e1b55ab2a88f
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-21 02:39:40 +0700

    change builds

--- gulp.safelink.ts
+++ gulp.safelink.ts
@@ -1,9 +1,9 @@
 import { readdirSync, statSync } from 'fs';
 import gulp from 'gulp';
-import dom from 'gulp-dom/src';
 import { join } from 'path';
 import sf from 'safelinkify';
 import { getConfig } from 'static-blog-generator';
+import through2 from 'through2';
 import { TaskCallback } from 'undertaker';
 import { deployConfig } from './deploy';
 
@@ -14,18 +14,21 @@ const configSafelink = Object.assign(
   config.external_link.safelink
 );
 const safelink = new sf.safelink({
+  // exclude patterns (dont anonymize these patterns)
+  exclude: [
+    ...config.external_link.exclude,
+    /https?:\/\/?(?:([^*]+)\.)?webmanajemen\.com/,
+    /([a-z0-9](?:[a-z0-9-]{1,61}[a-z0-9])?[.])*webmanajemen\.com/,
+    new URL(config.url).host,
+    'www.webmanajemen.com',
+    'https://github.com/dimaslanjaka',
+    '/dimaslanjaka1',
+    'dimaslanjaka.github.io'
+  ],
   redirect: [config.external_link.safelink.redirect],
   password: config.external_link.safelink.password,
   type: config.external_link.safelink.type
 });
-const internal_links = [
-  ...config.external_link.exclude,
-  new URL(config.url).host,
-  'www.webmanajemen.com',
-  'https://github.com/dimaslanjaka',
-  '/dimaslanjaka1',
-  'dimaslanjaka.github.io'
-];
 
 // safelinkify the deploy folder
 gulp.task('safelink', iterate);
@@ -54,57 +57,21 @@ export function safelinkProcess(_done?: TaskCallback, cwd = deployDir) {
         ]
       })
       .pipe(
-        dom(function (filePath) {
-          //https://github.com/trygve-lie/gulp-dom
-          //this.querySelectorAll('body')[0].setAttribute('data-version', '1.0');
-          const elements = Array.from(this.querySelectorAll('a'));
-          if (configSafelink.enable) {
-            for (let i = 0; i < elements.length; i++) {
-              const a = elements[i];
-              const href = String(a['href']).trim();
-              if (new RegExp('^https?://').test(href)) {
-                if (isValidHttpUrl(href)) {
-                  /**
-                   * match host
-                   */
-                  const matchHost = internal_links.includes(new URL(href).host);
-                  /**
-                   * match url
-                   */
-                  const matchHref = internal_links.includes(href);
-                  if (!matchHref) {
-                    a.setAttribute('rel', 'nofollow noopener noreferer');
-                  }
-                  if (!matchHost && !matchHref) {
-                    const safelinkPath = safelink.encodeURL(href);
-                    if (
-                      typeof safelinkPath == 'string' &&
-                      safelinkPath.length > 0
-                    ) {
-                      a.setAttribute('href', safelinkPath);
-                    }
-                  }
-                } else {
-                  console.log('invalid url', href, filePath);
-                }
-              }
-            }
+        through2.obj(async (file, _enc, next) => {
+          // drop null
+          if (file.isNull()) return next();
+          // do safelinkify
+          const content = String(file.contents);
+          const parsed = await safelink.parse(content);
+          if (parsed) {
+            file.contents = Buffer.from(parsed);
+            return next(null, file);
           }
+          console.log('cannot parse', file.path);
+          next();
         })
       )
       .pipe(gulp.dest(cwd))
       .once('end', () => resolve(null));
   });
-}
-
-function isValidHttpUrl(string: string | URL) {
-  let url: URL;
-
-  try {
-    url = new URL(string);
-  } catch (_) {
-    return false;
-  }
-
-  return url.protocol === 'http:' || url.protocol === 'https:';
 }
