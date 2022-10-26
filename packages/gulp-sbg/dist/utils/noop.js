6f245696a2f0415c21d144b85ec8c621883f1b25
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-27 04:06:55 +0700

    add docs

--- packages/gulp-sbg/dist/utils/noop.js
+++ packages/gulp-sbg/dist/utils/noop.js
@@ -1,5 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * no operations
+ * @param _args
+ * @returns
+ */
 function noop() {
     var _args = [];
     for (var _i = 0; _i < arguments.length; _i++) {
