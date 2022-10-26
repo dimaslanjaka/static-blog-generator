3d6effa719ee44b22e3d13a4cf6191fd301286e2
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-26 14:41:11 +0700

    update hotfix

--- packages/gulp-sbg/dist/gulp.post.d.ts
+++ packages/gulp-sbg/dist/gulp.post.d.ts
@@ -3,5 +3,10 @@
 import { TaskCallback } from 'undertaker';
 export declare function watchPost(done: TaskCallback): void;
 export declare const copySinglePost: (identifier: string, callback?: CallableFunction) => void;
-export declare function copyPost(): import("stream").Transform;
+/**
+ * copy function
+ * @param bind bind update date modified on process exit
+ * @returns
+ */
+export declare function copyPost(bind?: boolean): import("stream").Transform;
 export declare function copyAllPosts(): NodeJS.ReadWriteStream;
