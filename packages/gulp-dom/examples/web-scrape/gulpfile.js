1e25b69eb0cb644cf6044aff0bc5d0cfa139c2b8
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-26 11:57:07 +0700

    update

--- packages/gulp-dom/examples/web-scrape/gulpfile.js
+++ packages/gulp-dom/examples/web-scrape/gulpfile.js
@@ -1,28 +1,26 @@
 /* jshint node: true, strict: true */
 
-"use strict";
+'use strict';
 
-var request     = require('request'),
-    source      = require('vinyl-source-stream'),
-    buffer      = require('vinyl-buffer'),
-    gulp        = require('gulp'),
-    dom         = require('../../');
-
-
+var request = require('request'),
+  source = require('vinyl-source-stream'),
+  buffer = require('vinyl-buffer'),
+  gulp = require('gulp'),
+  dom = require('../../');
 
 // Gulp task for web scraping
 
-gulp.task('web-scrape', function() {
-    return request('http://nodejs.org/')
-        .pipe(source('example.txt'))
-        .pipe(buffer())
-        .pipe(dom(function(){
-            return this.title;
-        }, false))
-        .pipe(gulp.dest('./build'));
+gulp.task('web-scrape', function () {
+  return request('http://nodejs.org/')
+    .pipe(source('example.txt'))
+    .pipe(buffer())
+    .pipe(
+      dom(function () {
+        return this.title;
+      }, false)
+    )
+    .pipe(gulp.dest('./build'));
 });
-
-
 
 // Default task
 
--- packages/gulp-sbg/src/gulp.config.ts
+++ packages/gulp-sbg/src/gulp.config.ts
@@ -1,10 +1,14 @@
-import { readFileSync, writeFileSync } from 'fs';
+import { existsSync, readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
 import yaml from 'yaml';
 
-const file = join(__dirname, '_config.yml');
-const parse = yaml.parse(readFileSync(file, 'utf-8'));
-writeFileSync(join(__dirname, '_config.json'), JSON.stringify(parse, null, 2));
+const fileYML = join(process.cwd(), '_config.yml');
+let parse: Record<string, any> = {};
+
+if (existsSync(fileYML)) {
+  parse = yaml.parse(readFileSync(fileYML, 'utf-8'));
+  writeFileSync(join(__dirname, '_config.json'), JSON.stringify(parse, null, 2));
+}
 
 type importConfig = typeof import('./_config.json');
 export interface ProjConf extends importConfig {
