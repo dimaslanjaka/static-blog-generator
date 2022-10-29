8182278d2b5d2650b282193ea4e58e6ac97c3d55
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-29 12:07:15 +0700

    update

--- packages/article-rewriter/src/test.ts
+++ packages/article-rewriter/src/test.ts
@@ -1,10 +1,33 @@
 import escapeStringRegexp from 'escape-string-regexp';
-import thesaurus from './thesaurus-en.json' assert { type: 'json' };
 
-const text = 'install and activate VSCode ESLint extension for auto Linter And Formatter';
-Object.keys(thesaurus)
-  .map((str) => new RegExp(escapeStringRegexp('\b' + str + '\b'), 'i'))
-  .forEach((regex) => {
-    const has = regex.test(text);
-    console.log({ regex, has });
+main();
+
+interface Thesaurus {
+  [key: string]: string[];
+}
+
+async function main() {
+  const text = 'install and activate VSCode ESLint extension for auto Linter And Formatter';
+  const { default: thesaurus }: { default: Thesaurus } = await import('./thesaurus-en.json', {
+    assert: {
+      type: 'json'
+    }
   });
+  Object.keys(thesaurus)
+    // /\b(string)\b
+    .map((str) => {
+      return {
+        regex: new RegExp('\\b(' + escapeStringRegexp(str) + ')\\b', 'i'),
+        thesaurus: thesaurus[str]
+      };
+    })
+    .forEach(({ regex, thesaurus }) => {
+      const has = regex.test(text);
+      if (has) {
+        text.replace(regex, function (whole, match) {
+          console.log({ whole, match, thesaurus });
+          return whole;
+        });
+      }
+    });
+}
