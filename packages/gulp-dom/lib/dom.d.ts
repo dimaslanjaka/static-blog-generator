3d1eabf5f0df600a8aa3021aa275b8fcc3899486
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-29 11:06:20 +0700

    re-init packages

--- packages/gulp-dom/lib/dom.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="node" />
-declare type cb = (this: Document, path: string) => any;
-/**
- * gulpDom
- * @param mutator callback
- * @returns
- */
-export default function gulpDom(mutator: cb): import("stream").Transform;
-export {};
