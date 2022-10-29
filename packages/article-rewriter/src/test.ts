d7bb404e6bebc3c567b0253c8eb37083aeb58d37
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-29 11:19:54 +0700

    update sub project

--- packages/article-rewriter/src/test.ts
+++ packages/article-rewriter/src/test.ts
@@ -1,5 +1,9 @@
+import axios from 'axios';
+
 const text = 'install and activate VSCode ESLint extension for auto Linter And Formatter';
 const base = 'https://api.funtranslations.com/translate/article_rewrite.json?text=';
 
 const url = base + encodeURI(text);
-axios.get(url);
+axios.get(url).then((response) => {
+  console.log(response.data);
+});
