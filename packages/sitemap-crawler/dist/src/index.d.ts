bd6671429afec7528a051df6ddcdf086153e6778
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-29 11:06:20 +0700

    re-init packages

--- packages/sitemap-crawler/dist/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare const sitemap: (link: string | string[], opts?: {
-    isProgress: boolean;
-    isLog: boolean;
-}, callback?: ((arg0: Error | null, arg1?: any) => void) | undefined) => void;
-export default sitemap;
