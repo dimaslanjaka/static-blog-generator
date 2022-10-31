4edbdf3e8756ed5920d420eb87b176f0dee1d2f8
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-31 11:08:09 +0700

    update

--- packages/gulp-dom/lib/index.js
+++ packages/gulp-dom/lib/index.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.gulpDom = void 0;
-const dom_1 = __importDefault(require("./dom"));
+const tslib_1 = require("tslib");
+const dom_1 = tslib_1.__importDefault(require("./dom"));
 exports.gulpDom = dom_1.default;
 exports.default = dom_1.default;
