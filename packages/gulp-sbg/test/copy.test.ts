682b2c78bf323ed6bfd893f3f35bdc2c837268bc
dimaslanjaka <dimaslanjaka@gmail.com>
2022-10-31 13:06:54 +0700

    update

--- packages/gulp-sbg/test/copy.test.ts
+++
@@ -1,6 +0,0 @@
-import { spawn } from 'child_process';
-import { hexoProject } from '../project';
-
-const cwd = hexoProject;
-
-const child = spawn('npm', ['run', 'copy'], { cwd, stdio: 'inherit' });
