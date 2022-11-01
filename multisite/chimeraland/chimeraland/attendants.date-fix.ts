27b52a4f195897f2d8bdb2b452f25082cc4d57ae
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-01 14:42:20 +0700

    [attend] first modified dates

---
+++ multisite/chimeraland/chimeraland/attendants.date-fix.ts
@@ -0,0 +1,47 @@
+import { writeFile } from 'fs'
+import moment from 'moment'
+import { join } from 'upath'
+import attendants from './attendants.json'
+
+const updates: string[] = []
+const dates: string[] = []
+
+attendants.data = attendants.data.map((data) => {
+  let updated = data.dateModified
+  let date = data.datePublished
+  while (dates.includes(date)) {
+    // add 1 hour to date published
+    date = moment(date)
+      .add(Math.floor(Math.random() * 10), 'hours')
+      .format()
+    console.log({ date })
+  }
+
+  while (updates.includes(updated)) {
+    // add 1 hour to updated
+    updated = moment(updated)
+      .add(Math.floor(Math.random() * 10), 'hours')
+      .format()
+    console.log({ updated })
+  }
+
+  updates.push(updated)
+  dates.push(date)
+
+  data.datePublished = date
+  data.dateModified = updated
+
+  return data
+})
+
+writeFile(
+  join(__dirname, 'attendants.json'),
+  JSON.stringify(attendants, null, 2),
+  function (e) {
+    if (e instanceof Error) {
+      console.log(e.message)
+    } else {
+      console.log('written succesful')
+    }
+  }
+)
