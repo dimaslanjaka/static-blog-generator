27431a4b588514a8457b583034ee8d924db90228
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-03 04:01:12 +0700

    format prettier html

--- multisite/chimeraland/project.js
+++ multisite/chimeraland/project.js
@@ -1,10 +1,9 @@
 const { join } = require('upath')
-const spawn = require('cross-spawn')
 
 const hexoProject = join(__dirname, '../../')
 
 function copyPost() {
-  spawn('npm', ['run', 'copy'], { cwd: hexoProject, stdio: 'inherit' })
+  require('./test/copy')
 }
 
 module.exports = {
