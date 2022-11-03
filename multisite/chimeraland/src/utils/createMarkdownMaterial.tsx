0f4ca067f818570945b0b31b99927e9787215806
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-03 10:12:07 +0700

    update material post body

--- multisite/chimeraland/src/utils/createMarkdownMaterial.tsx
+++ multisite/chimeraland/src/utils/createMarkdownMaterial.tsx
@@ -1,3 +1,4 @@
+import Bluebird from 'bluebird'
 import { existsSync, mkdirpSync, writeFileSync } from 'fs-extra'
 import moment from 'moment'
 import prettier from 'prettier'
@@ -5,165 +6,263 @@ import ReactDOMServer from 'react-dom/server'
 import slugify from 'slugify'
 import { dirname, join } from 'upath'
 import yaml from 'yaml'
-import { hexoProject } from '../../project'
-import { MaterialsData } from './chimeraland'
+import { copyPost, hexoProject } from '../../project'
+import { jsxJoin } from './array-jsx'
+import { MaterialsData, RecipesData } from './chimeraland'
 import { capitalizer } from './string'
 
 const publicDir = join(hexoProject, 'src-posts/chimeraland/materials')
 
-MaterialsData.forEach((item) => {
-  const attr: Record<string, any> = {}
-  attr.title = 'Material ' + item.name
-  attr.webtitle = 'Chimeraland'
-  attr.date = item.datePublished
-  attr.updated = item.dateModified
-  //attr.updated = moment().format()
-  attr.author = 'L3n4r0x'
-  attr.permalink = item.pathname
-  attr.photos = item.images.map(
-    (image: { absolutePath: string; pathname: string }) => image.pathname
-  )
-  if (item.images.length > 0) {
-    const featured = item.images.find((image) =>
-      /feature/i.test(image.filename)
+Bluebird.all(MaterialsData)
+  .each((item) => {
+    const attr: Record<string, any> = {}
+    attr.title = 'Material ' + item.name
+    attr.webtitle = 'Chimeraland'
+    attr.date = item.datePublished
+    attr.updated = item.dateModified
+    //attr.updated = moment().format()
+    attr.author = 'L3n4r0x'
+    attr.permalink = item.pathname
+    attr.photos = item.images.map(
+      (image: { absolutePath: string; pathname: string }) => image.pathname
     )
-    attr.thumbnail = (featured || item.images[0]).pathname
-  }
-  attr.tags = ['Material']
-  attr.categories = ['Games', 'Chimeraland', 'Materials']
+    if (item.images.length > 0) {
+      const featured = item.images.find((image) =>
+        /feature/i.test(image.filename)
+      )
+      attr.thumbnail = (featured || item.images[0]).pathname
+    }
+    attr.tags = ['Material']
+    attr.categories = ['Games', 'Chimeraland', 'Materials']
 
-  let indexer = 0
+    let indexer = 0
 
-  const mdC = (
-    <section id="bootstrap-wrapper">
-      <link
-        rel="stylesheet"
-        href="https://cdn.statically.io/gh/dimaslanjaka/Web-Manajemen/40ac3225/css/bootstrap-4.5-wrapper.css"
-      />
-      <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm position-relative">
-        <div className="col p-4 d-flex flex-column position-static">
-          <strong className="d-inline-block mb-2 text-success">
-            {item.type}
-          </strong>
-          <h3 className="mb-0">{item.name}</h3>
-          <div className="mb-1 text-muted">
-            {moment(item.dateModified).format('LLL')}
+    const recipes = findRecipe(item.name)
+
+    const mdC = (
+      <section id="bootstrap-wrapper">
+        <link
+          rel="stylesheet"
+          href="https://cdn.statically.io/gh/dimaslanjaka/Web-Manajemen/40ac3225/css/bootstrap-4.5-wrapper.css"
+        />
+        <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm position-relative">
+          <div className="col p-4 d-flex flex-column position-static">
+            <strong className="d-inline-block mb-2 text-success">
+              {item.type}
+            </strong>
+            <h3 className="mb-0">{item.name}</h3>
+            <div className="mb-1 text-muted">
+              {moment(item.dateModified).format('LLL')}
+            </div>
+            {'background-info' in item && (
+              <div className="mb-2 border p-1">{item['background-info']}</div>
+            )}
+            <a href={attr.permalink} className="stretched-link d-none">
+              Continue reading {item.name}
+            </a>
           </div>
-          {'background-info' in item && (
-            <div className="mb-2 border p-1">{item['background-info']}</div>
-          )}
-          <a href="#" className="stretched-link d-none">
-            Continue reading {item.name}
-          </a>
-        </div>
-        <div className="col-auto d-none d-lg-block">
-          <img
-            src={
-              item.images[0]
-                ? item.images[0].pathname
-                : 'https://via.placeholder.com/550x50/FFFFFF/000000/?text=' +
-                  item.name
-            }
-            alt={item.name}
-          />
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-lg-6 col-12 mb-2">
-          {'details' in item && (
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">
-                  What is the use of the {item.name}
-                </h5>
-                <div className="card-text">
-                  <ul>
-                    {(item.details as string[]).map((str, i) => (
-                      <li key={'details' + i}>{str}</li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-            </div>
-          )}
-        </div>
-
-        <div className="col-lg-6 col-12 mb-2">
-          {'howto' in item && (
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">How to get {item.name}</h5>
-                <div className="card-text">
-                  <ul>
-                    {(item.howto as string[]).map((str, i) => (
-                      <li key={'details' + i}>{str}</li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-            </div>
-          )}
-        </div>
-
-        <div className="col-12 mb-2">
-          <h5>{item.name} Spawn Locations</h5>
-          {item.images.length > 1 ? (
-            (
-              item.images as {
-                absolutePath: string
-                pathname: string
-              }[]
-            ).map((o, ii) => {
-              indexer++
-              if (!/mount/i.test(o.pathname)) {
-                return <div key={o.pathname + ii + indexer}></div>
+          <div className="col-auto d-none d-lg-block">
+            <img
+              src={
+                item.images[0]
+                  ? item.images[0].pathname
+                  : 'https://via.placeholder.com/550x50/FFFFFF/000000/?text=' +
+                    item.name
               }
-
-              const split = o.pathname.split(/\//g)
-              const imgTitle = split[split.length - 1]
-                .substring(0, split[split.length - 1].lastIndexOf('.'))
-                .split(/-/g)
-                .map((str) => capitalizer(str))
-                .join(' ')
-              return (
-                <div key={o.pathname + ii + indexer} className="p-4">
-                  <h5>{imgTitle}</h5>
-                  <img
-                    src={o.pathname}
-                    alt={item.name}
-                    data-fancybox="true"
-                    width="100%"
-                  />
-                </div>
-              )
-            })
-          ) : (
-            <p>{item.name} is randomly spawned</p>
-          )}
+              alt={item.name}
+            />
+          </div>
         </div>
-      </div>
-    </section>
-  )
 
-  const html = ReactDOMServer.renderToStaticMarkup(mdC).toString()
+        <div className="row">
+          <div className="col-lg-6 col-12 mb-2">
+            {'details' in item && (
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">
+                    What is the use of the {item.name}
+                  </h5>
+                  <div className="card-text">
+                    <ul>
+                      {(item.details as string[]).map((str, i) => (
+                        <li key={'details' + i}>{str}</li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
 
-  const output = join(
-    publicDir,
-    slugify(item.name, { trim: true, lower: true }) + '.md'
-  )
-  console.log(output)
-  if (!existsSync(dirname(output))) mkdirpSync(dirname(output))
-  writeFileSync(
-    output,
-    `
+          <div className="col-lg-6 col-12 mb-2">
+            {'howto' in item && (
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">How to get {item.name}</h5>
+                  <div className="card-text">
+                    <ul>
+                      {(item.howto as string[]).map((str, i) => (
+                        <li key={'details' + i}>{str}</li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+
+          {/** print recipes only for cookable material */}
+          {recipes && (
+            <div className="col-12 mb-2">
+              <h2 id="cookable">Cooking Recipes Using {item.name}</h2>
+              {recipes}
+            </div>
+          )}
+
+          <div className="col-12 mb-2">
+            <h5>{item.name} Spawn Locations</h5>
+            {item.images.length > 1 ? (
+              (
+                item.images as {
+                  absolutePath: string
+                  pathname: string
+                }[]
+              ).map((o, ii) => {
+                indexer++
+                if (!/mount/i.test(o.pathname)) {
+                  return <div key={o.pathname + ii + indexer}></div>
+                }
+
+                const split = o.pathname.split(/\//g)
+                const imgTitle = split[split.length - 1]
+                  .substring(0, split[split.length - 1].lastIndexOf('.'))
+                  .split(/-/g)
+                  .map((str) => capitalizer(str))
+                  .join(' ')
+                return (
+                  <div key={o.pathname + ii + indexer} className="p-4">
+                    <h5>{imgTitle}</h5>
+                    <img
+                      src={o.pathname}
+                      alt={item.name}
+                      data-fancybox="true"
+                      width="100%"
+                    />
+                  </div>
+                )
+              })
+            ) : (
+              <p>{item.name} is randomly spawned</p>
+            )}
+          </div>
+        </div>
+      </section>
+    )
+
+    const html = ReactDOMServer.renderToStaticMarkup(mdC).toString()
+
+    const output = join(
+      publicDir,
+      slugify(item.name, { trim: true, lower: true }) + '.md'
+    )
+    if (/icebug/i.test(item.name)) console.log(output)
+    if (!existsSync(dirname(output))) mkdirpSync(dirname(output))
+    writeFileSync(
+      output,
+      `
 ---
 ${yaml.stringify(attr).trim()}
 ---
 
 ${prettier.format(html, { parser: 'html' })}
   `.trim()
-  )
+    )
 
-  //console.log(output)
-})
+    //console.log(output)
+  })
+  .then(() => {
+    copyPost()
+  })
+
+function findRecipe(matname: string) {
+  const find = RecipesData.filter((item) =>
+    item.recipes.some((str) => str.includes(matname))
+  ).map((item, i) => {
+    const id = (prefix: string) =>
+      slugify(prefix + item.name, {
+        lower: true,
+        trim: true,
+        replacement: '-',
+        strict: true
+      })
+    return (
+      <div id={id('recipe-')} key={item.name + i + matname}>
+        <h5 id={id('item-')}>{item.name}</h5>
+        {item.recipes.map((recipe, ri) => {
+          let device = ''
+          const rg = /--device: (.*)--/i
+          const split = recipe.match(rg)
+          if (split) {
+            device = split[1]
+          }
+          const recipeMaterials = recipe
+            .replace(rg, '')
+            .trim()
+            .split('+')
+            .map((str) => str.trim())
+            .map((str, mi) => {
+              const replacement = str
+                .replace(/\[\d\]/, '')
+                .trim()
+                .split('/')
+                .map((cleanstr) => {
+                  if (cleanstr.includes('/')) console.log(cleanstr)
+                  const findmat = MaterialsData.find(
+                    (mat) =>
+                      slugify(mat.name, { lower: true, trim: true }) ===
+                      slugify(cleanstr, { lower: true, trim: true })
+                  )
+                  if (findmat) {
+                    return (
+                      <a
+                        className="text-decoration-none"
+                        href={findmat.pathname}
+                        key={'material' + ri + mi}>
+                        {cleanstr}
+                      </a>
+                    )
+                  } else {
+                    //console.log(cleanstr)
+                    return <>{cleanstr}</>
+                  }
+                })
+
+              return jsxJoin(replacement, <span> / </span>)
+            })
+          //console.log(replace)
+          return (
+            <div className="mb-2" key={id('recipe-' + ri + '-')}>
+              <div className="card">
+                <div className="card-body">
+                  <h2 className="card-title fs-5">
+                    Recipe {item.name} {ri + 1}
+                  </h2>
+                  <div className="card-text">
+                    <ul>
+                      <li>{jsxJoin(recipeMaterials, <span> / </span>)}</li>
+                      {<li>Device: {device || 'Stove or Camp'}</li>}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    )
+  })
+  if (find) {
+    return jsxJoin(find, <br />)
+  }
+  return null
+}
