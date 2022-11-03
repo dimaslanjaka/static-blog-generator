4d6eb82564fece3b8b316ee0d8a2ad66bc29fc5c
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-03 07:04:33 +0700

    add info getter

---
+++ multisite/chimeraland/chimeraland/get-info.js
@@ -0,0 +1,41 @@
+const { statSync, writeFileSync } = require('fs-extra')
+const GulpClient = require('gulp')
+const through2 = require('through2')
+const moment = require('moment-timezone')
+const { join, toUnix } = require('upath')
+
+// only run this script on termux
+
+const info = []
+
+GulpClient.src(['**/*.{jpg,jpeg,png,webp}'], { cwd: __dirname }).pipe(
+  through2.obj((file, _enc, callback) => {
+    if (!file.isDirectory()) {
+      const stats = statSync(file.path)
+      // mtime = modified date
+      // atime = last accessed date
+      // birthtime = create date
+
+      const modified = moment(stats.mtimeMs).tz('Asia/Jakarta').format()
+      const created = moment(
+        stats.birthtimeMs === 0 ? stats.mtimeMs : stats.birthtimeMs
+      )
+        .tz('Asia/Jakarta')
+        .format()
+      const obj = {
+        filename: file.basename,
+        dirname: removeCwd(file.dirname),
+        path: removeCwd(file.path),
+        created,
+        modified
+      }
+      info.push(obj)
+      writeFileSync(join(__dirname, 'info.json'), JSON.stringify(info, null, 2))
+    }
+    callback()
+  })
+)
+
+function removeCwd(str) {
+  return toUnix(str).replace(join(__dirname, '..'), '')
+}
