d183f1670681a22107486ff87d2cb3cfbbba825b
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-08 03:44:07 +0700

    unfix

--- packages/instant-indexing/.eslintrc.js
+++ packages/instant-indexing/.eslintrc.js
@@ -12,10 +12,20 @@ module.exports = {
         node: true,
     },
     extends: [
-        'standard-with-typescript',
+        'eslint:recommended', // uses eslint default recommended
+        'plugin:@typescript-eslint/eslint-recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
         'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     ],
-    overrides: [],
+    // override rules for js files
+    overrides: [
+        {
+            files: ['*.js'],
+            rules: {
+                '@typescript-eslint/no-var-requires': 'off', // disable require warning on js files
+            },
+        },
+    ],
     parserOptions: {
         ecmaVersion: 'latest',
     },
