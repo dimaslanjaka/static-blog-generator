2f3e8822fabb732f211ddd46348b4d056cfb2033
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-08 03:03:32 +0700

    update

--- packages/instant-indexing/src/config/index.js
+++ packages/instant-indexing/src/config/index.js
@@ -1,14 +1,17 @@
-const { readdirSync, readFileSync } = require('fs-extra');
-const { join } = require('upath');
+const { readdirSync, readFileSync } = require('fs-extra')
+const { join } = require('upath')
 
 const readDir = readdirSync(__dirname)
-  .filter((str) => str.endsWith('.json'))
-  .map((str) => join(__dirname, str));
+    .filter((str) => str.endsWith('.json'))
+    .map((str) => join(__dirname, str))
 
-let serviceConfig;
+let serviceConfig
 
 if (readDir.length > 0) {
-  serviceConfig = JSON.parse(readFileSync(readDir[0], 'utf-8'));
+    serviceConfig = Object.assign(
+        { keyFile: readDir[0] },
+        JSON.parse(readFileSync(readDir[0], 'utf-8'))
+    )
 }
 
-module.exports = serviceConfig;
+module.exports = serviceConfig
