697692a080c9a4ea1fa35550e77c1d6927eb1988
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-08 01:44:15 +0700

    add linter and formatter (eslint+prettier)

--- packages/instant-indexing/src/index.js
+++ packages/instant-indexing/src/index.js
@@ -1,13 +1,17 @@
-const Notifier = require("./Notifier");
+const Notifier = require('./Notifier')
 
 class InstantIndexing {
-  /**
-   *
-   * @param {import('./globals').Key} key
-   */
-  constructor(key) {
-    //
-  }
+    /**
+     * @type {Notifier}
+     */
+    notifier
+    /**
+     *
+     * @param {import('./globals').Key} key
+     */
+    constructor(key) {
+        this.notifier = new Notifier(key)
+    }
 }
 
 module.exports = InstantIndexing
