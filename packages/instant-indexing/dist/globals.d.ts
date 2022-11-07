09527ef2f8f3c4774a2536cd3988808ecfc554d0
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-08 01:27:42 +0700

    add globals definition file

---
+++ packages/instant-indexing/dist/globals.d.ts
@@ -0,0 +1,4 @@
+export interface Key {
+    client_email: string;
+    private_key: string;
+}
--- packages/instant-indexing/dist/index.js
+++ packages/instant-indexing/dist/index.js
@@ -1,1 +1,12 @@
-module.exports = require('./Notifier');
+var Notifier = require("./Notifier");
+var InstantIndexing = /** @class */ (function () {
+    /**
+     *
+     * @param {import('./globals').Key} key
+     */
+    function InstantIndexing(key) {
+        //
+    }
+    return InstantIndexing;
+}());
+module.exports = InstantIndexing;
