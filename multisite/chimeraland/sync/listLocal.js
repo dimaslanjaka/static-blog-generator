621e2dfab9329714c20388ac3e85f06334db74b0
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-08 11:30:33 +0700

    add local auth test

---
+++ multisite/chimeraland/sync/listLocal.js
@@ -0,0 +1,31 @@
+const fs = require('fs').promises
+const path = require('path')
+
+async function listLocalFiles(localPath) {
+  try {
+    await fs.access(localPath)
+  } catch (e) {
+    //directory does not even exist yet
+    return []
+  }
+
+  let files = []
+
+  const dirFiles = await fs.readdir(localPath)
+  for (let i = 0; i < dirFiles.length; i++) {
+    const fullPath = path.join(localPath, dirFiles[i])
+    const isFolder = (await fs.stat(fullPath)).isDirectory()
+
+    if (isFolder) {
+      files = files.concat(await listLocalFiles(fullPath))
+    } else {
+      files.push(fullPath)
+    }
+  }
+
+  return files
+}
+
+module.exports = {
+  listLocalFiles
+}
