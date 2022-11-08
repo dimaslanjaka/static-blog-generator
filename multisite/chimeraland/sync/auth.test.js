301c6f96646f1f168162486c1c6aba2770998db2
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-08 11:30:33 +0700

    add local auth test

--- multisite/chimeraland/sync/auth.test.js
+++ multisite/chimeraland/sync/auth.test.js
@@ -1,40 +1,12 @@
 const Authenticator = require('./Authenticator.js')
 const { google } = require('googleapis')
-const fs = require('fs').promises
-const path = require('path')
 
-Authenticator.authenticate().then((oAuth) => {
-  const drive = google.drive({
-    version: 'v3',
-    auth: oAuth
+const _test1 = () =>
+  Authenticator.authenticate().then((oAuth) => {
+    const drive = google.drive({
+      version: 'v3',
+      auth: oAuth
+    })
   })
-})
 
-async function listLocalFiles(localPath) {
-  try {
-    await fs.access(localPath)
-  } catch (e) {
-    //directory does not even exist yet
-    return []
-  }
-
-  let files = []
-
-  const dirFiles = await fs.readdir(localPath)
-  for (let i = 0; i < dirFiles.length; i++) {
-    const fullPath = path.join(localPath, dirFiles[i])
-    const isFolder = (await fs.stat(fullPath)).isDirectory()
-
-    if (isFolder) {
-      files = files.concat(await listLocalFiles(fullPath))
-    } else {
-      files.push(fullPath)
-    }
-  }
-
-  return files
-}
-
-module.exports = {
-  listLocalFiles
-}
+Authenticator.localAuth()
