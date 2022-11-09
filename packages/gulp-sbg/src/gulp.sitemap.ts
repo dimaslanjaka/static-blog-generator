045d2ec9450347634d65885d007a5c4f17d9f14b
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-09 14:58:53 +0700

    prettify config

--- packages/gulp-sbg/src/gulp.sitemap.ts
+++ packages/gulp-sbg/src/gulp.sitemap.ts
@@ -21,6 +21,9 @@ const env = new nunjucks.Environment();
 env.addFilter('uriencode', (str) => {
   return encodeURL(str);
 });
+env.addFilter('noControlChars', (str) => {
+  return str.replace(/[\x00-\x1F\x7F]/g, ''); // eslint-disable-line no-control-regex
+});
 
 /**
  * Sitemap Generator
