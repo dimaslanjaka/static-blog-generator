6733b80667465128784a8fc8c1771e117234610c
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-09 13:22:53 +0700

    update

--- packages/sitemap-crawler/dist/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const sitemap_1 = tslib_1.__importDefault(require("./sitemap"));
-// export { default as siteMap } from './sitemap';
-const sitemap = sitemap_1.default;
-exports.default = sitemap;
--- packages/sitemap-crawler/dist/test/sitemapAsync.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sitemap_1 = require("../src/sitemap");
-const links = ['https://www.webmanajemen.com'];
-const opts = {
-    isProgress: true,
-    isLog: true
-};
-(0, sitemap_1.sitemapAsync)(links, opts).then(console.log);
