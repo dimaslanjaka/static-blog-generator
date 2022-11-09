f093fd82944bb5816c84f26abb89db7d68801d1d
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-09 15:24:38 +0700

    export utils/array

--- packages/gulp-sbg/dist/index.d.ts
+++ packages/gulp-sbg/dist/index.d.ts
@@ -2,3 +2,4 @@ import './gulpfile';
 export { copyAllPosts, copySinglePost, updatePost as copyPost, watchPost } from './gulp.post';
 export { generateSitemap, hexoGenerateSitemap } from './gulp.sitemap';
 export { commitProject } from './gulpfile';
+export { array_random, array_remove_empty, array_unique } from './utils/array';
--- packages/gulp-sbg/src/index.ts
+++ packages/gulp-sbg/src/index.ts
@@ -4,5 +4,6 @@ import scheduler from './utils/scheduler';
 export { copyAllPosts, copySinglePost, updatePost as copyPost, watchPost } from './gulp.post';
 export { generateSitemap, hexoGenerateSitemap } from './gulp.sitemap';
 export { commitProject } from './gulpfile';
+export { array_random, array_remove_empty, array_unique } from './utils/array';
 
 scheduler.register();
