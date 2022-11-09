791ed882059b46c434c6fac3c420154bfc643094
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-09 15:00:43 +0700

    add formatDate to nunjuck environtment

--- packages/gulp-sbg/src/gulp.sitemap.ts
+++ packages/gulp-sbg/src/gulp.sitemap.ts
@@ -24,6 +24,10 @@ env.addFilter('uriencode', (str) => {
 env.addFilter('noControlChars', (str) => {
   return str.replace(/[\x00-\x1F\x7F]/g, ''); // eslint-disable-line no-control-regex
 });
+// Extract date from datetime
+env.addFilter('formatDate', (input: import('moment-timezone').Moment) => {
+  return input.toISOString().substring(0, 10);
+});
 
 /**
  * Sitemap Generator
