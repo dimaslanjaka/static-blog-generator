c868452e3ff20dc7c6b8a351d2268c8e6446612c
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-09 13:55:10 +0700

    update sitemap generator

--- packages/gulp-sbg/dist/gulp.sitemap.d.ts
+++ packages/gulp-sbg/dist/gulp.sitemap.d.ts
@@ -1,2 +1,8 @@
 import Bluebird from 'bluebird';
+/**
+ * Sitemap Generator
+ * @param url url to crawl
+ * @param depth crawl deeper n times
+ * @returns
+ */
 export declare function generateSitemap(url?: string | null | undefined, depth?: number): Bluebird<string[]>;
