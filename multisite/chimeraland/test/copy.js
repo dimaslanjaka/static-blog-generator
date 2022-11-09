338215b8d597e58d396f574dc5c355c0b05ff1a1
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-09 08:42:10 +0700

    update

--- multisite/chimeraland/test/copy.js
+++ multisite/chimeraland/test/copy.js
@@ -1,4 +1,5 @@
 const spawn = require('cross-spawn')
 const { hexoProject } = require('../project')
 
+console.log('npm run copy on', hexoProject)
 spawn('npm', ['run', 'copy'], { cwd: hexoProject, stdio: 'inherit' })
