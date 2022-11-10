008846bb725c62f93cabbccf8ff85a5d97cae747
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-10 09:17:18 +0700

    add clean old archives task

--- packages/gulp-sbg/dist/gulp.deploy.d.ts
+++ packages/gulp-sbg/dist/gulp.deploy.d.ts
@@ -1,3 +1,13 @@
+/// <reference types="node" />
+/**
+ * copy generated files to deploy dir
+ * @returns
+ */
+export declare function copyGen(): NodeJS.ReadWriteStream;
+/**
+ * clean old archives (categories, tags, pagination)
+ */
+export declare function cleanOldArchives(): Promise<void>;
 export declare function deployConfig(): {
     deployDir: string;
     config: import("./gulp.config").ProjConf;
