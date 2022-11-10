34d77e5e0498c271725b4c7fd557d5eebb61ff79
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-11-11 06:03:25 +0700

    include release

--- packer.js
+++ packer.js
@@ -5,10 +5,13 @@ const { join, dirname } = require('upath');
 const packagejson = require('./package.json');
 
 const child = spawn('npm', ['pack'], { cwd: __dirname, stdio: 'ignore' });
+let version = (function () {
+  const v = parseVersion(packagejson.version);
+  return `${v.major}.${v.minor}.${v.patch}`;
+})();
+
 child.on('exit', function () {
-  const filename = slugifyPkgName(
-    `${packagejson.name}-${packagejson.version}.tgz`
-  );
+  const filename = slugifyPkgName(`${packagejson.name}-${version}.tgz`);
   const tgz = join(__dirname, filename);
   const tgzlatest = join(
     __dirname,
@@ -36,4 +39,23 @@ child.on('exit', function () {
 
 function slugifyPkgName(str) {
   return str.replace(/\//g, '-').replace(/@/g, '');
+}
+
+/**
+ * Parse Version
+ * @param {string} versionString
+ * @returns
+ */
+function parseVersion(versionString) {
+  var vparts = versionString.split('.');
+  const version = {
+    major: parseInt(vparts[0]),
+    minor: parseInt(vparts[1]),
+    patch: parseInt(vparts[2].split('-')[0]),
+    build: parseInt(vparts[3] || null),
+    range: parseInt(vparts[2].split('-')[1]),
+    commit: vparts[2].split('-')[2]
+  };
+
+  return version;
 }
