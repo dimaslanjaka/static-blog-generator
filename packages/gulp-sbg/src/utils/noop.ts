9726db5b7d98d20eaebd0d376abd2f23cd7e8fc2
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-10 08:52:48 +0700

    add nooperation for clean

--- packages/gulp-sbg/src/utils/noop.ts
+++ packages/gulp-sbg/src/utils/noop.ts
@@ -6,3 +6,22 @@
 export default function noop(..._args: any[]) {
   return;
 }
+
+interface FN extends CallableFunction {
+  then: CallableFunction;
+  catch: CallableFunction;
+}
+
+/**
+ * try catch nooperation
+ * @param fn
+ * @returns
+ */
+export function trycatchnoop(fn: FN) {
+  try {
+    if (typeof fn.catch === 'function') return fn.catch(noop);
+    if (typeof fn === 'function') fn();
+  } catch {
+    //
+  }
+}
