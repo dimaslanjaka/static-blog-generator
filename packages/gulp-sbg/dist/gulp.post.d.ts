c171a7ab3002c173b8da4496b094dfd457c3a21b
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-11 01:35:35 +0700

    update jsdoc

--- packages/gulp-sbg/dist/gulp.post.d.ts
+++ packages/gulp-sbg/dist/gulp.post.d.ts
@@ -1,5 +1,14 @@
 import { TaskCallback } from 'undertaker';
+/**
+ * Watch post while you writing new or modify posts from src-posts folder
+ * @param done
+ */
 export declare function watchPost(done: TaskCallback): void;
+/**
+ * Copy single post from src-posts folder to source/_posts
+ * @param identifier
+ * @param callback
+ */
 export declare const copySinglePost: (identifier: string, callback?: CallableFunction) => void;
 /**
  * copy watched post
