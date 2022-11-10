80bba388d009c53e372558f8cd55e87e28440138
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-10 08:52:48 +0700

    add nooperation for clean

--- packages/gulp-sbg/dist/utils/noop.js
+++ packages/gulp-sbg/dist/utils/noop.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.trycatchnoop = void 0;
 /**
  * no operations
  * @param _args
@@ -13,3 +14,20 @@ function noop() {
     return;
 }
 exports.default = noop;
+/**
+ * try catch nooperation
+ * @param fn
+ * @returns
+ */
+function trycatchnoop(fn) {
+    try {
+        if (typeof fn.catch === 'function')
+            return fn.catch(noop);
+        if (typeof fn === 'function')
+            fn();
+    }
+    catch (_a) {
+        //
+    }
+}
+exports.trycatchnoop = trycatchnoop;
