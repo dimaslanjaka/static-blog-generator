0a0d463bc75514b5d0723b188338c24db627b550
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-11 12:09:54 +0700

    add test

--- packages/gulp-sbg/dist/gulp-utils/gulp.cache.js
+++ packages/gulp-sbg/dist/gulp-utils/gulp.cache.js
@@ -3,35 +3,45 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.gulpCached = exports.calculateHash = void 0;
+exports.gulpCached = exports.md5 = exports.getShaFile = void 0;
 var crypto_1 = __importDefault(require("crypto"));
 var fs_1 = __importDefault(require("fs"));
 var persistent_cache_1 = require("persistent-cache");
 var through2_1 = __importDefault(require("through2"));
+var upath_1 = require("upath");
 /**
  * calculate sha1sum of file
  * @param file
  * @returns
  */
-function calculateHash(file) {
+function getShaFile(file) {
+    if (fs_1.default.statSync(file).isDirectory())
+        return null;
     var testFile = fs_1.default.readFileSync(file);
     var sha1sum = crypto_1.default.createHash('sha1').update(testFile).digest('hex');
     return sha1sum;
 }
-exports.calculateHash = calculateHash;
+exports.getShaFile = getShaFile;
+var md5 = function (data) { return crypto_1.default.createHash('md5').update(data).digest('hex'); };
+exports.md5 = md5;
 /**
  *
  * @param options
  * @returns
  */
 function gulpCached(options) {
-    if (options === void 0) { options = { name: 'gulp-cached' }; }
+    if (options === void 0) { options = {}; }
+    options = Object.assign(options, { name: 'gulp-cached', base: (0, upath_1.join)(process.cwd(), 'tmp') });
     var caches = (0, persistent_cache_1.persistentCache)(options);
     return through2_1.default.obj(function (file, _enc, next) {
-        var hasCache = caches.getSync(file.path);
-        var sha1sum = calculateHash(file.path);
+        // skip directory
+        if (file.isDirectory())
+            return next(null, file);
+        var cacheKey = (0, exports.md5)(file.path);
+        var hasCache = caches.getSync(cacheKey);
+        var sha1sum = getShaFile(file.path);
         if (!hasCache) {
-            caches.setSync(file.path, sha1sum);
+            caches.setSync(cacheKey, sha1sum);
             return next(null, file);
         }
         else {
