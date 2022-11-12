05056d2dc2661e7078ba670c0b91846fa71777b9
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-12 22:14:48 +0700

    update gulp cache

--- packages/gulp-sbg/dist/gulp-utils/gulp.cache.d.ts
+++ packages/gulp-sbg/dist/gulp-utils/gulp.cache.d.ts
@@ -7,9 +7,12 @@ import { persistentCache } from 'persistent-cache';
  */
 export declare function getShaFile(file: string): string | null;
 export declare const md5: (data: string) => string;
+export declare type gulpCachedOpt = Parameters<typeof persistentCache>[0] & {
+    prefix?: string;
+};
 /**
- *
+ * * [source idea](https://github.com/gulp-community/gulp-cached/blob/8e8d13cb07b17113ff94700e87f136eeaa1f1340/index.js#L35-L44)
  * @param options
  * @returns
  */
-export declare function gulpCached(options?: Parameters<typeof persistentCache>[0]): import("stream").Transform;
+export declare function gulpCached(options?: gulpCachedOpt): import("stream").Transform;
