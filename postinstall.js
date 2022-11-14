ec9b9e4ac7605ec59b75acd12e4faa42b2236273
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-14 12:58:00 +0700

    update post and pre install script

--- postinstall.js
+++ postinstall.js
@@ -1,1 +1,24 @@
 require('./bin/update-cache.js');
+const pjson = require('./package.json');
+const { summon } = require('./preinstall.js');
+
+(async () => {
+  // @todo clear cache local packages
+  const packages = Object.assign(pjson.dependencies, pjson.devDependencies);
+  for (const pkgname in packages) {
+    /**
+     * @type {string}
+     */
+    const version = packages[pkgname];
+    if (version.startsWith('file:')) {
+      /*
+      const nodeModule = join(__dirname, 'node_modules', pkgname);
+      if (existsSync(nodeModule)) {
+        console.log('deleting', nodeModule);
+        rmSync(nodeModule, { recursive: true });
+      }
+      */
+      await summon('npm', ['install', version], { cwd: __dirname });
+    }
+  }
+})();
