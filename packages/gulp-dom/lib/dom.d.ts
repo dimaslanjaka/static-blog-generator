6fd9bfb38df661cb3050826d915500c45a48fd81
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-21 19:10:05 +0700

    prepare update 1.0.2

--- packages/gulp-dom/lib/dom.d.ts
+++ packages/gulp-dom/lib/dom.d.ts
@@ -1,9 +1,10 @@
 /// <reference types="node" />
-declare type cb = (this: Document, path: string) => any;
+import jsdom from 'jsdom';
+type cb = (this: Document, path: string) => any;
 /**
  * gulpDom
  * @param mutator callback
  * @returns
  */
-export default function gulpDom(mutator: cb): import("stream").Transform;
+export default function gulpDom(this: jsdom.JSDOM, mutator: cb): import("stream").Transform;
 export {};
