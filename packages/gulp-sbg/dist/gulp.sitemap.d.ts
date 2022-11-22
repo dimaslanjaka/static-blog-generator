5f58d0c2735a1a6a200d7f94864551249c78682f
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-22 16:29:36 +0700

    -neo theme

---
+++ packages/gulp-sbg/dist/gulp.sitemap.d.ts
@@ -0,0 +1,9 @@
+import Bluebird from 'bluebird';
+/**
+ * Sitemap Generator
+ * @param url url to crawl
+ * @param deep crawl deeper n times
+ * @returns
+ */
+export declare function generateSitemap(url?: string | null | undefined, deep?: number): Bluebird<string[]>;
+export declare function hexoGenerateSitemap(): Bluebird<unknown>;
---
+++ packages/gulp-sbg/dist/gulpfile.d.ts
@@ -0,0 +1,7 @@
+import { TaskCallback } from 'undertaker';
+import './gulp.clean';
+import './gulp.deploy';
+import './gulp.feed';
+import './gulp.post';
+import './gulp.safelink';
+export declare function commitProject(finish: TaskCallback): void | import("bluebird")<any>;
