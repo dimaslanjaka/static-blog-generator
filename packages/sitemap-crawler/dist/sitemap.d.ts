a92d0261776c70984476bbf9f6438efec38b51cd
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-22 09:47:47 +0700

    undo modify protocol assigner

--- packages/sitemap-crawler/dist/sitemap.d.ts
+++ packages/sitemap-crawler/dist/sitemap.d.ts
@@ -7,18 +7,16 @@ export interface Opt {
      * keep query url ?key=value
      */
     keepQuery?: boolean;
-}
-export declare class SiteMapCrawlerCore {
-    static start(links: string[], core_opt: Opt, isCounting: boolean, callback: cb): void;
-    static filterLink(parent: string, href: string): string | null;
-}
-export declare const sitemapCrawler: (link: string | string[], opts?: Opt, callback?: cb) => void;
-export interface SitemapAsyncOpt extends Opt {
     /**
      * Crawl internal links [n] times
      * * **WARNING** dont put `Infinite`
      */
     deep?: number | null;
 }
-export declare function sitemapCrawlerAsync(link: string | string[], opts?: SitemapAsyncOpt): Bluebird<Record<string, string[]>>;
+export declare class SiteMapCrawlerCore {
+    static start(links: string[], core_opt: Opt, isCounting: boolean, callback: cb): void;
+    static filterLink(parent: string, href: string): string | null;
+}
+export declare const sitemapCrawler: (link: string | string[], opts?: Opt, callback?: cb) => void;
+export declare function sitemapCrawlerAsync(link: string | string[], opts?: Opt): Bluebird<Record<string, string[]>>;
 export default sitemapCrawler;
