7536e55790ff6405ea5bffc4abd38c7de15e3517
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-22 11:37:00 +0700

    add typedoc

--- packages/gulp-sbg/gulpfile.js
+++ packages/gulp-sbg/gulpfile.js
@@ -1,9 +1,31 @@
 const GulpClient = require('gulp');
 const { join } = require('upath');
+const typedocModule = require('typedoc');
+const semver = require('semver');
+const typedocOptions = require('./typedoc');
+const { default: git } = require('git-command-helper');
 
 // copy non-javascript assets from src folder
 exports.copy = function () {
   return GulpClient.src(['**/*.*'], { cwd: join(__dirname, 'src'), ignore: ['**/*.{ts,js,json}'] }).pipe(
     GulpClient.dest(join(__dirname, 'dist'))
   );
+};
+
+exports.docs = async function () {
+  const outDir = join(__dirname, 'docs');
+  const app = new typedocModule.Application();
+  if (semver.gte(typedocModule.Application.VERSION, '0.16.1')) {
+    app.options.addReader(new typedocModule.TSConfigReader());
+    app.options.addReader(new typedocModule.TypeDocReader());
+  }
+  app.bootstrap(typedocOptions);
+  const project = app.convert();
+  if (typeof project !== 'undefined') {
+    await app.generateDocs(project, join(outDir, 'gulp-sbg'));
+    await app.generateJson(project, join(outDir, 'gulp-sbg/info.json'));
+  }
+  const github = new git(outDir);
+  await github.addAndCommit('gulp-sbg');
+  await github.push();
 };
