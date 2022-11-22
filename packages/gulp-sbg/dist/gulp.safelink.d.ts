114c78c569db962ae4fde2dc1bb5cd1d50fa0bb0
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-22 19:17:08 +0700

    refresh

---
+++ packages/gulp-sbg/dist/gulp.safelink.d.ts
@@ -0,0 +1,2 @@
+import { TaskCallback } from 'undertaker';
+export declare function safelinkProcess(_done?: TaskCallback): Promise<unknown>;
---
+++ packages/gulp-sbg/test/src-posts/without-description.md
@@ -0,0 +1,6 @@
+---
+title: Normal post
+date: 2022-11-11
+---
+
+Normal post body
