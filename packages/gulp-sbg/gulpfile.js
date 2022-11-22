d9afef56e12742022957db6422ba531a951346f6
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-22 12:07:17 +0700

    update

--- packages/gulp-sbg/gulpfile.js
+++ packages/gulp-sbg/gulpfile.js
@@ -1,10 +1,6 @@
 const GulpClient = require('gulp');
 const { join } = require('upath');
-const typedocModule = require('typedoc');
-const semver = require('semver');
 const typedocOptions = require('./typedoc');
-const { default: git } = require('git-command-helper');
-const { mkdirSync, existsSync } = require('fs');
 
 // copy non-javascript assets from src folder
 const copy = function () {
@@ -13,44 +9,8 @@ const copy = function () {
   );
 };
 
-const docs = async function () {
-  const outDir = join(__dirname, 'docs');
-  if (!existsSync(join(outDir, '.git'))) mkdirSync(join(outDir, '.git'));
-
-  const github = new git(outDir);
-  try {
-    //await github.init();
-    await github.setremote('https://github.com/dimaslanjaka/docs.git').catch(noop);
-    await github.setbranch('master').catch(noop);
-    await github.reset('master').catch(noop);
-  } catch {
-    //
-  }
-
-  const app = new typedocModule.Application();
-  if (semver.gte(typedocModule.Application.VERSION, '0.16.1')) {
-    app.options.addReader(new typedocModule.TSConfigReader());
-    app.options.addReader(new typedocModule.TypeDocReader());
-  }
-  app.bootstrap(typedocOptions);
-  const project = app.convert();
-  if (typeof project !== 'undefined') {
-    await app.generateDocs(project, join(outDir, 'gulp-sbg'));
-    await app.generateJson(project, join(outDir, 'gulp-sbg/info.json'));
-  }
-
-  try {
-    await github.addAndCommit('gulp-sbg', 'update gulp-sbg docs\nat ' + new Date()).catch(noop);
-    await github.push().catch(noop);
-  } catch {
-    //
-  }
-};
+const docs = typedocOptions.run;
 
 exports.copy = copy;
 exports.docs = docs;
 exports.default = GulpClient.series(copy, docs);
-
-function noop() {
-  //
-}
