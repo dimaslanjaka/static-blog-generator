42e2c33fb4f0311140e4f9ec92a16da608c67af4
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-22 19:16:39 +0700

    refresh

--- packages/gulp-sbg/dist/gulp.sitemap.d.ts
+++
@@ -1,9 +0,0 @@
-import Bluebird from 'bluebird';
-/**
- * Sitemap Generator
- * @param url url to crawl
- * @param deep crawl deeper n times
- * @returns
- */
-export declare function generateSitemap(url?: string | null | undefined, deep?: number): Bluebird<string[]>;
-export declare function hexoGenerateSitemap(): Bluebird<unknown>;
--- packages/gulp-sbg/test/copy.test.ts
+++
@@ -1,6 +0,0 @@
-import { toUnix } from 'upath';
-process.cwd = () => toUnix(__dirname);
-
-import { copyAllPosts } from '../src';
-
-copyAllPosts();
