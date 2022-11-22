720ce4ed414838798fc3fbe82e8eda4dde139a5f
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-22 19:53:58 +0700

    hotfix build theme script

---
+++ packages/gulp-sbg/typedoc-theme-hierarchy/dist/partials/navigation.d.ts
@@ -0,0 +1,5 @@
+import { DefaultThemeRenderContext, JSX, PageEvent, Reflection } from 'typedoc';
+/**
+ * Рендерит панель навигации.
+ */
+export declare const navigation: (context: DefaultThemeRenderContext) => (props: PageEvent<Reflection>) => JSX.Element;
