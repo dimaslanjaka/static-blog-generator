eab0fe39c49705bd37c063bb84fee3983c46bcda
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-26 00:58:18 +0700

    -typedoc theme

--- packages/gulp-sbg/typedoc-theme-hierarchy/assets/js/custom.ts
+++
@@ -1,6 +0,0 @@
-import '../css/custom.css';
-import { HierarchyManager } from './HierarchyManager';
-
-const hierarchyManager = new HierarchyManager();
-
-hierarchyManager.init();
--- packages/gulp-sbg/typedoc-theme-hierarchy/dist/partials/navigation.d.ts
+++
@@ -1,5 +0,0 @@
-import { DefaultThemeRenderContext, JSX, PageEvent, Reflection } from 'typedoc';
-/**
- * Рендерит панель навигации.
- */
-export declare const navigation: (context: DefaultThemeRenderContext) => (props: PageEvent<Reflection>) => JSX.Element;
