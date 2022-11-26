c547b989870d8e9752340f582bb8005eb661bf2e
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-26 17:24:17 +0700

    add test seo

---
+++ packages/gulp-sbg/test/gulp.seo.test.ts
@@ -0,0 +1,22 @@
+import fm from 'front-matter';
+import gulp from 'gulp';
+import { join } from 'path';
+import through2 from 'through2';
+
+const publicDIR = join(__dirname, 'public');
+const postDIR = join(__dirname, 'source/_posts');
+gulp
+  .src('**/*.md', { cwd: postDIR })
+  .pipe(
+    through2.obj(function (file, _enc, next) {
+      if (file.isNull()) return next();
+      if (file.isBuffer()) {
+        const md = file.contents.toString('utf-8');
+        const parse = fm(md);
+        console.log(parse.attributes);
+        file.extname = '.html';
+      }
+      next(null, file);
+    })
+  )
+  .pipe(gulp.dest(publicDIR));
