742338f719bb20f374bc0221cf2edf47ce8e58c2
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-11-26 17:02:21 +0700

    Update dependencies and docs

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import './a_index';
 import { buildPost } from './buildPost';
 import { parsePost } from './parsePost';
 export { generatePostId } from './generatePostId';
-export { renderMarkdownIt } from './markdown/toHtml';
+export { renderMarkdownIt as renderMarkdown } from './markdown/toHtml';
 export { parsePermalink } from './parsePermalink';
 export { DeepPartial, Nullable, ParseOptions } from './parsePost';
 export { postMap } from './types/postMap';
 export { postMeta } from './types/postMeta';
 export { parsePost, buildPost };
-export const obj = { parsePost, buildPost };
+
+const obj = { parsePost, buildPost };
 
 export default obj;
