e51939603f8354f91c1f43b0fe66f1a783203288
dimaslanjaka <dimaslanjaka@gmail.com>
2022-11-26 18:32:54 +0700

    Hotfix Auto SEO

--- packages/gulp-sbg/test/gulp.seo.test.ts
+++ packages/gulp-sbg/test/gulp.seo.test.ts
@@ -1,8 +1,25 @@
-import { join } from 'path';
-import { autoSeo } from '../src/gulp.seo';
+import { toUnix } from 'upath';
+process.cwd = () => toUnix(__dirname);
+
+// stay here
+import '../src/gulp.post';
+import '../src/gulp.seo';
+// stay here
+
+import gulp from 'gulp';
 import { renderHtmlToSource } from './utils';
 
-const publicDIR = join(__dirname, 'public');
-renderHtmlToSource().once('end', function () {
-  autoSeo(publicDIR);
+import { copyToDeployDir } from './utils';
+
+gulp.series('copy-posts')(function () {
+  console.log('[copy] done');
+  renderHtmlToSource().once('end', function () {
+    copyToDeployDir().once('end', function () {
+      console.log('[copy deploy] done');
+
+      gulp.series('seo')(function () {
+        console.log('[seo] done');
+      });
+    });
+  });
 });
