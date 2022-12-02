9b3b02ed26224c8156fcaab640cf9d23b1493dd3
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-12-02 14:00:48 +0700

    update

--- build.ts
+++ build.ts
@@ -25,47 +25,33 @@ async function build() {
   });
   summon.once('close', () => {
     git({ cwd: __dirname, stdio: 'ignore' }, 'add', 'dist').then(() => {
-      git(
-        { cwd: __dirname, stdio: 'ignore' },
-        'commit',
-        '-m',
-        'build ' + new Date()
-      );
+      git({ cwd: __dirname, stdio: 'ignore' }, 'commit', '-m', 'build ' + new Date());
     });
   });
 }
 
 function updateVersion() {
   if (!process.env['GITHUB_WORKFLOW']) {
-    exec(
-      'git describe --tags --first-parent --dirty --broken',
-      { cwd: __dirname },
-      function (err, hash) {
-        getLatestCommitHash(join(__dirname, 'src')).then((result) => {
-          const srcErr = result.err;
-          const srcHash = result.hash;
-          if (!err) {
-            //console.log('Described this branch', hash);
-          }
-          if (!srcErr) {
-            //console.log('Last commit hash ./src', srcHash);
-          }
+    exec('git describe --tags --first-parent --dirty --broken', { cwd: __dirname }, function (err, hash) {
+      getLatestCommitHash(join(__dirname, 'src')).then((result) => {
+        const srcErr = result.err;
+        const srcHash = result.hash;
+        if (!err) {
+          //console.log('Described this branch', hash);
+        }
+        if (!srcErr) {
+          //console.log('Last commit hash ./src', srcHash);
+        }
 
-          if (typeof hash === 'string' && hash.length > 1) {
-            const parse = hash
-              .trim()
-              .replace(/^v|-dirty$/gim, '')
-              .split('-');
-            pkg.version = parse[0] + '-' + parse[1] + '-' + srcHash;
-            writeFile(
-              join(__dirname, 'package.json'),
-              JSON.stringify(pkg, null, 2)
-            );
-            exec(
-              'npm install && npm audit fix',
-              { cwd: __dirname },
-              async () => {
-                /*await git(
+        if (typeof hash === 'string' && hash.length > 1) {
+          const parse = hash
+            .trim()
+            .replace(/^v|-dirty$/gim, '')
+            .split('-');
+          pkg.version = parse[0] + '-' + parse[1] + '-' + srcHash;
+          writeFile(join(__dirname, 'package.json'), JSON.stringify(pkg, null, 2));
+          exec('npm install && npm audit fix', { cwd: __dirname }, async () => {
+            /*await git(
                   { cwd: __dirname, stdio: 'ignore' },
                   'add',
                   'package.json'
@@ -81,12 +67,10 @@ function updateVersion() {
                   '-m',
                   `[${srcHash}] update ${hash}`
                 );*/
-              }
-            );
-          }
-        });
-      }
-    );
+          });
+        }
+      });
+    });
   } else {
     console.log('not updating the commit hash on github workflow');
   }
@@ -100,11 +84,7 @@ function updateVersion() {
 function git(options: null | SpawnOptions = {}, ...args: string[]) {
   return new Promise(
     (
-      resolve: (args: {
-        code: number | null;
-        stdout: string | null;
-        stderr: string | null;
-      }) => any,
+      resolve: (args: { code: number | null; stdout: string | null; stderr: string | null }) => any,
       reject: (args: { args: string[]; err: Error }) => any
     ) => {
       options = Object.assign(
@@ -138,30 +118,15 @@ function git(options: null | SpawnOptions = {}, ...args: string[]) {
  * @returns
  */
 function getLatestCommitHash(path?: string) {
-  return new Bluebird(
-    (
-      resolve: (result: {
-        err: ExecException | null;
-        hash: string | null;
-      }) => any
-    ) => {
-      if (path) {
-        exec(
-          `git log --pretty=tformat:"%h" -n1 ${path}`,
-          { cwd: __dirname },
-          (err, hash) => {
-            resolve({ err, hash: String(hash).trim() });
-          }
-        );
-      } else {
-        exec(
-          `git log --pretty=tformat:"%h" -n1 .`,
-          { cwd: __dirname },
-          (err, hash) => {
-            resolve({ err, hash: String(hash).trim() });
-          }
-        );
-      }
+  return new Bluebird((resolve: (result: { err: ExecException | null; hash: string | null }) => any) => {
+    if (path) {
+      exec(`git log --pretty=tformat:"%h" -n1 ${path}`, { cwd: __dirname }, (err, hash) => {
+        resolve({ err, hash: String(hash).trim() });
+      });
+    } else {
+      exec(`git log --pretty=tformat:"%h" -n1 .`, { cwd: __dirname }, (err, hash) => {
+        resolve({ err, hash: String(hash).trim() });
+      });
     }
-  );
+  });
 }
