b6dce2884c47a34fcb20b93bd9f303319b2754be
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-03 06:54:59 +0700

    update

--- packages/gulp-sbg/dist/gulp.seo.d.ts
+++ packages/gulp-sbg/dist/gulp.seo.d.ts
@@ -1,6 +1,8 @@
 /// <reference types="node" />
+import gulp from 'gulp';
 /**
  * Auto seo runner
  * @param cwd directory to scan htmls
  */
 export declare function autoSeo(cwd: string): NodeJS.ReadWriteStream;
+export default gulp;
