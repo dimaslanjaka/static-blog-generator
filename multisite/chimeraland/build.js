88ff7841ea0e47016e02e720f62300caa0f0ec3e
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-02 16:22:11 +0700

    update chimeraland resources

--- multisite/chimeraland/build.js
+++ multisite/chimeraland/build.js
@@ -1,10 +1,11 @@
-require('ts-node').register()
-const { copyPost } = require('./project')
-const copySrcPost = require('./src/copy-src-posts').default
-const copySource = require('./src/copy-source').default
+require('ts-node').register();
+require('./src');
+const { copyPost } = require('./project');
+const copySrcPost = require('./src/copy-src-posts').default;
+const copySource = require('./src/copy-source').default;
 
 copySrcPost().on('end', function () {
   copySource().on('end', function () {
-    copyPost()
-  })
-})
+    copyPost();
+  });
+});
