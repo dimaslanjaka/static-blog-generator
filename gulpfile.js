06a972a47d2991877ca90eb48121aa7395067891
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-12-02 13:17:26 +0700

    remove `gulp-typedoc`

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-const typedoc = require('gulp-typedoc');
-const gulp = require('gulp');
-const typedocOptions = require('./typedoc-config');
-
-gulp.task('typedoc', function () {
-  return gulp.src(['src/*.ts'], { cwd: __dirname }).pipe(typedoc(typedocOptions));
-});
