f90fa23b4070f7af161f926685e8cd3c8c8b51fa
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-03 18:01:56 +0700

    update docs

--- packages/gulp-sbg/dist/api.d.ts
+++ packages/gulp-sbg/dist/api.d.ts
@@ -12,16 +12,19 @@ declare class SBG {
      */
     seo: () => NodeJS.ReadWriteStream;
     /**
-     * Copy all **src-post** to **source/_posts**
+     * Copy all **src-post** to **source/_posts** (run before generate)
      * * see the method {@link copyAllPosts}
      * @returns
      */
     copy: () => NodeJS.ReadWriteStream;
     /**
-     * Anonymize external links
+     * Anonymize external links on public dir (_config_yml.public_dir) (run after generated)
      * @returns
      */
     safelink: () => Promise<unknown>;
+    /**
+     * generate site with hexo
+     */
     generate(): Promise<void>;
 }
 export default SBG;
