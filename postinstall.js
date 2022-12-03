3db85aefd4c755a8d4a2338edd2345b4f954d340
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-03 08:22:27 +0700

    update auto update package script

--- postinstall.js
+++ postinstall.js
@@ -3,6 +3,32 @@ const fs = require('fs');
 const path = require('path/posix');
 const { spawn } = require('cross-spawn');
 
+// cache file
+const cacheJSON = path.join(__dirname, 'tmp/cache/npm-install.json');
+if (!fs.existsSync(path.dirname(cacheJSON))) {
+  fs.mkdirSync(path.dirname(cacheJSON), { recursive: true });
+}
+if (!fs.existsSync(cacheJSON)) {
+  fs.writeFileSync(cacheJSON, '{}');
+}
+/**
+ * Get cache
+ * @returns {import('./tmp/cache/npm-install.json')}
+ */
+const getCache = () => require('./tmp/cache/npm-install.json');
+
+/**
+ * Save cache
+ * @param {any} data
+ * @returns
+ * @example
+ * const data = getCache()
+ * data['key']='value';
+ * saveCache(data)
+ */
+const saveCache = (data) =>
+  fs.writeFileSync(cacheJSON, JSON.stringify(data, null, 2));
+
 (async () => {
   // @todo clear cache local packages
   const packages = [pjson.dependencies, pjson.devDependencies];
@@ -15,20 +41,35 @@ const { spawn } = require('cross-spawn');
        */
       const version = pkgs[pkgname];
       // re-installing local and monorepo package
-      if (/^((file|github):|(git|ssh)\+|http)/.test(version)) {
+      if (/^((file|github):|(git|ssh)\+|http)/i.test(version)) {
         const isYarn = fs.existsSync(path.join(__dirname, 'yarn.lock'));
         //const arg = [version, isDev ? '-D' : ''].filter((str) => str.trim().length > 0);
+
         if (isYarn) {
           // yarn upgrade package
           await summon('yarn', ['upgrade'].concat(pkgname), {
             cwd: __dirname,
             stdio: 'inherit'
+          }).finally(function () {
+            // save to cache
+            const data = getCache();
+            data[pkgname] = Object.assign(data[pkgname] || {}, {
+              lastInstall: new Date().getTime()
+            });
+            saveCache(data);
           });
         } else {
           // npm update package
           await summon('npm', ['update'].concat(pkgname), {
             cwd: __dirname,
             stdio: 'inherit'
+          }).finally(function () {
+            // save to cache
+            const data = getCache();
+            data[pkgname] = Object.assign(data[pkgname] || {}, {
+              lastInstall: new Date().getTime()
+            });
+            saveCache(data);
           });
         }
       }
@@ -46,7 +87,8 @@ const { spawn } = require('cross-spawn');
 function summon(cmd, args = [], opt = {}) {
   // *** Return the promise
   return new Promise(function (resolve, reject) {
-    if (typeof cmd !== 'string' || cmd.trim().length === 0) return reject('cmd empty');
+    if (typeof cmd !== 'string' || cmd.trim().length === 0)
+      return reject('cmd empty');
     const process = spawn(cmd, args, opt);
     process.on('close', function (code) {
       // Should probably be 'exit', not 'close'
