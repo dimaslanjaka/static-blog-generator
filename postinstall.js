f1528cbf34f16faf06e3022c9538c96f15c2b4ee
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-12-05 12:16:43 +0700

    Update postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -35,8 +35,7 @@ const getCache = () => require('./node_modules/.cache/npm-install.json');
  * data['key']='value';
  * saveCache(data)
  */
-const saveCache = (data) =>
-  fs.writeFileSync(cacheJSON, JSON.stringify(data, null, 2));
+const saveCache = (data) => fs.writeFileSync(cacheJSON, JSON.stringify(data, null, 2));
 
 (async () => {
   // @todo clear cache local packages
@@ -67,17 +66,24 @@ const saveCache = (data) =>
 
   const isYarn = fs.existsSync(path.join(__dirname, 'yarn.lock'));
 
+  /**
+   * Internal update cache
+   * @returns {Promise<ReturnType<typeof getCache>>}
+   */
   const updateCache = () => {
-    // save to cache
-    const data = getCache();
-    for (let i = 0; i < toUpdate.length; i++) {
-      const pkgname = toUpdate[i];
-      data[pkgname] = Object.assign(data[pkgname] || {}, {
-        lastInstall: new Date().getTime()
-      });
-    }
+    return new Promise((resolve) => {
+      // save to cache
+      const data = getCache();
+      for (let i = 0; i < toUpdate.length; i++) {
+        const pkgname = toUpdate[i];
+        data[pkgname] = Object.assign(data[pkgname] || {}, {
+          lastInstall: new Date().getTime()
+        });
+      }
 
-    saveCache(data);
+      saveCache(data);
+      resolve(data);
+    });
   };
 
   /**
@@ -88,27 +94,45 @@ const saveCache = (data) =>
     const exists = toUpdate.map(
       (pkgname) =>
         fs.existsSync(path.join(__dirname, 'node_modules', pkgname)) &&
-        fs.existsSync(
-          path.join(__dirname, 'node_modules', pkgname, 'package.json')
-        )
+        fs.existsSync(path.join(__dirname, 'node_modules', pkgname, 'package.json'))
     );
     //console.log({ exists });
     return exists.every((exist) => exist === true);
   };
 
   if (checkNodeModules()) {
-    if (isYarn) {
-      // yarn upgrade package
-      await summon('yarn', ['upgrade'].concat(...toUpdate), {
-        cwd: __dirname,
-        stdio: 'inherit'
-      }).finally(updateCache);
-    } else {
-      // npm update package
-      await summon('npm', ['update'].concat(...toUpdate), {
-        cwd: __dirname,
-        stdio: 'inherit'
-      }).finally(updateCache);
+    try {
+      if (isYarn) {
+        // yarn upgrade package
+        await summon('yarn', ['upgrade'].concat(...toUpdate), {
+          cwd: __dirname,
+          stdio: 'inherit'
+        });
+      } else {
+        // npm update package
+        await summon('npm', ['update'].concat(...toUpdate), {
+          cwd: __dirname,
+          stdio: 'inherit'
+        });
+      }
+
+      // update cache
+      await updateCache();
+
+      const argv = process.argv;
+      if (fs.existsSync(path.join(__dirname, '.git')) && argv.includes('--commit')) {
+        await summon('git', ['add', 'package.json'], { cwd: __dirname });
+        await summon('git', ['add', 'package-lock.json'], { cwd: __dirname });
+        const status = await summon('git', ['status', '--porcelain'], { cwd: __dirname });
+        console.log({ status });
+        if (status.stdout && (status.stdout.includes('package.json') || status.stdout.includes('package-lock.json'))) {
+          await summon('git', ['commit', '-m', 'Update dependencies\nDate: ' + new Date()], {
+            cwd: __dirname
+          });
+        }
+      }
+    } catch (e) {
+      if (e instanceof Error) console.error(e.message);
     }
   } else {
     console.log('some packages already deleted from node_modules');
@@ -120,22 +144,55 @@ const saveCache = (data) =>
  * @param {string} cmd
  * @param {string[]} args
  * @param {Parameters<typeof spawn>[2]} opt
- * @returns
+ * @returns {Promise<Error|{stdout:string,stderr:string}>}
  */
 function summon(cmd, args = [], opt = {}) {
+  const spawnopt = Object.assign({ cwd: __dirname }, opt || {});
   // *** Return the promise
-  return new Promise(function (resolve, reject) {
-    if (typeof cmd !== 'string' || cmd.trim().length === 0)
-      return reject('cmd empty');
-    const process = spawn(cmd, args, opt);
-    process.on('close', function (code) {
-      // Should probably be 'exit', not 'close'
-      // *** Process completed
-      resolve(code);
+  return new Promise(function (resolve) {
+    if (typeof cmd !== 'string' || cmd.trim().length === 0) return resolve(new Error('cmd empty'));
+    let stdout = '';
+    let stderr = '';
+    const child = spawn(cmd, args, spawnopt);
+    // if (spawnopt.stdio === 'ignore') child.unref();
+
+    if (child.stdout && 'on' in child.stdout) {
+      child.stdout.setEncoding('utf8');
+      child.stdout.on('data', (data) => {
+        stdout += data;
+      });
+    }
+
+    if (child.stderr && 'on' in child.stdout) {
+      child.stderr.setEncoding('utf8');
+      child.stderr.on('data', (data) => {
+        stderr += data;
+      });
+    }
+
+    // silence errors
+    child.on('error', (err) => {
+      console.log('got error', err);
     });
-    process.on('error', function (err) {
+
+    child.on('close', function (code) {
+      // Should probably be 'exit', not 'close'
+      if (code !== 0) console.log('[ERROR]', cmd, ...args, 'dies with code', code);
+      // *** Process completed
+      resolve({ stdout, stderr });
+    });
+    child.on('error', function (err) {
       // *** Process creation failed
-      reject(err);
+      resolve(err);
     });
   });
+}
+
+/**
+ * No Operation
+ * @param  {...any} _
+ * @returns
+ */
+function _noop(..._) {
+  return;
 }
