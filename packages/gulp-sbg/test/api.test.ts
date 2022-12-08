9bc6a72925afa835dc1705c5e69c518c52f6f6d1
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-08 22:12:16 +0700

    [api] fix clean method

--- packages/gulp-sbg/test/api.test.ts
+++ packages/gulp-sbg/test/api.test.ts
@@ -1,11 +1,17 @@
-import { toUnix } from 'upath';
-import { Application } from '../src';
-const cwd = toUnix(__dirname);
-process.cwd = () => cwd;
+/* eslint-disable @typescript-eslint/no-var-requires */
+process.cwd = () => require('upath').toUnix(__dirname);
+
+import { existsSync } from 'fs';
+import { join } from 'upath';
+import { Application, ProjectConfig } from '../src';
 
 (async function () {
-  const app = new Application(cwd);
+  const app = new Application(process.cwd());
   await app.clean();
+  console.log('tmp', !existsSync(join(__dirname, 'tmp')));
+  console.log('source/_posts', !existsSync(join(__dirname, 'source/_posts')));
+  if (!ProjectConfig.public_dir) throw new Error('public_dir not settled in _config.yml');
+  console.log('public_dir', !existsSync(join(__dirname, ProjectConfig.public_dir || 'public')));
   //await app.copy();
   // await c.generate();
 })();
