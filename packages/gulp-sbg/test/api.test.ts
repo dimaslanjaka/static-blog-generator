a8a26ce0845a08f2dba67770fa1b6ff558cf7e08
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-08 22:42:04 +0700

    [pending] fix deploy method

--- packages/gulp-sbg/test/api.test.ts
+++ packages/gulp-sbg/test/api.test.ts
@@ -8,10 +8,10 @@ import { Application, ProjectConfig } from '../src';
 (async function () {
   const app = new Application(process.cwd());
   await app.clean();
-  console.log('tmp', !existsSync(join(__dirname, 'tmp')));
-  console.log('source/_posts', !existsSync(join(__dirname, 'source/_posts')));
+  console.log('[test][clean] tmp', !existsSync(join(__dirname, 'tmp')));
+  console.log('[test][clean] source/_posts', !existsSync(join(__dirname, 'source/_posts')));
   if (!ProjectConfig.public_dir) throw new Error('public_dir not settled in _config.yml');
-  console.log('public_dir', !existsSync(join(__dirname, ProjectConfig.public_dir || 'public')));
-  //await app.copy();
-  // await c.generate();
+  console.log('[test][clean] public_dir', !existsSync(join(__dirname, ProjectConfig.public_dir || 'public')));
+  await app.copy();
+  await app.generate();
 })();
