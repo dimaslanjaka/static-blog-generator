77a9e07aaca72c9c64e88a1ad7cd00e467dfedbb
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-12-09 16:27:16 +0700

    Update packer.js

--- packages/gulp-sbg/packer.js
+++ packages/gulp-sbg/packer.js
@@ -5,6 +5,7 @@ const { existsSync, renameSync, rmSync, mkdirpSync, writeFileSync } = require("f
 const GulpClient = require("gulp");
 const { join, dirname } = require("upath");
 const packagejson = require("./package.json");
+const crypto = require("crypto");
 
 // auto create tarball (tgz) on release folder
 // raw: https://raw.githubusercontent.com/dimaslanjaka/static-blog-generator-hexo/master/packages/gulp-sbg/packer.js
@@ -48,8 +49,6 @@ child.on("exit", function () {
         readDir.push(pkglock);
       }
       readDir.forEach((file, index, all) => {
-        const crypto = require("crypto");
-
         const sha1 = (path) =>
           new Promise((resolve, reject) => {
             const hash = crypto.createHash("sha1");
