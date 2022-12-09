24d2267bcf4dbe9b71e6a4df263b9da75c649281
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-12-09 17:09:15 +0700

    Update packer.js

--- packages/gulp-sbg/packer.js
+++ packages/gulp-sbg/packer.js
@@ -8,10 +8,11 @@ const packagejson = require("./package.json");
 const crypto = require("crypto");
 
 // auto create tarball (tgz) on release folder
-// raw: https://raw.githubusercontent.com/dimaslanjaka/static-blog-generator-hexo/master/packages/gulp-sbg/packer.js
-// github: https://github.com/dimaslanjaka/static-blog-generator-hexo/blob/master/packages/gulp-sbg/packer.js
-// update: curl https://raw.githubusercontent.com/dimaslanjaka/static-blog-generator-hexo/master/packages/gulp-sbg/packer.js > packer.js
-// usage: node packer.js
+// raw            : https://raw.githubusercontent.com/dimaslanjaka/static-blog-generator-hexo/master/packages/gulp-sbg/packer.js
+// github         : https://github.com/dimaslanjaka/static-blog-generator-hexo/blob/master/packages/gulp-sbg/packer.js
+// update         : curl https://raw.githubusercontent.com/dimaslanjaka/static-blog-generator-hexo/master/packages/gulp-sbg/packer.js > packer.js
+// usage          : node packer.js
+// github actions : https://github.com/dimaslanjaka/nodejs-package-types/blob/main/.github/workflows/build-release.yml
 
 console.log("=".repeat(19));
 console.log("= packing started =");
