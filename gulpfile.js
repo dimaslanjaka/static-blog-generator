39faef6d5c197d425bd70de764ae82d2932c791c
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-10 08:16:25 +0700

    Update deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
-const sbg = require('static-blog-generator');
+//const sbg = require('static-blog-generator');
+const sbg = require('./packages/gulp-sbg');
 
 exports.sbg = sbg;
 exports = gulp;
