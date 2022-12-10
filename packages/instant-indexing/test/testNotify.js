b74e9c781ad4e4accdcebf8a3e1fd67a65666f91
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-10 17:52:28 +0700

    move static-blog-generator to their own branch

--- packages/instant-indexing/test/testNotify.js
+++
@@ -1,8 +0,0 @@
-const Notifier = require('../src/Notifier')
-const serviceConfig = require('../src/config')
-
-const url = 'https://www.webmanajemen.com/chimeraland/blacklist-player.html'
-
-const notify = new Notifier(serviceConfig)
-notify.single(url, 'update')
-//console.log(serviceConfig)
