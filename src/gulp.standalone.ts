82dc59b7bd6f06a35190df745ca9c4245d5ae3ec
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-13 02:06:24 +0700

    update

--- src/gulp.standalone.ts
+++ src/gulp.standalone.ts
@@ -6,15 +6,23 @@ import ProjectConfig from './gulp.config';
 import { replacePath } from './utils/string';
 
 function standaloneRunner() {
+  console.log('[standalone] Running scripts...');
   gulp.src(join(ProjectConfig.cwd, '**/_*.standalone.js')).pipe(
-    through2.obj(function (file, _enc, next) {
-      console.log(`node ${replacePath(file.path, ProjectConfig.cwd, '')}`);
-      const child = spawn('node', [file.path], { stdio: 'inherit' });
-      child.on('close', () => {
-        // drop file
-        next();
-      });
-    })
+    through2
+      .obj(async function (file, _enc, next) {
+        console.log('='.repeat(10) + ' input ' + '='.repeat(10));
+        console.log(`node ${await replacePath(file.path, ProjectConfig.cwd, '')}`);
+        console.log('='.repeat(10) + ' ouput ' + '='.repeat(10));
+        const child = spawn('node', [file.path], { stdio: 'inherit' });
+        child.on('close', () => {
+          // drop file
+          next();
+        });
+      })
+      .pipe(gulp.dest(join(ProjectConfig.cwd, 'tmp/standalone')))
+      .once('end', function () {
+        console.log('[standalone] stopped');
+      })
   );
 }
 
