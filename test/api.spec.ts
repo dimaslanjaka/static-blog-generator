27934574bbe87b1c552c59734559a9cbd8ad2dc1
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-20 12:06:53 +0700

    [application] Update test unit

--- test/api.spec.ts
+++ test/api.spec.ts
@@ -4,7 +4,7 @@ process.cwd = () => __dirname;
 import { describe, expect, jest, test } from '@jest/globals';
 import { existsSync } from 'fs';
 import { join, toUnix } from 'upath';
-import { Application, getConfig } from '../src';
+import { Application, getConfig } from '../dist';
 
 jest.setTimeout(10000);
 
@@ -16,17 +16,27 @@ describe('API', function () {
     expect(toUnix(process.cwd())).toBe(toUnix(__dirname));
   });
 
-  test('Clean auto generated files', async function () {
-    await app.clean();
-    expect(existsSync(join(__dirname, 'tmp'))).toBe(false);
-    expect(existsSync(join(__dirname, 'source/_posts'))).toBe(false);
-    expect(existsSync(join(__dirname, getConfig().public_dir || 'public'))).toBe(false);
+  test('Clean auto generated files', function (done) {
+    app.clean().then(() => {
+      expect(existsSync(join(__dirname, 'tmp'))).toBe(false);
+      expect(existsSync(join(__dirname, 'source/_posts'))).toBe(false);
+      expect(existsSync(join(__dirname, getConfig().public_dir || 'public'))).toBe(false);
+      done();
+    });
   });
 
-  test('Copy source posts', async function () {
-    expect.assertions(1);
-    await app.copy();
-    expect(existsSync(join(__dirname, 'source'))).toBe(true);
+  test('Run standalone', function (done) {
+    app.standalone().once('end', function () {
+      done();
+    });
+  });
+
+  test('Copy source posts', function (done) {
+    // expect.assertions(1);
+    app.copy().then(() => {
+      expect(existsSync(join(__dirname, 'source/_posts'))).toBe(true);
+      done();
+    });
   });
 
   //await app.generate();
