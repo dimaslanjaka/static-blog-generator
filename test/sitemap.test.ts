67ad5517792910c9b380b5512e502baeedd0c461
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-20 22:02:19 +0700

    migrate test

--- test/sitemap.test.ts
+++
@@ -1,6 +0,0 @@
-process.cwd = () => __dirname;
-
-import { copyAllPosts, hexoGenerateSitemap } from '../src';
-import { chain } from '../src/utils/chain';
-
-chain([{ callback: copyAllPosts }, { callback: hexoGenerateSitemap }]);
