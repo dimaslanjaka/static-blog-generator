25aa1561df6ae4b9a355163c97cd5efb8bfccb6d
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-20 11:54:28 +0700

    [chain] trigger after option

--- test/copy.test.ts
+++ test/copy.test.ts
@@ -1,18 +1,19 @@
 import { toUnix } from 'upath';
 process.cwd = () => toUnix(__dirname);
 
-import { Application, copyAllPosts } from '../src';
+import { Application, cleanDb, copyAllPosts } from '../src';
 import { chain } from '../src/utils/chain';
 
 chain([
-  { callback: copyAllPosts, opt: { before: () => console.log('[direct]') } },
   {
-    callback: function () {
-      const app = new Application(__dirname);
-      app.copy();
-    },
+    callback: copyAllPosts,
+    opt: { before: () => console.log('='.repeat(16) + '[ DIRECT ]' + '='.repeat(16)), after: () => cleanDb() }
+  },
+  {
+    callback: () => new Application(__dirname).copy(),
     opt: {
-      before: () => console.log('[api]')
+      before: () => console.log('='.repeat(20) + '[ API ]' + '='.repeat(20)),
+      after: () => new Application(__dirname).clean()
     }
   }
 ]);
