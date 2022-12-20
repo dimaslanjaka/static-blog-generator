5200a6392d23579ecc9bdc6f1c76db60fb65a9aa
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-20 13:25:39 +0700

    fix for platform-specific new line

--- dist/utils/fm.d.ts
+++ dist/utils/fm.d.ts
@@ -1,9 +1,12 @@
 /// <reference types="node" />
 import { MakeDirectoryOptions } from 'fs';
+export interface writefileOpt extends MakeDirectoryOptions {
+    append?: boolean;
+}
 /**
  * write to file recursively
  * @param file
  * @param content
  * @param opt
  */
-export declare function writefile(file: string, content: string, opt?: MakeDirectoryOptions): void;
+export declare function writefile(file: string, content: string, opt?: writefileOpt): void;
--- dist/utils/fm.js
+++ dist/utils/fm.js
@@ -14,6 +14,9 @@ function writefile(file, content, opt) {
     if (opt === void 0) { opt = {}; }
     if (!(0, fs_1.existsSync)((0, path_1.dirname)(file)))
         (0, fs_1.mkdirSync)((0, path_1.dirname)(file), Object.assign({ recursive: true }, opt));
+    if (opt.append) {
+        return (0, fs_1.appendFileSync)(file, content);
+    }
     (0, fs_1.writeFileSync)(file, content);
 }
 exports.writefile = writefile;
