7df34c3aa10b6ecbc621cdec2f226962657f40a4
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-21 09:10:29 +0700

    Update build manual

---
+++ dist/defaults.js
@@ -0,0 +1,27 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getDefaultConfigYaml = exports.getDefaultConfig = void 0;
+var fs_1 = require("fs");
+var upath_1 = require("upath");
+var yaml_1 = __importDefault(require("yaml"));
+/**
+ * get default configuration
+ * @returns
+ */
+function getDefaultConfig() {
+    var defaultConfig = { post_dir: 'src-posts', cwd: (0, upath_1.toUnix)(process.cwd()) };
+    var configYML = yaml_1.default.parse((0, fs_1.readFileSync)((0, upath_1.join)(__dirname, '_config.yml'), 'utf-8'));
+    return Object.assign(defaultConfig, configYML);
+}
+exports.getDefaultConfig = getDefaultConfig;
+/**
+ * get default _config.yml
+ * @returns
+ */
+function getDefaultConfigYaml() {
+    return (0, fs_1.readFileSync)((0, upath_1.join)(__dirname, '_config.yml'), 'utf-8');
+}
+exports.getDefaultConfigYaml = getDefaultConfigYaml;
