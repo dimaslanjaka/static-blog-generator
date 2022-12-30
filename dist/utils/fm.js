d3e217b7f09f241f8ab65834a8ba2380f5b7a396
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-30 15:12:24 +0700

    update deps

--- dist/utils/fm.js
+++ dist/utils/fm.js
@@ -7,9 +7,19 @@ function writefile(file, content, opt) {
     if (opt === void 0) { opt = {}; }
     if (!(0, fs_1.existsSync)((0, path_1.dirname)(file)))
         (0, fs_1.mkdirSync)((0, path_1.dirname)(file), Object.assign({ recursive: true }, opt));
+    var result = {
+        file: file,
+        append: false
+    };
     if (opt.append) {
-        return (0, fs_1.appendFileSync)(file, content);
+        result.append = true;
+        (0, fs_1.appendFileSync)(file, content);
     }
-    (0, fs_1.writeFileSync)(file, content);
+    else {
+        (0, fs_1.writeFileSync)(file, content);
+    }
+    if (opt.async)
+        return Promise.resolve(result);
+    return result;
 }
 exports.writefile = writefile;
