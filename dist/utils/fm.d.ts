f935538b2a36ec5fd19b7b41bca26aead18ca25b
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-30 15:12:24 +0700

    update deps

--- dist/utils/fm.d.ts
+++ dist/utils/fm.d.ts
@@ -2,5 +2,13 @@
 import { MakeDirectoryOptions } from 'fs';
 export interface writefileOpt extends MakeDirectoryOptions {
     append?: boolean;
+    async?: boolean | true;
 }
-export declare function writefile(file: string, content: string, opt?: writefileOpt): void;
+export interface writefileResult {
+    file: string;
+    append: boolean;
+}
+export declare function writefile(file: string, content: string): writefileResult;
+export declare function writefile(file: string, content: string, opt: {
+    async: true;
+} & writefileOpt): Promise<writefileResult>;
