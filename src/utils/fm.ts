77dc2e2f5ce7adada9deb732402951184d14fe96
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-30 15:04:31 +0700

    add sync and async options

--- src/utils/fm.ts
+++ src/utils/fm.ts
@@ -5,18 +5,57 @@ import { dirname } from 'path';
 
 export interface writefileOpt extends MakeDirectoryOptions {
   append?: boolean;
+  async?: boolean;
+}
+
+export interface writefileResult {
+  file: string;
+  append: boolean;
 }
 
 /**
- * write to file recursively (auto create dirname)
+ * sync write to file recursively (auto create dirname)
+ * @param file
+ * @param content
+ */
+export function writefile(file: string, content: string): writefileResult;
+/**
+ * async write to file recursively (auto create dirname)
+ * @param file
+ * @param content
+ * @param opt
+ */
+export function writefile(
+  file: string,
+  content: string,
+  opt: { async?: true } & writefileOpt
+): Promise<writefileResult>;
+/**
+ * sync write to file recursively (auto create dirname)
+ * @param file
+ * @param content
+ * @param opt
+ */
+export function writefile(file: string, content: string, opt: { async?: false } & writefileOpt): writefileResult;
+/**
+ * sync write to file recursively (auto create dirname)
  * @param file
  * @param content
  * @param opt
  */
 export function writefile(file: string, content: string, opt: writefileOpt = {}) {
+  // create dirname when not exist
   if (!existsSync(dirname(file))) mkdirSync(dirname(file), Object.assign({ recursive: true }, opt));
+  const result = {
+    file,
+    append: false
+  };
   if (opt.append) {
-    return appendFileSync(file, content);
+    result.append = true;
+    appendFileSync(file, content);
+  } else {
+    writeFileSync(file, content);
   }
-  writeFileSync(file, content);
+  if (opt.async) return Promise.resolve(result);
+  return result;
 }
