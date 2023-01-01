0da9cca757db02a38076fe34e04938d8b6fb4899
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-01 17:56:30 +0700

    add lock file while copying posts

--- dist/gulp.post.d.ts
+++ dist/gulp.post.d.ts
@@ -1,7 +1,8 @@
 /// <reference types="node" />
 /// <reference types="node" />
+/// <reference types="node" />
 import gulp from 'gulp';
 export declare function watchPost(done: gulp.TaskFunctionCallback): void;
 export declare const copySinglePost: (identifier: string, callback?: ((...args: any[]) => any) | undefined) => void;
 export declare function updatePost(): import("stream").Transform;
-export declare function copyAllPosts(): NodeJS.ReadWriteStream;
+export declare function copyAllPosts(): NodeJS.ReadWriteStream | import("fs").WriteStream;
