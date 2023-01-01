5977b7324f75718e1d70499c685933c3f4c8eca2
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-02 05:50:00 +0700

    unresolved gulp.src post

--- dist/gulp-utils/gulp.debug.js
+++ dist/gulp-utils/gulp.debug.js
@@ -1,15 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.gulpDebug = void 0;
 var tslib_1 = require("tslib");
+var ansi_colors_1 = tslib_1.__importDefault(require("ansi-colors"));
 var through2_1 = tslib_1.__importDefault(require("through2"));
+var upath_1 = require("upath");
 var logger_1 = tslib_1.__importDefault(require("../utils/logger"));
 function gulpDebug() {
     return through2_1.default.obj(function (file, _enc, cb) {
-        logger_1.default.log(file.path);
+        logger_1.default.log(ansi_colors_1.default.yellowBright('gulp-debug'), (0, upath_1.toUnix)(file.path.replace(process.cwd(), '')));
         if (typeof this.push === 'function')
             this.push(file);
         cb(null, file);
     });
 }
-exports.gulpDebug = gulpDebug;
+exports.default = gulpDebug;
