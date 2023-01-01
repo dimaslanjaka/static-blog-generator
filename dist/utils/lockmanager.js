75a4be4441e3af13536f96a6cd42c09944e9e982
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-02 05:02:43 +0700

    unsolved lock manager

--- dist/utils/lockmanager.js
+++ dist/utils/lockmanager.js
@@ -2,12 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var tslib_1 = require("tslib");
 var fs_1 = require("fs");
+var os_1 = require("os");
 var path_1 = tslib_1.__importDefault(require("path"));
 var fm_1 = require("./fm");
 var LockManager = (function () {
     function LockManager(name) {
-        this.folder = path_1.default.join(process.cwd(), 'tmp/static-blog-generator/lock-' + process.env.CACHE_NAME || 'default');
-        this.file = path_1.default.join(this.folder, name, 'index.lock');
+        this.folder = path_1.default.join(process.cwd(), 'tmp/cache/lock');
+        this.file = path_1.default.join(this.folder, name, (0, os_1.platform)() + 'index.lock');
     }
     LockManager.prototype.lock = function () {
         return (0, fm_1.writefile)(this.file, '');
