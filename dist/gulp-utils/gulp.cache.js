e7e7622c93ce6f00b7c4b14590639131c42ef68a
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-02 19:14:46 +0700

    update: build

--- dist/gulp-utils/gulp.cache.js
+++ dist/gulp-utils/gulp.cache.js
@@ -1,27 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -29,18 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.gulpCached = exports.md5 = exports.getShaFile = void 0;
 var ansi_colors_1 = __importDefault(require("ansi-colors"));
 var crypto_1 = __importDefault(require("crypto"));
-var fs_1 = __importStar(require("fs"));
+var fs_extra_1 = __importDefault(require("fs-extra"));
 var os_1 = require("os");
 var persistent_cache_1 = require("persistent-cache");
 var through2_1 = __importDefault(require("through2"));
 var upath_1 = require("upath");
 var gulp_config_1 = require("../gulp.config");
 var fm_1 = require("../utils/fm");
+var hash_1 = require("../utils/hash");
 var scheduler_1 = __importDefault(require("../utils/scheduler"));
 function getShaFile(file) {
-    if (fs_1.default.statSync(file).isDirectory())
+    if (fs_extra_1.default.statSync(file).isDirectory())
         return null;
-    var testFile = fs_1.default.readFileSync(file);
+    var testFile = fs_extra_1.default.readFileSync(file);
     var sha1sum = crypto_1.default.createHash('sha1').update(testFile).digest('hex');
     return sha1sum;
 }
@@ -48,10 +26,13 @@ exports.getShaFile = getShaFile;
 var md5 = function (data) { return crypto_1.default.createHash('md5').update(data).digest('hex'); };
 exports.md5 = md5;
 function gulpCached(options) {
+    var _a;
     if (options === void 0) { options = {}; }
     var config = (0, gulp_config_1.getConfig)();
     options = Object.assign({ name: 'gulp-cached', base: (0, upath_1.join)(config.cwd, 'tmp'), prefix: '' }, options);
     var caches = (0, persistent_cache_1.persistentCache)(options);
+    var caller = (0, hash_1.data_to_hash_sync)('md5', ((_a = new Error('get caller').stack) === null || _a === void 0 ? void 0 : _a.split(/\r?\n/gim).filter(function (str) { return /(dist|src)/i.test(str); })[1]) || '').slice(0, 5);
+    var pid = process.pid;
     return through2_1.default.obj(function (file, _enc, next) {
         var _a, _b;
         if (file.isDirectory())
@@ -67,14 +48,17 @@ function gulpCached(options) {
         };
         if (options.dest && options.cwd) {
             var destPath = (0, upath_1.join)((0, upath_1.toUnix)(options.dest), (0, upath_1.toUnix)(file.path).replace((0, upath_1.toUnix)(options.cwd), ''));
-            if (!(0, fs_1.existsSync)(destPath))
+            if (!fs_extra_1.default.existsSync(destPath))
                 isCached = false;
         }
-        var dumpfile = (0, upath_1.join)(process.cwd(), 'tmp/dump/gulp-cache.txt');
-        (0, fm_1.writefile)(dumpfile, "".concat(paths.source, " is cached ").concat(isCached, " with dest validation ").concat(options.dest && options.cwd) + os_1.EOL, {
+        var dumpfile = (0, upath_1.join)(process.cwd(), 'tmp/dump/gulp-cache', "".concat(caller, "-").concat(pid, ".log"));
+        (0, fm_1.writefile)(dumpfile, "\"".concat(paths.source, "\" is cached ").concat(isCached, " with dest validation ").concat(options.dest && options.cwd ? 'true' : 'false') +
+            os_1.EOL, {
             append: true
         });
-        scheduler_1.default.add('dump gulp-cache', function () { return console.log(ansi_colors_1.default.yellowBright('gulp-cache'), dumpfile); });
+        scheduler_1.default.add("dump gulp-cache ".concat(caller, " ").concat(pid), function () {
+            return console.log(ansi_colors_1.default.yellowBright('gulp-cache'), dumpfile);
+        });
         if (!isCached) {
             caches.setSync(cacheKey, sha1sum);
             if (typeof this.push === 'function')
