197fa4d6390436733e674ef2d3038c5121094ecf
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-02 13:49:39 +0700

    clean all locks before process exit

--- src/utils/lockmanager.ts
+++ src/utils/lockmanager.ts
@@ -2,12 +2,16 @@ import { existsSync, rmSync } from 'fs';
 import { platform } from 'os';
 import path from 'path';
 import { writefile } from './fm';
+import scheduler from './scheduler';
+
+const locks: LockManager[] = [];
 
 export default class LockManager {
   folder = path.join(process.cwd(), 'tmp/cache/lock');
   file: string;
   constructor(name: string) {
     this.file = path.join(this.folder, name, platform() + 'index.lock');
+    locks.push(this);
   }
 
   lock() {
@@ -22,3 +26,7 @@ export default class LockManager {
     return existsSync(this.file);
   }
 }
+
+scheduler.add('clean locks', () => {
+  locks.forEach((lock) => lock.release());
+});
