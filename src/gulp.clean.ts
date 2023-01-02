fba44f97e56f6c9119ea3fed8fcb34c49b72c9b1
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-02 18:02:54 +0700

    progress: fix clean archives

--- src/gulp.clean.ts
+++ src/gulp.clean.ts
@@ -1,7 +1,8 @@
 import Bluebird from 'bluebird';
-import { existsSync, rm, RmOptions } from 'fs-extra';
+import { existsSync, readdir, rm, RmOptions } from 'fs-extra';
 import gulp from 'gulp';
 import hexoLib from 'hexo';
+import { EOL } from 'os';
 import { join } from 'upath';
 import { inspect } from 'util';
 import { getConfig } from './gulp.config';
@@ -74,8 +75,10 @@ gulp.task('clean', cleanDb);
 /**
  * clean old archives (categories, tags, pagination)
  */
-export function cleanOldArchives(done?: gulp.TaskFunctionCallback) {
+export async function cleanOldArchives(done?: gulp.TaskFunctionCallback) {
   const config = getConfig();
+
+  // clean archives, tags, categories
   const archives = join(config.deploy.deployDir, config.archive_dir);
   const categories = join(config.deploy.deployDir, config.category_dir);
   const tags = join(config.deploy.deployDir, config.tag_dir);
@@ -89,7 +92,25 @@ export function cleanOldArchives(done?: gulp.TaskFunctionCallback) {
     folders.push(join(config.deploy.deployDir, String(config.language)));
   }
 
+  // delete pagination
+  const pagesDir = join(config.deploy.deployDir, 'page');
+  if (existsSync(pagesDir)) {
+    const pages = (await readdir(pagesDir)).filter((str) => /^\d+$/.test(str)).map((str) => join(pagesDir, str));
+    folders.push(...pages);
+  }
+
+  const hexo = new hexoLib(config.base_dir);
+  await hexo.init().catch(noop);
+  await hexo.load().catch(noop);
+  const count = hexo.locals.get('posts').count();
+  console.log(count);
+
   const promises: Promise<any>[] = [];
+
+  // dump to file
+  const dumpfile = join(process.cwd(), 'tmp/clean/dump.txt');
+  writefile(dumpfile, folders.join(EOL));
+  Logger.log('list deleted files', dumpfile);
 
   for (let i = 0; i < folders.length; i++) {
     const pathStr = folders[i];
@@ -100,14 +121,17 @@ export function cleanOldArchives(done?: gulp.TaskFunctionCallback) {
     }
   }
 
-  const finishNow = function () {
+  const finishNow = function (e: any) {
+    if (e instanceof Error) {
+      console.log('clean archives has some erros');
+    }
     if (typeof done === 'function') done();
     return undefined;
   };
 
-  return Bluebird.all(promises).then(finishNow).catch(finishNow);
+  await Bluebird.all(promises).then(finishNow).catch(finishNow);
 }
 
 gulp.task('clean-archives', cleanOldArchives);
-
+gulp.task('clean-archive', cleanOldArchives);
 gulp.task('clean-all', gulp.series('clean', 'clean-archives'));
