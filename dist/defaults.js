00a0e1a25c2c8bbe718d9224a9c28887a0023a2d
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-04 03:34:06 +0700

    add hexo default config
    import from https://github.com/hexojs/hexo/blob/master/lib/hexo/default_config.js

--- dist/defaults.js
+++ dist/defaults.js
@@ -8,9 +8,76 @@ var fs_1 = require("fs");
 var upath_1 = require("upath");
 var yaml_1 = __importDefault(require("yaml"));
 function getDefaultConfig() {
+    var hexoDefaultConfig = {
+        title: 'Hexo',
+        subtitle: '',
+        description: '',
+        author: 'John Doe',
+        language: 'en',
+        timezone: '',
+        url: 'http://example.com',
+        root: '/',
+        permalink: ':year/:month/:day/:title/',
+        permalink_defaults: {},
+        pretty_urls: {
+            trailing_index: true,
+            trailing_html: true
+        },
+        source_dir: 'source',
+        public_dir: 'public',
+        tag_dir: 'tags',
+        archive_dir: 'archives',
+        category_dir: 'categories',
+        code_dir: 'downloads/code',
+        i18n_dir: ':lang',
+        skip_render: [],
+        new_post_name: ':title.md',
+        default_layout: 'post',
+        titlecase: false,
+        external_link: {
+            enable: true,
+            field: 'site',
+            exclude: ''
+        },
+        filename_case: 0,
+        render_drafts: false,
+        post_asset_folder: false,
+        relative_link: false,
+        future: true,
+        syntax_highlighter: 'highlight.js',
+        highlight: {
+            auto_detect: false,
+            line_number: true,
+            tab_replace: '',
+            wrap: true,
+            exclude_languages: [],
+            language_attr: false,
+            hljs: false
+        },
+        prismjs: {
+            preprocess: true,
+            line_number: true,
+            tab_replace: ''
+        },
+        default_category: 'uncategorized',
+        category_map: {},
+        tag_map: {},
+        date_format: 'YYYY-MM-DD',
+        time_format: 'HH:mm:ss',
+        updated_option: 'mtime',
+        per_page: 10,
+        pagination_dir: 'page',
+        theme: 'landscape',
+        server: {
+            cache: false
+        },
+        deploy: {},
+        ignore: [],
+        meta_generator: true
+    };
     var defaultConfig = { post_dir: 'src-posts', cwd: (0, upath_1.toUnix)(process.cwd()) };
     var configYML = yaml_1.default.parse(getDefaultConfigYaml());
-    return Object.assign(defaultConfig, configYML);
+    return Object.assign(hexoDefaultConfig, defaultConfig, configYML);
 }
 exports.getDefaultConfig = getDefaultConfig;
 function getDefaultConfigYaml() {
