08dc0725a766ab43208dc12b00b0497a0e3b188f
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-04 16:52:34 +0700

    add instance getter and setter

--- dist/utils/fm.d.ts
+++ dist/utils/fm.d.ts
@@ -1,3 +1,4 @@
+/// <reference types="node" />
 import fs, { MakeDirectoryOptions } from 'fs-extra';
 export interface writefileOpt extends MakeDirectoryOptions {
     append?: boolean;
@@ -14,6 +15,6 @@ export declare function writefile(file: string, content: string, opt: {
 export declare function writefile(file: string, content: string, opt: {
     async?: false;
 } & writefileOpt): writefileResult;
-export declare function createWriteStream(dest: string, options?: Parameters<typeof fs['createWriteStream']>[1]): any;
+export declare function createWriteStream(dest: string, options?: Parameters<typeof fs['createWriteStream']>[1]): fs.WriteStream;
 export declare const isAsset: (path: any) => boolean;
 export declare const isMarkdown: (path: any) => boolean;
