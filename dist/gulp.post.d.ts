67a60d729bb9763e4a861b4fd4b0724b394f9d8e
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-04 16:17:03 +0700

    remove stream from updatePost method

--- dist/gulp.post.d.ts
+++ dist/gulp.post.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="node" />
 /// <reference types="node" />
-/// <reference types="node" />
 export declare function copySinglePost(identifier: string, callback?: (...args: any[]) => any): void;
-export declare function updatePost(): import("stream").Transform;
-export declare function copyAllPosts(): any;
+export declare function updatePost(postPath: string): Promise<void>;
+export declare function copyAllPosts(): import("fs").WriteStream | NodeJS.ReadWriteStream;
