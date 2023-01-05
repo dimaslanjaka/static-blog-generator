33afc688fe0854fd2651b68e1ce4cdf776226581
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-05 18:29:15 +0700

    update temp folder

--- src/generator.ts
+++ src/generator.ts
@@ -6,19 +6,20 @@ import './gulp/tasks/generate';
 import { dbFolder } from './node/cache';
 import { join, rm } from './node/filemanager';
 import scheduler from './node/scheduler';
-import config, { root, tmp } from './types/_config';
+import config from './types/_config';
 
 // register scheduler
 new scheduler();
 
 /** clean generated folder */
 const clean_public = (done?: TaskCallback) =>
-  rm(join(root, config.public_dir), { recursive: true }, done);
+  rm(join(config.root, config.public_dir), { recursive: true }, done);
 /** clean posts from config.source_dir */
 const clean_posts = (done?: TaskCallback) =>
-  rm(join(root, config.source_dir, '_posts'), { recursive: true }, done);
+  rm(join(config.root, config.source_dir, '_posts'), { recursive: true }, done);
 /** clean temp folder */
-const clean_tmp = (done?: TaskCallback) => rm(tmp(), { recursive: true }, done);
+const clean_tmp = (done?: TaskCallback) =>
+  rm(join(process.cwd(), 'tmp'), { recursive: true }, done);
 /** clean database folder */
 const clean_db = (done?: TaskCallback) =>
   rm(dbFolder, { recursive: true }, done);
