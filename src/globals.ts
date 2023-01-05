4b52a7c5bbc3dfc9ff8b53340908e6002a6364a4
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-05 20:17:54 +0700

    add types

--- src/globals.ts
+++ src/globals.ts
@@ -8,3 +8,8 @@ export type Writeable<T> = { -readonly [P in keyof T]: T[P] };
  * * https://stackoverflow.com/a/43001581
  */
 export type DeepWriteable<T> = { -readonly [P in keyof T]: DeepWriteable<T[P]> };
+
+/**
+ * null | undefined | type
+ */
+export type Nullable<T> = T | null | undefined;
