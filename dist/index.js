72255b1d809c4d012a05c1b372137e331d8fb095
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-05 17:54:57 +0700

    build Thu Jan 05 2023 17:54:57 GMT+0700 (Western Indonesia Time)

--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,13 @@ const toHtml_1 = require("./markdown/toHtml");
 const parsePermalink_1 = require("./parsePermalink");
 const parsePost_1 = require("./parsePost");
 Object.defineProperty(exports, "parsePost", { enumerable: true, get: function () { return parsePost_1.parsePost; } });
-const hexoPostParser = { parsePost: parsePost_1.parsePost, buildPost: buildPost_1.buildPost, generatePostId: generatePostId_1.generatePostId, renderMarkdown: toHtml_1.renderMarkdownIt, parsePermalink: parsePermalink_1.parsePermalink };
+const hexoPostParser = {
+    parsePost: parsePost_1.parsePost,
+    buildPost: buildPost_1.buildPost,
+    generatePostId: generatePostId_1.generatePostId,
+    renderMarkdown: toHtml_1.renderMarkdownIt,
+    parsePermalink: parsePermalink_1.parsePermalink
+};
 exports.default = hexoPostParser;
 var generatePostId_2 = require("./generatePostId");
 Object.defineProperty(exports, "generatePostId", { enumerable: true, get: function () { return generatePostId_2.generatePostId; } });
