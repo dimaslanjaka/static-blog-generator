3129581def189be2fa650747f63ff3a5456ea386
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-07 02:56:18 +0700

    build Sat Jan 07 2023 02:56:18 GMT+0700 (Western Indonesia Time)

--- dist/types/_config.js
+++ dist/types/_config.js
@@ -102,23 +102,27 @@ let defaultSiteOptions = {
         amp: false
     }
 };
+let fetched = false;
 function getConfig() {
-    // find _config.yml
-    const file = (0, upath_1.join)(process.cwd(), '_config.yml');
-    // console.log('finding', file);
-    if ((0, fs_1.existsSync)(file)) {
-        const readConfig = (0, fs_1.readFileSync)(file, 'utf-8');
-        const parse = yaml_1.default.parse(readConfig);
-        defaultSiteOptions = (0, deepmerge_ts_1.deepmerge)(defaultSiteOptions, parse, {
-            verbose,
-            generator: {
-                cache: !nocache
-            }
-        });
-        //console.log(defaultSiteOptions.url);
-    }
-    else {
-        console.log(file, 'not found');
+    if (!fetched) {
+        fetched = true;
+        // find _config.yml
+        const file = (0, upath_1.join)(process.cwd(), '_config.yml');
+        // console.log('finding', file);
+        if ((0, fs_1.existsSync)(file)) {
+            const readConfig = (0, fs_1.readFileSync)(file, 'utf-8');
+            const parse = yaml_1.default.parse(readConfig);
+            defaultSiteOptions = (0, deepmerge_ts_1.deepmerge)(defaultSiteOptions, parse, {
+                verbose,
+                generator: {
+                    cache: !nocache
+                }
+            });
+            //console.log(defaultSiteOptions.url);
+        }
+        else {
+            console.log(file, 'not found');
+        }
     }
     return defaultSiteOptions;
 }
