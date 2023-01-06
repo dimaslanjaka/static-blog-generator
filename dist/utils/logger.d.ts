9d8ed4634139986f0f92097816309a4548bdd5cd
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-07 01:01:56 +0700

    update logger docs

--- dist/utils/logger.d.ts
+++ dist/utils/logger.d.ts
@@ -2,6 +2,7 @@ import 'nodejs-package-types';
 declare class Logger {
     static log(...args: any[]): void;
     static info(...args: any[]): void;
+    static error(...args: any[]): void;
     private static tracer;
 }
 export default Logger;
