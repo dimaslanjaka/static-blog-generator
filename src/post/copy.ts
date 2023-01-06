408a70043a62008dd25da1ae2a013b8a1c1a56cc
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-06 21:08:18 +0700

    separate process function

--- src/post/copy.ts
+++ src/post/copy.ts
@@ -9,7 +9,6 @@ import { commonIgnore, getConfig } from '../gulp.config';
 import * as fm from '../utils/fm';
 import LockManager from '../utils/lockmanager';
 import Logger from '../utils/logger';
-import scheduler from '../utils/scheduler';
 
 const sourcePostDir = join(process.cwd(), getConfig().post_dir);
 const generatedPostDir = join(process.cwd(), getConfig().source_dir, '_posts');
@@ -37,7 +36,10 @@ export function copySinglePost(identifier: string, callback?: (...args: any[]) =
  * copy all posts from src-posts to source/_posts
  * @returns
  */
-export function copyAllPosts() {
+export function copyAllPosts(
+  callback?: ((...args: any[]) => any) | null | undefined,
+  config?: ReturnType<typeof getConfig>
+) {
   // lock runner
   const lm = new LockManager(copyAllPosts.name);
   const logname = 'post:' + ansiColors.grey('copy');
@@ -53,13 +55,14 @@ export function copyAllPosts() {
   lm.lock();
 
   // function start
-  const config = getConfig();
+  if (!config) config = getConfig();
   const excludes: string[] = Array.isArray(config.exclude) ? config.exclude : [];
   excludes.push(...commonIgnore);
   Logger.log(logname, 'cwd', toUnix(process.cwd()));
   Logger.log(logname, 'copying source posts from', sourcePostDir);
+  Logger.log(logname, excludes);
 
-  const streamer = gulp.src('**/*.*', { cwd: toUnix(sourcePostDir), ignore: excludes });
+  const streamer = gulp.src(['**/*.*', '*.*'], { cwd: toUnix(sourcePostDir), ignore: excludes, dot: true });
   streamer.once('end', () => {
     // delete lock file
     lm.release();
@@ -69,111 +72,105 @@ export function copyAllPosts() {
   if (config.generator?.cache) streamer.pipe(gulpCached({ name: 'post-copy' }));
   // apply verbose
   const verbose = config.generator?.verbose;
-  const writeVerbose = (msg: string) => {
-    const write = fm.writefile(join(process.cwd(), 'tmp/logs/post-copy', process.pid + '.log'), msg, {
-      append: true
-    });
-    scheduler.add('verbose', () => console.log(write.file));
-  };
-  if (verbose) streamer.pipe(gulpDebug());
+  if (verbose) streamer.pipe(gulpDebug('post-copy'));
+
   // transform start
-  streamer
-    .pipe(
-      through2.obj(async (file, _enc, callback) => {
-        if (file.isNull()) {
-          if (verbose) writeVerbose(file.path + ' is null');
-          return callback();
-        }
-        if (file.isStream()) {
-          if (verbose) writeVerbose(file.path + ' is stream');
-          return callback();
-        }
-        // process markdown files
-        if (file.extname === '.md') {
-          const contents = file.contents?.toString() || '';
-          // drop empty body
-          if (contents.trim().length === 0) return callback();
-          const parse = await parsePost(contents, {
-            shortcodes: {
-              youtube: true,
-              css: true,
-              include: true,
-              link: true,
-              now: true,
-              script: true,
-              text: true,
-              codeblock: true
-            },
-            cache: false,
-            config: <any>config,
-            formatDate: true,
-            fix: true,
-            sourceFile: file.path
-          });
-          if (parse && parse.metadata) {
-            // process tags and categories
-            const array = ['tags', 'categories'];
-            for (let i = 0; i < array.length; i++) {
-              const groupLabel = array[i];
-              if (parse.metadata[groupLabel]) {
-                // label assign
-                if (config[groupLabel]?.assign) {
-                  for (const oldLabel in config[groupLabel].assign) {
-                    const index = parse.metadata[groupLabel].findIndex((str: string) => str == oldLabel);
+  streamer.pipe(processPost(config)).pipe(gulp.dest(generatedPostDir));
 
-                    if (index !== -1) {
-                      parse.metadata[groupLabel] = config[groupLabel].assign[oldLabel];
-                    }
-                  }
-                }
-                // label mapper
-                if (config[groupLabel]?.mapper) {
-                  for (const oldLabel in config[groupLabel].mapper) {
-                    const index = parse.metadata[groupLabel].findIndex((str: string) => str == oldLabel);
+  return streamer.once('end', () => {
+    typeof callback == 'function' && callback();
+  });
+}
 
-                    if (index !== -1) {
-                      parse.metadata[groupLabel][index] = config[groupLabel].mapper[oldLabel];
-                    }
+export function processPost(config: ReturnType<typeof getConfig>) {
+  const logname = processPost.name;
+
+  return through2.obj(async (file, _enc, callback) => {
+    if (file.isNull()) {
+      if (config.generator.verbose) Logger.log(file.path + ' is null');
+      return callback();
+    }
+    if (file.isStream()) {
+      if (config.generator.verbose) Logger.log(file.path + ' is stream');
+      return callback();
+    }
+    if (config) {
+      // process markdown files
+      if (file.extname === '.md') {
+        const contents = file.contents?.toString() || '';
+        // drop empty body
+        if (contents.trim().length === 0) return;
+        const parse = await parsePost(contents, {
+          shortcodes: {
+            youtube: true,
+            css: true,
+            include: true,
+            link: true,
+            now: true,
+            script: true,
+            text: true,
+            codeblock: true
+          },
+          cache: false,
+          config: <any>config,
+          formatDate: true,
+          fix: true,
+          sourceFile: file.path
+        });
+        if (parse && parse.metadata) {
+          // process tags and categories
+          const array = ['tags', 'categories'];
+          for (let i = 0; i < array.length; i++) {
+            const groupLabel = array[i];
+            if (parse.metadata[groupLabel]) {
+              // label assign
+              if (config[groupLabel]?.assign) {
+                for (const oldLabel in config[groupLabel].assign) {
+                  const index = parse.metadata[groupLabel].findIndex((str: string) => str == oldLabel);
+
+                  if (index !== -1) {
+                    parse.metadata[groupLabel] = config[groupLabel].assign[oldLabel];
                   }
-                }
-                // label lowercase
-                if (config.tags?.lowercase) {
-                  parse.metadata.tags = parse.metadata.tags?.map((str) => str.toLowerCase()) || [];
                 }
               }
-            }
+              // label mapper
+              if (config[groupLabel]?.mapper) {
+                for (const oldLabel in config[groupLabel].mapper) {
+                  const index = parse.metadata[groupLabel].findIndex((str: string) => str == oldLabel);
 
-            console.log(parse.metadata.permalink);
-
-            if (verbose) {
-              fm.writefile(join(process.cwd(), 'tmp/dump.json'), parse);
+                  if (index !== -1) {
+                    parse.metadata[groupLabel][index] = config[groupLabel].mapper[oldLabel];
+                  }
+                }
+              }
+              // label lowercase
+              if (config.tags?.lowercase) {
+                parse.metadata.tags = parse.metadata.tags?.map((str) => str.toLowerCase()) || [];
+              }
             }
-
-            const build = buildPost(parse);
-            if (typeof build === 'string') {
-              file.contents = Buffer.from(build);
-              return callback(null, file);
-            } else {
-              Logger.log(logname, 'cannot rebuild', toUnix(file.path).replace(toUnix(process.cwd()), ''));
-              return callback();
-            }
-          } else {
-            Logger.log(logname, 'cannot parse', toUnix(file.path).replace(toUnix(process.cwd()), ''));
           }
-        }
-        callback(null, file);
-      })
-    )
-    .pipe(gulp.dest(generatedPostDir));
 
-  return streamer;
+          console.log(parse.metadata.permalink);
+
+          if (config.verbose) {
+            fm.writefile(join(process.cwd(), 'tmp/dump.json'), parse);
+          }
+
+          const build = buildPost(parse);
+          if (typeof build === 'string') {
+            file.contents = Buffer.from(build);
+          } else {
+            Logger.log(logname, 'cannot rebuild', toUnix(file.path).replace(toUnix(process.cwd()), ''));
+          }
+        } else {
+          Logger.log(logname, 'cannot parse', toUnix(file.path).replace(toUnix(process.cwd()), ''));
+        }
+      }
+      callback(null, file);
+    } else {
+      callback();
+    }
+  });
 }
 
 gulp.task('post:copy', copyAllPosts);
-gulp.task('copy-all-post', gulp.series('post:copy'));
-gulp.task('copy-all-posts', gulp.series('post:copy'));
-gulp.task('copy-posts', gulp.series('post:copy'));
-gulp.task('copy-post', gulp.series('post:copy'));
-
-//gulp.task('watch-post', watchPost);
-//gulp.task('watch-posts', watchPost);
