e3d7e66781320dc2cd31de5e831fc2deda171bc0
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-07 01:03:21 +0700

    remove dist from repository

--- dist/packages/hexo-post-parser/src/node/escape-string-regexp.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function escapeStringRegexp(string) {
-    if (typeof string !== 'string') {
-        throw new TypeError('Expected a string');
-    }
-    return string.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&').replace(/-/g, '\\x2d');
-}
-exports.default = escapeStringRegexp;
--- dist/packages/hexo-post-parser/src/utils/string.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function removeDoubleSlashes(str: string): string;
-export declare function countWords(str: string): number;
