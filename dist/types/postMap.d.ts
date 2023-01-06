2d687e34134a4ab4e3bd03d410a63c6e99dfe5a5
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-07 02:20:01 +0700

    build Sat Jan 07 2023 02:20:01 GMT+0700 (Western Indonesia Time)

--- dist/types/postMap.d.ts
+++ dist/types/postMap.d.ts
@@ -1,6 +1,6 @@
 import { DeepPartial } from '../globals';
 import { postMeta } from './postMeta';
-import config from './_config';
+import { getConfig } from './_config';
 export interface postMap extends Object {
     [key: string]: any;
     /**
@@ -20,7 +20,7 @@ export interface postMap extends Object {
     /**
      * _config.yml
      */
-    config?: DeepPartial<typeof config> | null;
+    config?: DeepPartial<typeof getConfig> | null;
     /**
      * Article metadata
      */
