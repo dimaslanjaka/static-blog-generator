7d030fe4fb7515fe1cee8aaa7b19663547ed7ae5
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-07 21:40:26 +0700

    add logname

--- src/gulp-utils/gulp.debug.ts
+++ src/gulp-utils/gulp.debug.ts
@@ -37,9 +37,9 @@ export default function gulpDebug(filename?: string) {
  * log all files
  * @returns
  */
-export function gulpLog() {
+export function gulpLog(logname = '') {
   return through2.obj(function (file, _enc, cb) {
-    Logger.log(ansiColors.yellowBright('gulp-log'), toUnix(file.path.replace(process.cwd(), '')));
+    Logger.log(ansiColors.yellowBright('gulp-log'), logname, toUnix(file.path.replace(process.cwd(), '')));
     if (typeof this.push === 'function') this.push(file);
     cb(null, file);
   });
