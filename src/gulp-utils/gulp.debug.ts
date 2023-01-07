7161a2b1619e3ea7e3163062c8cf6322dbb59330
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-07 21:51:53 +0700

    log content length

--- src/gulp-utils/gulp.debug.ts
+++ src/gulp-utils/gulp.debug.ts
@@ -39,7 +39,12 @@ export default function gulpDebug(filename?: string) {
  */
 export function gulpLog(logname = '') {
   return through2.obj(function (file, _enc, cb) {
-    Logger.log(ansiColors.yellowBright('gulp-log'), logname, toUnix(file.path.replace(process.cwd(), '')));
+    Logger.log(
+      ansiColors.yellowBright('gulp-log'),
+      logname,
+      toUnix(file.path.replace(process.cwd(), '')),
+      String(file.contents).length
+    );
     if (typeof this.push === 'function') this.push(file);
     cb(null, file);
   });
