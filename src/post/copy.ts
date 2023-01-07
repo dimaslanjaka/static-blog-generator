1a8677933c2d232eaff7725d0f3c9416365f6fba
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-07 15:14:31 +0700

    hotfix: copy post not resolved

--- src/post/copy.ts
+++ src/post/copy.ts
@@ -2,12 +2,7 @@ import ansiColors from 'ansi-colors';
 import gulp from 'gulp';
 import through2 from 'through2';
 import { extname, join, toUnix } from 'upath';
-import { Application } from '..';
-import gulpCached from '../gulp-utils/gulp.cache';
-import gulpDebug from '../gulp-utils/gulp.debug';
-import { commonIgnore, getConfig } from '../gulp.config';
-import * as fm from '../utils/fm';
-import LockManager from '../utils/lockmanager';
+import { getConfig } from '../gulp.config';
 import Logger from '../utils/logger';
 
 //
@@ -16,9 +11,6 @@ import { buildPost, parsePost } from 'hexo-post-parser';
 import debug from '../utils/debug';
 //
 
-const sourcePostDir = join(process.cwd(), getConfig().post_dir);
-const generatedPostDir = join(process.cwd(), getConfig().source_dir, '_posts');
-
 /**
  * Copy single post from src-posts folder to source/_posts
  * @param identifier
@@ -26,6 +18,9 @@ const generatedPostDir = join(process.cwd(), getConfig().source_dir, '_posts');
  */
 export function copySinglePost(identifier: string, callback?: (...args: any[]) => any) {
   identifier = identifier.replace(extname(identifier), '');
+
+  const sourcePostDir = join(process.cwd(), getConfig().post_dir);
+  const generatedPostDir = join(process.cwd(), getConfig().source_dir, '_posts');
   ///const fileList = [];
   gulp
     .src(['**/*' + identifier + '*/*', '**/*' + identifier + '*'], {
@@ -42,53 +37,20 @@ export function copySinglePost(identifier: string, callback?: (...args: any[]) =
  * copy all posts from src-posts to source/_posts
  * @returns
  */
-export function copyAllPosts(callback?: ((...args: any[]) => any) | null | undefined) {
-  // lock runner
-  const lm = new LockManager(copyAllPosts.name);
-  const logname = 'post:' + ansiColors.grey('copy');
-  // skip process when found
-  if (lm.exist()) {
-    Logger.log('another process still running');
-    const writeStream = fm.createWriteStream(join(lm.folder, 'dummy.txt'), { flags: 'a' });
-    writeStream.write(String(new Date()));
-    writeStream.close();
-    return writeStream;
-  }
-  // write new lock
-  lm.lock();
-
-  // function start
-  const api = new Application(process.cwd());
-  const config = api.getConfig();
-  const excludes: string[] = Array.isArray(config.exclude) ? config.exclude : [];
-  excludes.push(...commonIgnore);
-  Logger.log(logname, 'cwd', toUnix(process.cwd()));
-  Logger.log(logname, 'copying source posts from', sourcePostDir);
-  if (config.generator.verbose) Logger.log(logname, { excludes });
-
-  const streamer = gulp.src(['**/*.*', '*.*', '**/*'], {
-    cwd: toUnix(sourcePostDir),
-    ignore: excludes,
-    dot: true,
-    noext: true
-  });
-  streamer.once('end', () => {
-    // delete lock file
-    lm.release();
-  });
-
-  // apply cache
-  if (config.generator?.cache) streamer.pipe(gulpCached({ name: 'post-copy' }));
-  // apply verbose
-  const verbose = config.generator?.verbose;
-  if (verbose) streamer.pipe(gulpDebug('post-copy'));
-
-  // transform start
-  streamer.pipe(processPost(config)).pipe(gulp.dest(generatedPostDir));
-
-  return streamer.once('end', () => {
-    typeof callback == 'function' && callback();
-  });
+export function copyAllPosts() {
+  const config = getConfig();
+  const { excludes } = config;
+  const sourcePostDir = join(process.cwd(), getConfig().post_dir);
+  const generatedPostDir = join(process.cwd(), getConfig().source_dir, '_posts');
+  return gulp
+    .src(['**/*.*', '*.*', '**/*'], {
+      cwd: toUnix(sourcePostDir),
+      ignore: excludes,
+      dot: true,
+      noext: true
+    })
+    .pipe(processPost(config))
+    .pipe(gulp.dest(generatedPostDir));
 }
 
 /**
@@ -104,14 +66,23 @@ export function processPost(config: ReturnType<typeof getConfig>) {
 
   const log = debug('post');
 
-  return through2.obj((file, _enc, callback) => {
+  return through2.obj(function (file, _enc, callback) {
+    const self = this;
+    const success = function (file: import('vinyl')) {
+      if (self.push) self.push(file);
+      callback();
+    };
+    const drop = function () {
+      callback();
+    };
+
     if (file.isNull()) {
       if (config.generator.verbose) Logger.log(file.path + ' is null');
-      return callback();
+      return drop();
     }
     if (file.isStream()) {
       if (config.generator.verbose) Logger.log(file.path + ' is stream');
-      return callback();
+      return drop();
     }
 
     if (config) {
@@ -127,7 +98,7 @@ export function processPost(config: ReturnType<typeof getConfig>) {
           if (config.generator.verbose) {
             Logger.log('content empty', ansiColors.redBright(toUnix(file.path.replace(process.cwd(), ''))));
           }
-          return callback();
+          return drop();
         }
 
         parsePost(file.path, {
@@ -147,7 +118,7 @@ export function processPost(config: ReturnType<typeof getConfig>) {
           fix: true,
           sourceFile: file.path
         })
-          .then((parse) => {
+          .then(function (parse) {
             if (parse && parse.metadata) {
               // process tags and categories
               const array = ['tags', 'categories'];
@@ -208,9 +179,10 @@ export function processPost(config: ReturnType<typeof getConfig>) {
               const build = buildPost(parse);
               if (typeof build === 'string') {
                 file.contents = Buffer.from(build);
-                if (config.generator.verbose)
+                if (config.generator.verbose) {
                   Logger.log(logname, 'success', toUnix(file.path).replace(toUnix(process.cwd()), ''));
-                return callback(null, file);
+                }
+                success(file);
               } else {
                 Logger.log(logname, 'cannot rebuild', toUnix(file.path).replace(toUnix(process.cwd()), ''));
               }
@@ -220,11 +192,11 @@ export function processPost(config: ReturnType<typeof getConfig>) {
           })
           .catch((e) => Logger.log(e));
       } else {
-        callback(null, file);
+        success(file);
       }
     } else {
       Logger.log('options not configured');
-      callback();
+      drop();
     }
   });
 }
