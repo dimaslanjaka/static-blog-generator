453f450bbae5ec3a713b4e1457a1a4051ef9587b
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-10 01:59:01 +0700

    disable permalink parser

---
+++ dist/utils/index.d.ts
@@ -0,0 +1,7 @@
+export * as array from './array';
+export * as chain from './chain';
+export * as debug from './debug';
+export * as filemanager from './fm';
+export * as logger from './logger';
+export * as object from './object';
+export * as stream from './stream';
