5351d91d921d1b58e9a6911ba7b5de65bb06892f
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-10 01:18:35 +0700

    include dist

---
+++ dist/gulp.safelink.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="node" />
+/// <reference types="node" />
+import gulp from 'gulp';
+export declare function taskSafelink(_done?: gulp.TaskFunctionCallback | null | undefined, cwd?: undefined | null | string): import("fs").WriteStream | NodeJS.ReadWriteStream;
