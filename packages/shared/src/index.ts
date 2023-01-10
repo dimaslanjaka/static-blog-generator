a9b06a4a70744b73e33a280d2f3899b8436d8c6c
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-10 23:47:12 +0700

    add sitemap

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,8 +2,8 @@ import { config } from './config';
 import * as gutils from './gulp-utils';
 import * as utils from './utils';
 
+export { utils, config, gutils };
 export const Logger = utils.logger;
 
-export { utils, config, gutils };
 const sbgUtils = Object.assign({}, utils, config);
 export default sbgUtils;
--- packages/shared/src/utils/scheduler.ts
+++ packages/shared/src/utils/scheduler.ts
@@ -166,3 +166,4 @@ class scheduler {
 }
 
 export default scheduler;
+export { scheduler };
