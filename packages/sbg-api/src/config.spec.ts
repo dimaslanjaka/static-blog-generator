fd284774308a198e556eae18f6c5d47dcff2f751
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-11 01:29:13 +0700

    import api from old branch

---
+++ packages/sbg-api/src/config.spec.ts
@@ -0,0 +1,1 @@
+process.cwd = () => __dirname;
--- packages/sbg-utility/src/index.ts
+++ packages/sbg-utility/src/index.ts
@@ -1,9 +1,18 @@
 import { config } from './config';
+import * as globals from './globals';
 import * as gutils from './gulp-utils';
 import * as utils from './utils';
 
 export { utils, config, gutils };
-export const Logger = utils.logger;
 
-const sbgUtils = Object.assign({}, utils, config);
+const sbgUtils = {
+  ...utils,
+  ...config,
+  ...globals,
+  ...gutils,
+  ...utils.logger,
+  ...utils.array,
+  ...utils.string,
+  ...utils.chain
+};
 export default sbgUtils;
