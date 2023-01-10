f0657b9396b14f1b731e3a51012d722f49d21759
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-10 21:21:37 +0700

    init shared library

---
+++ packages/shared/src/utils/stream.ts
@@ -0,0 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+import { PassThrough } from 'stream';
+
+export function createDuplexStream() {
+  const readStream = fs.createReadStream(path.join(process.cwd(), `tmp/streams/read-${process.pid}.txt`));
+  const writeStream = fs.createWriteStream(path.join(process.cwd(), `tmp/streams/write-${process.pid}.txt`));
+  const tunnel = new PassThrough(); // simply...
+
+  //tunnel.pipe(writeStream, { end: false }); // pipe without closing the target stream on end
+  //closingFunction(tunnel); // this function closes the pipe for me
+
+  console.log(tunnel.writable); // false: the pipe was closed
+  console.log(writeStream.writable); // true: the stream is still open
+  return readStream.pipe(tunnel).pipe(writeStream);
+}
