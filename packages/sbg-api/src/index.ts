048dd3db5aa0ab6e6fb7ca3a43f90cc8bb2849a3
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-11 03:13:05 +0700

    add main module

---
+++ packages/sbg-api/src/index.ts
@@ -0,0 +1,8 @@
+export * as clean from './clean';
+export * as feed from './feed';
+export * as safelink from './gulp.safelink';
+export * as seo from './gulp.seo';
+export * as post from './post';
+export * as sitemap from './sitemap';
+
+//
