46a31bd934211abe69cbf86807af90e0b8dd0ebc
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-10 23:13:49 +0700

    excluding src from monorepo

--- src/utils/jest.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Check if current runner is JEST
- * * source: {@link https://stackoverflow.com/a/52231746}
- * @returns
- */
-export function areWeTestingWithJest() {
-  return process.env.JEST_WORKER_ID !== undefined;
-}
