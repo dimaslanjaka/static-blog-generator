6bc3215a545b6e261f783d72280ce1193fa14205
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-12 03:32:36 +0700

    change and register post route

--- packages/sbg-server/src/post/index.ts
+++ packages/sbg-server/src/post/index.ts
@@ -1,16 +1,13 @@
 import express from 'express';
 import glob from 'glob';
+import * as apis from 'sbg-api';
 import serveIndex from 'serve-index';
 import path from 'upath';
-import serverConfig from '../config';
 
-export default async function routePost() {
-  process.cwd = () => config.root;
-  const config = serverConfig.get();
+export default function routePost(api: apis.Application) {
   const routePost = express.Router();
-  const apis = await import('sbg-api');
-  const api = new apis.Application(config.root);
   const POST_ROOT = path.join(api.cwd, api.config.post_dir);
+  console.log('root<post>', POST_ROOT);
   // list all posts
   routePost.use('/debug', express.static(POST_ROOT), serveIndex(POST_ROOT, { icons: true }));
   routePost.use('/list', function (_req, res) {
