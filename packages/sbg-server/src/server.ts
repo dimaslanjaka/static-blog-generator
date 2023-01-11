27afc5fe3a2de301e67628f902b9e814c4980b4a
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-12 03:30:29 +0700

    add start method

--- packages/sbg-server/src/server.ts
+++ packages/sbg-server/src/server.ts
@@ -2,35 +2,56 @@ import cookieParser from 'cookie-parser';
 import express from 'express';
 import { Express } from 'express-serve-static-core';
 import nunjucks from 'nunjucks';
+import * as apis from 'sbg-api';
 import path from 'upath';
 import serverConfig from './config';
 
 export default interface SBGServer {
   config: {
     root: string;
+    port: number;
   };
 }
 
 export default class SBGServer {
   server: Express;
   env: nunjucks.Environment;
-  constructor(options?: SBGServer['config']) {
+  api: apis.Application;
+  constructor(options?: Partial<SBGServer['config']>) {
+    // init express
     this.server = express();
-    const app = this.server;
+    // update config
     serverConfig.update(options || {});
+    // get updated config
     const config = serverConfig.get();
+    // start api
+    this.api = new apis.Application(config.root);
+    // init nunjuck environment
     this.env = nunjucks.configure(path.join(__dirname, 'views'), {
       noCache: true,
       autoescape: true,
-      express: app,
+      express: this.server,
       web: { useCache: true, async: true }
     });
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-    app.use(cookieParser());
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.use(express.static(path.join(config.root, 'public')));
-    app.use(express.static(path.join(config.root, 'node_modules')));
+    // init default express static
+    this.server.use(express.json());
+    this.server.use(express.urlencoded({ extended: false }));
+    this.server.use(cookieParser());
+    this.server.use(express.static(path.join(__dirname, 'public')));
+    this.server.use(express.static(path.join(config.root, 'public')));
+    this.server.use(express.static(path.join(config.root, 'node_modules')));
   }
+  /**
+   * get the configured server
+   * @returns express server instance
+   */
   get = () => this.server;
+  /**
+   * start server
+   */
+  start() {
+    this.server.listen(this.config.port, function () {
+      console.log('http://localhost:' + this.config.port);
+    });
+  }
 }
