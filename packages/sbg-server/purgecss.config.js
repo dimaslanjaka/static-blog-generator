d5ee5cb387006ff7df70fbbedbc70621213bd5b1
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-12 20:40:05 +0700

    tailwind using postcss-import method

--- packages/sbg-server/purgecss.config.js
+++ packages/sbg-server/purgecss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: ['src/views/**/*.njk', 'src/public/**/*.js'],
-  css: ['src/public/app.css'],
+  css: ['src/public/css/app.css'],
   extractors: [
     {
       extractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
