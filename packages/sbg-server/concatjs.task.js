87765288c9ed9831a09efc027801e389b2b88fbb
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-12 23:23:03 +0700

    update project compiler

--- packages/sbg-server/concatjs.task.js
+++ packages/sbg-server/concatjs.task.js
@@ -7,27 +7,33 @@ const path = require('upath');
 const fs = require('fs-extra');
 
 const dest = path.join(__dirname, 'src/public');
-if (fs.existsSync(dest)) fs.emptyDirSync(dest);
+if (!fs.existsSync(dest + '/css/app.css')) {
+  startCompile();
+}
 
-gulp
-  .src('./source/scripts/**/*.js', { cwd: __dirname, ignore: ['**/*.min.js'] })
-  .pipe(concat('app.js'))
-  .pipe(sourcemaps.init())
-  .pipe(
-    terser(
-      {
-        keep_fnames: true,
-        mangle: false
-      },
-      terserlib.minify
+function startCompile() {
+  if (fs.existsSync(dest)) fs.emptyDirSync(dest);
+
+  gulp
+    .src('./source/scripts/**/*.js', { cwd: __dirname, ignore: ['**/*.min.js'] })
+    .pipe(concat('app.js'))
+    .pipe(sourcemaps.init())
+    .pipe(
+      terser(
+        {
+          keep_fnames: true,
+          mangle: false
+        },
+        terserlib.minify
+      )
     )
-  )
-  .pipe(sourcemaps.write('./'))
-  .pipe(gulp.dest(dest + '/js'));
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(dest + '/js'));
 
-// copy font-awesome assets
-gulp
-  .src('./source/styles/fontawesome/**/*.{woff,woff2,eot,svg,otf}', {
-    cwd: __dirname
-  })
-  .pipe(gulp.dest(dest));
+  // copy font-awesome assets
+  gulp
+    .src('./source/styles/fontawesome/**/*.{woff,woff2,eot,svg,otf}', {
+      cwd: __dirname
+    })
+    .pipe(gulp.dest(dest));
+}
