0058f16a7e7b5ff7b101af8361059c5445334731
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-14 02:26:18 +0700

    show list post as default `/post`

--- packages/sbg-server/src/post/index.ts
+++ packages/sbg-server/src/post/index.ts
@@ -14,31 +14,51 @@ export default function routePost(api: apis.Application) {
   const POST_ROOT = path.join(api.cwd, api.config.post_dir);
   log('root<post>', POST_ROOT);
   // list all posts
-  router.get('/debug', express.static(POST_ROOT), serveIndex(POST_ROOT, { icons: true }));
+  router.get(
+    '/debug',
+    express.static(POST_ROOT),
+    serveIndex(POST_ROOT, { icons: true })
+  );
   interface PostRequestMiddleware extends express.Request {
     data: (apis.post.ResultSourcePosts & Record<string, any>)[];
   }
-  const middleware = async function (req: PostRequestMiddleware, _res: express.Response, next: express.NextFunction) {
+  const middleware = async function (
+    req: PostRequestMiddleware,
+    _res: express.Response,
+    next: express.NextFunction
+  ) {
     const posts = await getSourcePosts();
-    req.data = posts.map((parsed) => Object.assign(parsed, parsed.metadata, { body: parsed.body }));
+    req.data = posts.map((parsed) =>
+      Object.assign(parsed, parsed.metadata, { body: parsed.body })
+    );
     next(null);
   };
-  router.get('/list', middleware, async function (req: PostRequestMiddleware, res) {
-    const data = {
-      title: 'Post List',
-      section: 'Post',
-      // merge metadata
-      posts: req.data
-    };
-    res.render('post/list.njk', data);
-  });
+  router.get(
+    '/list',
+    middleware,
+    async function (req: PostRequestMiddleware, res) {
+      const data = {
+        title: 'Post List',
+        section: 'Post',
+        // merge metadata
+        posts: req.data
+      };
+      res.render('post/index.njk', data);
+    }
+  );
 
-  router.get('/edit/:id', middleware, function (req: PostRequestMiddleware, res) {
-    const postid = req.params['id'];
-    const findPost = req.data.find((post) => post.metadata?.id === postid) || new Error(postid + ' not found');
-    if (findPost instanceof Error) return res.json(findPost);
-    res.render('post/edit2.html', { post: findPost });
-  });
+  router.get(
+    '/edit/:id',
+    middleware,
+    function (req: PostRequestMiddleware, res) {
+      const postid = req.params['id'];
+      const findPost =
+        req.data.find((post) => post.metadata?.id === postid) ||
+        new Error(postid + ' not found');
+      if (findPost instanceof Error) return res.json(findPost);
+      res.render('post/edit2.html', { post: findPost });
+    }
+  );
 
   return router;
 }
