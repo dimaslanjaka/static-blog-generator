8ddd8542ad3ec77daf35751eedf38cb2951199f6
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-13 15:03:12 +0700

    add md5

--- packages/sbg-server/src/helper/nunjucks.ts
+++ packages/sbg-server/src/helper/nunjucks.ts
@@ -1,3 +1,4 @@
+import crypto from 'crypto';
 import { postAuthor } from 'hexo-post-parser';
 import moment from 'moment-timezone';
 import { Environment } from 'nunjucks';
@@ -14,7 +15,10 @@ export function parseDate(input: moment.MomentInput, pattern = 'LLL') {
   return moment(input).format(pattern);
 }
 
+export const md5 = (data: string) => crypto.createHash('md5').update(data).digest('hex');
+
 export default function setupNunjuckHelper(env: Environment) {
   env.addGlobal('getAuthorName', getAuthorName);
   env.addGlobal('parseDate', parseDate);
+  env.addGlobal('md5', md5);
 }
