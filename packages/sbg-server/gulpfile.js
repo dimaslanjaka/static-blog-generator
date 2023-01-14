75a4507c7abe581a196ab2e3f9a71c66c83600a7
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-14 18:21:48 +0700

    migrate using browserify

--- packages/sbg-server/gulpfile.js
+++ packages/sbg-server/gulpfile.js
@@ -3,13 +3,16 @@ const { spawn } = require('git-command-helper/dist/spawn');
 const gulp = require('gulp');
 const path = require('upath');
 const fs = require('fs-extra');
-require('./gulpfile-compile-js.task');
+const utility = require('sbg-utility');
+require('./gulpfile-browserify.task');
 
 const cmd = (commandName) => {
   const cmdPath = [
     __dirname,
     process.cwd(),
-    (process.mainModule || process.main).paths[0].split('node_modules')[0].slice(0, -1)
+    (process.mainModule || process.main).paths[0]
+      .split('node_modules')[0]
+      .slice(0, -1)
   ]
     .map((cwd) => {
       const nm = path.join(cwd, 'node_modules/.bin');
@@ -18,22 +21,45 @@ const cmd = (commandName) => {
     })
     .filter(fs.existsSync)[0];
 
-  return process.platform === 'win32' ? cmdPath.replace(/\//g, '\\\\') + '.cmd' : cmdPath;
+  return process.platform === 'win32'
+    ? cmdPath.replace(/\//g, '\\\\') + '.cmd'
+    : cmdPath;
 };
 
-const dist = path.join(__dirname, 'dist');
+// dest folder
+const dist = path.join(__dirname, 'src/public');
+
+/**
+ * copy font-awesome assets
+ * @returns
+ */
+gulp.task('copy:fa', function () {
+  return gulp
+    .src('./source/libs/fontawesome/**/*.{woff,woff2,eot,svg,otf,ttf}', {
+      cwd: __dirname
+    })
+    .pipe(utility.gutils.gulpCached({ name: 'copy-font-awesome' }))
+    .pipe(gulp.dest(dist));
+});
 
 // copy non-javascript assets from src folder
 const copy = function () {
   return gulp
-    .src(['**/*.*'], { cwd: path.join(__dirname, 'src'), ignore: ['**/*.{ts,js,json}'] })
+    .src(['**/*.*'], {
+      cwd: path.join(__dirname, 'src'),
+      ignore: ['**/*.{ts,js,json}']
+    })
     .pipe(gulp.dest(dist));
 };
 
 gulp.task('copy', gulp.series(copy));
 
 function tsc(done) {
-  spawn(cmd('tsc'), ['--build', 'tsconfig.build.json'], { cwd: __dirname, shell: true, stdio: 'inherit' })
+  spawn(cmd('tsc'), ['-p', 'tsconfig.build.json'], {
+    cwd: __dirname,
+    shell: true,
+    stdio: 'inherit'
+  })
     .then(() => done())
     .catch(done);
 }
@@ -41,7 +67,9 @@ function tsc(done) {
 gulp.task('clean', (done) => {
   if (fs.existsSync(dist)) {
     fs.emptyDir(dist, done);
-  } else done();
+  } else {
+    done();
+  }
 });
 
-gulp.task('build', gulp.series('clean', tsc, copy));
+gulp.task('build', gulp.series('copy:fa', tsc, copy));
