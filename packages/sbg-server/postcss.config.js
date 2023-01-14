8d9f7313debf4d6196ee436b6ad5b9277beef048
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-14 11:48:18 +0700

    cancel using webpack to generate html

--- packages/sbg-server/postcss.config.js
+++ packages/sbg-server/postcss.config.js
@@ -1,11 +1,16 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const flowbite = require('flowbite');
+const postcssimport = require('postcss-import');
+
 /** @type {import('postcss-import').AtImportOptions} */
 module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': {},
-    tailwindcss: {},
-    autoprefixer: {},
-    flowbite: {},
-    codemirror: {}
-  }
+  plugins: [
+    'postcss-preset-env',
+    postcssimport,
+    'tailwindcss/nesting',
+    tailwindcss,
+    autoprefixer,
+    flowbite
+  ]
 };
