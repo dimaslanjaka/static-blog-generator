157265ae41cab7305f0d2db830c40ef28e2403c7
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-16 01:53:59 +0700

    chore(post-settings): hotfix post metadata

--- packages/sbg-server/source/scripts/post/settings.js
+++ packages/sbg-server/source/scripts/post/settings.js
@@ -1,14 +1,15 @@
+import axios from 'axios';
 import CodeMirror from 'codemirror';
 import 'codemirror/addon/mode/overlay';
 import 'codemirror/mode/gfm/gfm';
 import 'codemirror/mode/markdown/markdown';
 import 'codemirror/mode/yaml-frontmatter/yaml-frontmatter';
 import 'codemirror/mode/yaml/yaml';
-// import hljs from 'highlight.js';
+import yaml from 'yaml';
 
 console.log('post metadata settings');
 
-const editor = CodeMirror(document.getElementById('editor'), {
+const editor = CodeMirror.fromTextArea(document.getElementById('editor'), {
   mode: 'yaml-frontmatter',
   theme: 'monokai',
   tabSize: 2,
@@ -30,6 +31,36 @@ const editor = CodeMirror(document.getElementById('editor'), {
 editor.on('contextmenu', function (cm, e) {
   console.log(e);
 });
-function submitForm() {
-  //
+
+document
+  .querySelector('#submit-metadata')
+  .addEventListener('click', submitForm);
+function submitForm(e) {
+  if (e && e.preventDefault) e.preventDefault();
+  const metadata = editor.getValue();
+  const pageData = JSON.parse(document.getElementById('post-data').textContent);
+
+  const options = {
+    url: '/post/save',
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json;charset=UTF-8'
+    },
+    data: {
+      metadata: yaml.parse(metadata),
+      id: pageData.id
+    }
+  };
+
+  axios(options)
+    .then((response) => {
+      console.info(response.status);
+      if (response.data.error) {
+        //toastr.error(response.data.message, 'Error saving metadata');
+      } else {
+        //toastr.success(response.data.message, 'Success saving metadata');
+      }
+    })
+    .catch(console.error);
 }
