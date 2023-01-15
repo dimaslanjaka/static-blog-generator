e60a5545ef9e410a3a2be29017f55dd59cfd6ea8
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-15 20:23:15 +0700

    refactor: change some environment - tailwind processing with gulp using wildcard glob - separate tasks

--- packages/sbg-server/gulpfile-browserify.task.js
+++ packages/sbg-server/gulpfile-browserify.task.js
@@ -5,9 +5,6 @@ const glob = require('glob');
 const gulp = require('gulp');
 const Bluebird = require('bluebird');
 const utility = require('sbg-utility');
-const { spawnAsync } = require('git-command-helper/dist/spawn');
-const spawn = require('child_process').spawn;
-const kill = require('tree-kill');
 
 const createWriteStream = (p) => {
   if (!fs.existsSync(path.dirname(p)))
@@ -52,95 +49,3 @@ const bundle = (done) => {
 };
 
 gulp.task('compile:js', gulp.series(bundle));
-
-const pids = [];
-/** @type {import('child_process').ChildProcess[]} */
-const childs = [];
-const killPids = function () {
-  for (let i = 0; i < pids.length; i++) {
-    const pid = pids[i];
-    try {
-      kill(pid);
-      // process.kill(-pid);
-      // const index = pids.indexOf(pid);
-      // if (index !== -1) pids.splice(index, 1);
-    } catch (e) {
-      console.log(String(e));
-      console.log('pid', pid, 'not found');
-    }
-  }
-};
-
-const startServer = function (done) {
-  killPids();
-
-  const child = spawn('npm', ['run', 'dev:server'], {
-    cwd: __dirname,
-    shell: true,
-    detached: true
-  });
-  child.once('exit', (code, signal) => {
-    console.log('subprocess', child.pid, 'exit', { code, signal });
-  });
-  pids.push(child.pid);
-  childs.push(child);
-  typeof done === 'function' && done();
-};
-
-gulp.task('compile:css', async function (done) {
-  await spawnAsync('npm', ['run', 'tw'], {
-    cwd: __dirname,
-    stdio: 'inherit'
-  });
-  await spawnAsync('npm', ['run', 'purge'], {
-    cwd: __dirname,
-    stdio: 'inherit'
-  });
-  typeof done === 'function' && done();
-});
-
-gulp.task('watch', function (done) {
-  const x = gulp.watch(
-    'scripts/**/*',
-    { cwd: __dirname + '/source' },
-    gulp.series('compile:js')
-  );
-  const y = gulp.watch(
-    'styles/**/*',
-    { cwd: __dirname + '/source' },
-    gulp.series('compile:css')
-  );
-  const z = gulp.watch(
-    '**/*.ts',
-    {
-      ignored: ['**/public/**'],
-      cwd: __dirname + '/src'
-    },
-    gulp.series(startServer)
-  );
-
-  [x, y, z].forEach((p) => {
-    p.once('close', () => done());
-  });
-  gulp.parallel('compile:js', 'compile:css', startServer)(null);
-});
-
-process.on('SIGTERM', function () {
-  console.info('SIGTERM signal received.');
-  killPids();
-  return this;
-});
-process.on('SIGHUP', function () {
-  console.log('SIGHUP signal received');
-  killPids();
-  return this;
-});
-process.on('exit', function () {
-  killPids();
-  return this;
-});
-process.on('SIGINT', function () {
-  console.log('SIGINT signal received');
-  killPids();
-  process.exit(1);
-});
--- packages/sbg-server/gulpfile.js
+++ packages/sbg-server/gulpfile.js
@@ -1,10 +1,12 @@
-// const fs = require('fs-extra');
-const { spawn } = require('git-command-helper/dist/spawn');
 const gulp = require('gulp');
 const path = require('upath');
 const fs = require('fs-extra');
 const utility = require('sbg-utility');
+const { spawnAsync } = require('git-command-helper/dist/spawn');
+const spawn = require('child_process').spawn;
+const kill = require('tree-kill');
 require('./gulpfile-browserify.task');
+require('./gulpfile-tailwind.task');
 
 const cmd = (commandName) => {
   const cmdPath = [
@@ -55,7 +57,7 @@ const copy = function () {
 gulp.task('copy', gulp.series(copy));
 
 function tsc(done) {
-  spawn(cmd('tsc'), ['-p', 'tsconfig.build.json'], {
+  spawnAsync(cmd('tsc'), ['-p', 'tsconfig.build.json'], {
     cwd: __dirname,
     shell: true,
     stdio: 'inherit'
@@ -73,3 +75,102 @@ gulp.task('clean', (done) => {
 });
 
 gulp.task('build', gulp.series('copy:fa', tsc, copy));
+
+// dev
+const pids = [];
+/** @type {import('child_process').ChildProcess[]} */
+const childs = [];
+const killPids = function () {
+  for (let i = 0; i < pids.length; i++) {
+    const pid = pids[i];
+    try {
+      kill(pid);
+      // process.kill(-pid);
+      // const index = pids.indexOf(pid);
+      // if (index !== -1) pids.splice(index, 1);
+    } catch (e) {
+      console.log(String(e));
+      console.log('pid', pid, 'not found');
+    }
+  }
+};
+
+const startServer = function (done) {
+  killPids();
+
+  const child = spawn('npm', ['run', 'dev:server'], {
+    cwd: __dirname,
+    shell: true,
+    detached: true
+  });
+  child.once('exit', (code, signal) => {
+    console.log('subprocess', child.pid, 'exit', { code, signal });
+  });
+  pids.push(child.pid);
+  childs.push(child);
+  typeof done === 'function' && done();
+};
+
+gulp.task('compile:css', async function (done) {
+  await spawnAsync('npm', ['run', 'tw'], {
+    cwd: __dirname,
+    stdio: 'inherit'
+  });
+  await spawnAsync('npm', ['run', 'purge'], {
+    cwd: __dirname,
+    stdio: 'inherit'
+  });
+  typeof done === 'function' && done();
+});
+
+gulp.task('watch', function (done) {
+  const v = gulp.watch(
+    '**/*',
+    { cwd: __dirname + '/src/views' },
+    gulp.series('compile:css')
+  );
+  const x = gulp.watch(
+    'scripts/**/*',
+    { cwd: __dirname + '/source' },
+    gulp.series('compile:js')
+  );
+  const y = gulp.watch(
+    'styles/**/*',
+    { cwd: __dirname + '/source' },
+    gulp.series('compile:css')
+  );
+
+  const z = gulp.watch(
+    '**/*.ts',
+    {
+      ignored: ['**/public/**'],
+      cwd: __dirname + '/src'
+    },
+    gulp.series(startServer)
+  );
+
+  [v, x, y, z].forEach((p) => {
+    p.once('close', () => done());
+  });
+  gulp.parallel('compile:js', 'compile:css', startServer)(null);
+});
+
+process.on('SIGTERM', function () {
+  console.info('SIGTERM signal received.');
+  killPids();
+  return this;
+});
+process.on('SIGHUP', function () {
+  console.log('SIGHUP signal received');
+  killPids();
+  return this;
+});
+process.on('exit', function () {
+  killPids();
+  return this;
+});
+process.on('SIGINT', function () {
+  console.log('SIGINT signal received');
+  killPids();
+  process.exit(1);
+});
