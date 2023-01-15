ec38b4a0a6435d3554175db9314b27223620d460
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-15 19:15:27 +0700

    refactor: add more generic types

--- packages/sbg-server/src/globals.d.ts
+++ packages/sbg-server/src/globals.d.ts
@@ -7,6 +7,36 @@ declare let require: {
   ) => void;
 };
 
-declare global {
-  const navigator: global.navigator;
-}
+export type DeepNullable<T> = {
+  [K in keyof T]: DeepNullable<T[K]> | null;
+};
+
+export type DeepRequired<T> = {
+  [K in keyof T]: Required<DeepRequired<T[K]>>;
+};
+
+export type DeepPartial<T> = {
+  [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P];
+};
+
+export type Paths<T, D extends number = 10> = [D] extends [never]
+  ? never
+  : T extends object
+  ? {
+      [K in keyof T]-?: K extends string | number
+        ? `${K}` | Join<K, Paths<T[K], Prev[D]>>
+        : never;
+    }[keyof T]
+  : '';
+
+export type Leaves<T, D extends number = 10> = [D] extends [never]
+  ? never
+  : T extends object
+  ? { [K in keyof T]-?: Join<K, Leaves<T[K], Prev[D]>> }[keyof T]
+  : '';
+
+export type Join<K, P> = K extends string | number
+  ? P extends string | number
+    ? `${K}${'' extends P ? '' : '.'}${P}`
+    : never
+  : never;
