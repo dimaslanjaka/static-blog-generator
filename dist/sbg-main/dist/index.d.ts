453bd7eed594be211a7347b92e1cde1c1b3c988f
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-19 19:51:46 +0700

    chore: remove dist, add release tarball

--- dist/sbg-main/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * as api from 'sbg-api';
-export * as server from 'sbg-server';
-export * as utility from 'sbg-utility';
-//# sourceMappingURL=index.d.ts.map
--- dist/sbg-server/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import SBGServer from './server';
-export default SBGServer;
-//# sourceMappingURL=index.d.ts.map
