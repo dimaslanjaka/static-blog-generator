2568bfb1434bc96688e6c6c41df25c51a7bc7f42
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-19 18:59:18 +0700

    chore(dist): add dist

---
+++ dist/sbg-server/dist/config.d.ts
@@ -0,0 +1,7 @@
+import { createConfig } from 'sbg-utility/dist/config/_config';
+declare const serverConfig: createConfig<{
+    root: string;
+    port: number;
+}>;
+export default serverConfig;
+//# sourceMappingURL=config.d.ts.map
---
+++ dist/sbg-server/dist/server.d.ts
@@ -0,0 +1,27 @@
+import { Express } from 'express-serve-static-core';
+import nunjucks from 'nunjucks';
+import * as apis from 'sbg-api';
+export default interface SBGServer {
+    config: {
+        root: string;
+        port: number;
+    };
+}
+export default class SBGServer {
+    server: Express;
+    env: nunjucks.Environment;
+    api: apis.Application;
+    config: SBGServer['config'];
+    constructor(options?: Partial<SBGServer['config']>);
+    private startExpress;
+    /**
+     * get the configured server
+     * @returns express server instance
+     */
+    get: () => Express;
+    /**
+     * start server
+     */
+    start(): void;
+}
+//# sourceMappingURL=server.d.ts.map
