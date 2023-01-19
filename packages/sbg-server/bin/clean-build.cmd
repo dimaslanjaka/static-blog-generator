bf7529aea82fb632e8a785a4b61ce075d107cfb1
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-19 17:18:49 +0700

    refactor: add log

--- packages/sbg-server/bin/clean-build.cmd
+++ packages/sbg-server/bin/clean-build.cmd
@@ -1,4 +1,10 @@
 @echo off
-rm -rf dist tmp
+echo "removing dist"
+rm -rf dist
+echo "removing tmp"
+rm -rf tmp
+echo "removing src/public"
 rm -rf src/public
+echo "building"
 npm run build
+echo "build finished"
