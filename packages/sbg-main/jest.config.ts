c837d4143f9aeadeb59b619af3b51b582e9d4289
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-20 16:51:50 +0700

    refactor(jest): sync jest config from base

--- packages/sbg-main/jest.config.ts
+++ packages/sbg-main/jest.config.ts
@@ -1,7 +1,4 @@
-import fs from 'fs';
 import { Config } from 'jest';
-import path from 'upath';
-import base from '../../jest.config';
 import tsconfigTest from './tsconfig.test.json';
 
 const config: Config = {
@@ -12,17 +9,7 @@ const config: Config = {
       // https://kulshekhar.github.io/ts-jest/docs/getting-started/options/tsconfig
       { tsconfig: tsconfigTest.compilerOptions }
     ]
-  },
-  rootDir: 'test',
-  // coverage directory
-  coverageDirectory: path.join(__dirname, 'coverage')
+  }
 };
 
-const merged = Object.assign(base, config);
-merged.roots?.push(__dirname + '/test');
-merged.roots?.push(__dirname);
-if (merged.roots) {
-  merged.roots = merged.roots.filter(fs.existsSync);
-}
-
-export default merged;
+export default config;
