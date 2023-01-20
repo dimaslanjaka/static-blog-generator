0f3e50c3bce50be13f18a0502cc22fdd1532ff09
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-20 18:02:21 +0700

    chore(api): copy wait all handlers to be closed

--- packages/sbg-api/test/validate-copy.ts
+++ packages/sbg-api/test/validate-copy.ts
@@ -13,6 +13,6 @@ export default function validateCopy(api: Application) {
     api.copy().then(() => done());
   }, 60000);
   test('validate total markdown', function () {
-    expect(readdirSync(join(__dirname, 'source/_posts')).length).toBeGreaterThan(8);
+    expect(readdirSync(join(api.config.cwd, api.config.source_dir, '_posts')).length).toBeGreaterThan(8);
   });
 }
