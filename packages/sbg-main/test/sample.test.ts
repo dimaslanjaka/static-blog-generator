9d9fec08a3b5eca01675097649e2afcc89072853
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-20 16:09:32 +0700

    refactor(main): separate test function

--- packages/sbg-main/test/sample.test.ts
+++ packages/sbg-main/test/sample.test.ts
@@ -1,5 +1,4 @@
-import { spawn } from 'child_process';
-import path from 'upath';
+import cmd from './utils/cmd';
 
 describe('mock usage', () => {
   it('should show help', async () => {
@@ -7,30 +6,3 @@ describe('mock usage', () => {
     expect(spy).toEqual(expect.stringContaining('usage: sbg <command>'));
   }, 30000);
 });
-
-/**
- * Programmatically set arguments and execute the CLI script
- *
- * @param args - positional and option arguments for the command to run
- * @returns
- */
-function cmd(...args: string[]): Promise<string> {
-  const p = spawn('npx', ['ts-node', path.resolve(__dirname + '/../src/cli.ts'), ...args], {
-    cwd: __dirname,
-    shell: true
-  });
-  return new Promise((resolveFunc) => {
-    const output = [] as string[];
-    p.stdout.on('data', (x) => {
-      output.push(x.toString());
-      // process.stdout.write(x.toString());
-    });
-    p.stderr.on('data', (x) => {
-      output.push(x.toString());
-      // process.stderr.write(x.toString());
-    });
-    p.on('exit', (_code) => {
-      resolveFunc(output.join('\n'));
-    });
-  });
-}
