4951e8e37208ebf7bdaf7efd954068d15902a4f9
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-20 16:21:43 +0700

    chore(main): add colors

--- packages/sbg-main/src/cli.ts
+++ packages/sbg-main/src/cli.ts
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 
+import ansiColors from 'ansi-colors';
+import { Application } from 'sbg-api';
 import yargs from 'yargs';
+
+const api = new Application(process.cwd());
+const rootColor = ansiColors.bgYellowBright.black('<root>');
 
 yargs
   .scriptName('sbg')
@@ -13,6 +18,19 @@ yargs
     },
     () => {
       yargs.showHelp();
+    }
+  )
+  .command(
+    'post <key>',
+    `operation inside ${rootColor}/${api.config.post_dir}`,
+    (yargs) => {
+      yargs.positional(`copy`, {
+        type: `string`,
+        describe: `copy ${rootColor}/${api.config.post_dir} to ${rootColor}/${api.config.source_dir}/_posts`
+      });
+    },
+    async ({ key }) => {
+      console.log(key);
     }
   )
   .help('help')
