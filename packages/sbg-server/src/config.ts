ea071dbe13342639e51f074a01eab2f039fa2e11
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-21 16:53:32 +0700

    refactor: separate branches - remove failure packer - fresh build - separate dist branch

--- packages/sbg-server/src/config.ts
+++ packages/sbg-server/src/config.ts
@@ -1,8 +1,9 @@
 import fs from 'fs-extra';
 import { createConfig } from 'sbg-utility/dist/config/_config';
 import SBGServer from './server';
+import path from 'upath';
 
-const DEV_SITE_ROOT = 'D:/Repositories/static-blog-generator/packages/sbg-main/test';
+const DEV_SITE_ROOT = path.join(__dirname, '../../../test'); // 'D:/Repositories/static-blog-generator/test';
 
 // set default config
 const serverConfig = new createConfig<SBGServer['config']>('sbg-server', {
