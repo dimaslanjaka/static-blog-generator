f6393ad7d6eb1a385da974405a6cdfb5432ed75b
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-21 18:20:50 +0700

    fix(utils): fix empty dir with filter

--- packages/sbg-utility/src/utils/filemanager/emptyDir.ts
+++ packages/sbg-utility/src/utils/filemanager/emptyDir.ts
@@ -1,35 +1,37 @@
-import fs from 'fs-extra';
-import path from 'upath';
+import minimatch from 'minimatch';
+import readDir from './readDir';
 
-interface readDirDone {
-  (err: Error, results?: string[]): any;
+interface emptyDirOpt {
+  ignore: (string | RegExp)[];
 }
 
-const readDir = function (dir: fs.PathLike, done: readDirDone) {
-  let results = [];
-  fs.readdir(dir, function (err, list) {
-    if (err) return done(err);
-    let i = 0;
-    (function next() {
-      let file = list[i++];
-      if (!file) return done(null, results);
-      file = path.resolve(dir, file);
-      fs.stat(file, function (err, stat) {
-        if (!err && stat && stat.isDirectory()) {
-          readDir(file, function (err, res) {
-            if (!err) results = results.concat(res);
-            next();
-          });
+/**
+ * empty dir with filters
+ * @param dir
+ * @param param1
+ */
+export function emptyDir(dir: string, { ignore }: emptyDirOpt) {
+  readDir(dir, function (err, fileList) {
+    if (err) throw err;
+    // const filterFn = (pattern: string) => minimatch.filter(pattern, { matchBase: true });
+    const filter = fileList.filter((file) => {
+      for (let i = 0; i < ignore.length; i++) {
+        const pattern = ignore[i];
+        if (typeof pattern === 'string') {
+          const match = minimatch(file, pattern, { matchBase: true, dot: true });
+          // console.log(file.replace(dir, ''), pattern, match);
+          // filter file if matched
+          if (match) return false;
         } else {
-          results.push(file);
-          next();
+          // filter file if matched
+          if (pattern.test(file)) return false;
         }
-      });
-    })();
+      }
+      return true;
+    });
+    return filter;
   });
-};
+}
 
-readDir(path.join(__dirname, '../..'), function (err, results) {
-  if (err) throw err;
-  console.log(results);
-});
+//const _ex = path.join(__dirname, '../..');
+//emptyDir(_ex, { ignore: ['**/config/**'] });
