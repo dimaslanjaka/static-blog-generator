f9c44163fde9023f052d540d4b69b9a69acd4ec0
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-21 15:38:42 +0700

    feat(pack): ignore some paths

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,7 @@ const { spawnAsync } = require('git-command-helper/dist/spawn');
 const gulp = require('gulp');
 const { join, toUnix, resolve: resolvePath } = require('upath');
 const Bluebird = require('bluebird');
-
-/**
- * dump list files from `npm pack`
- * @see {@link https://www.webmanajemen.com/NodeJS/snippet/get-list-files-from-npm-pack.html}
- * @param {string} cwd
- */
-async function checkPacked(cwd) {
-  const result = await spawnAsync('npm', ['pack', '--json', '--dry-run'], { cwd });
-  const parse = JSON.parse(result.stdout)[0];
-  const { files } = parse;
-  // uncomment for log to file
-  //const output = join(__dirname, 'tmp/listpack.txt');
-  //writeFileSync(output, files.map((o) => o && o.path).join('\n'));
-  //console.log(output);
-  console.log(files);
-}
+const { checkPacked } = require('./check-packed.js');
 
 gulp.task('check-dist', () => checkPacked(__dirname + '/dist'));
 
