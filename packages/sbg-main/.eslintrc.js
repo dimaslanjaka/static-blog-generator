00118f90dc00002887d5fa1bdcd0034e7dc03e91
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-21 13:50:57 +0700

    refactor(main): init custom eslint rule

---
+++ packages/sbg-main/.eslintrc.js
@@ -0,0 +1,8 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: '../../.eslintrc.js',
+  rules: {
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': 'off'
+  }
+};
