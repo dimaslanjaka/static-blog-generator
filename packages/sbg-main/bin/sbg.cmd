66648f1424793a72815daaa67dbed3caea99744b
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-21 15:01:32 +0700

    feat(cli)!: rework cli using yargs positional

--- packages/sbg-main/bin/sbg.cmd
+++ packages/sbg-main/bin/sbg.cmd
@@ -1,3 +1,16 @@
-@echo off
+@ECHO OFF
 
-ts-node src/cli.ts "%*"
+rem sample https://github.com/npm/cli/blob/latest/bin/npx.cmd
+
+SETLOCAL
+
+SET "NODE_EXE=%~dp0\node.exe"
+IF NOT EXIST "%NODE_EXE%" (
+  SET "NODE_EXE=node"
+)
+
+SET "SBG_CLI=%~dp0\..\src\cli.ts"
+
+"%NODE_EXE%" -r ts-node/register "%SBG_CLI%" %*
+
+rem ts-node src/cli.ts "%*"
--- packages/sbg-main/src/index.ts
+++ packages/sbg-main/src/index.ts
@@ -1,3 +1,4 @@
 export * as api from 'sbg-api';
-export * as server from 'sbg-server';
+export { default as server } from 'sbg-server';
 export * as utility from 'sbg-utility';
+//
