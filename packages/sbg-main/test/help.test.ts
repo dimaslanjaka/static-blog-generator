d3e140891363f5869bd2ef5cf1c070ce6cfd30fe
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-21 18:46:49 +0700

    fix(cli): increase test timeout

--- packages/sbg-main/test/help.test.ts
+++ packages/sbg-main/test/help.test.ts
@@ -6,17 +6,17 @@ process.cwd = () => testCwd;
 import { describe, expect, it } from '@jest/globals';
 import cmd from './utils/cmd';
 
-describe('mock CLI usage', () => {
+describe('test CLI usage', () => {
   it('without args', async () => {
     const spy = await cmd('');
     expect(spy).toEqual(expect.stringContaining('usage: sbg <command>'));
-  }, 30000);
+  }, 60000);
   it('help', async () => {
     const spy = await cmd('help');
     expect(spy).toEqual(expect.stringContaining('Show help'));
-  }, 30000);
-  it('post without subcommand', async () => {
+  }, 60000);
+  it('post without option', async () => {
     const spy = await cmd('post');
-    expect(spy).toEqual(expect.stringContaining('Positionals:'));
-  }, 30000);
+    expect(spy).toEqual(expect.stringContaining('Not enough non-option arguments: got 0, need at least 1'));
+  }, 60000);
 });
