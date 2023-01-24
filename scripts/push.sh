b5feba2b5329139f96646bf5e46d3f03be85ffcb
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-24 13:19:12 +0700

    chore(deps): update submodules and dependencies

--- scripts/push.sh
+++ scripts/push.sh
@@ -3,5 +3,20 @@
 ## Push with submodules
 ## usages: sh scripts/push.sh
 
-git submodule foreach "git push | :"
-git push
+ROOT="$(git rev-parse --show-toplevel)"
+git -C "${REPO_PATH}" config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
+    while read -r KEY MODULE_PATH
+    do
+        # cd git root dir
+        cd ${ROOT}
+
+
+        cd "${MODULE_PATH}"
+        if [ -z $(git status --porcelain) ];
+        then
+            echo "${repo} at ${MODULE_PATH} no changes"
+        else
+            echo "push for ${repo} at ${MODULE_PATH} branch ${BRANCH}"
+            git push origin "${BRANCH}"
+        fi
+    done
