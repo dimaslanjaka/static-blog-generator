2cddebe43963a1594d2642c5e03853da74a02beb
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-24 12:47:22 +0700

    fix(ci): apply token each submodule

--- scripts/install-submodules.sh
+++ scripts/install-submodules.sh
@@ -3,9 +3,13 @@
 # auto clone all submodules, prevent not-matched hash
 # usages: sh scripts/install-submodules.sh
 
+ROOT="$(git rev-parse --show-toplevel)"
+
 git -C "${REPO_PATH}" config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
     while read -r KEY MODULE_PATH
     do
+        # cd git root dir
+        cd ${ROOT}
         # If the module's path exists, remove it.
         # This is done b/c the module's path is currently
         # not a valid git repo and adding the submodule will cause an error.
@@ -22,10 +26,17 @@ git -C "${REPO_PATH}" config -f .gitmodules --get-regexp '^submodule\..*\.path$'
         URL="$(git config -f .gitmodules --get "${url_key}")"
         BRANCH="$(git config -f .gitmodules --get "${branch_key}" || echo "master")"
 
+        git -C "${REPO_PATH}" submodule add --force -b "${BRANCH}" --name "${NAME}" "${URL}" "${MODULE_PATH}" || echo "cannot add submodule ${MODULE_PATH}"
+
         repo=${URL#"https://github.com/"}
         URL_WITH_TOKEN="https://${ACCESS_TOKEN}@github.com/${repo}"
 
-        git -C "${REPO_PATH}" submodule add --force -b "${BRANCH}" --name "${NAME}" "${URL}" "${MODULE_PATH}" || continue
+        echo "apply token at ${MODULE_PATH} branch ${BRANCH}"
+        cd "${MODULE_PATH}"
+        git remote set-url origin "${URL_WITH_TOKEN}"
+        git fetch --all
+        git pull origin "${BRANCH}" -X theirs
+        #git checkout --track "origin/${BRANCH}" ||
     done
 
 git -C "${REPO_PATH}" submodule update --init --recursive
