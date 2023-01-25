4c654259753c4390b922cc9f2caffe44f240a9d8
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-25 11:48:30 +0700

    fix(deps): fix non-tarball caches in lockfile

--- scripts/remove-node-temp.js
+++ scripts/remove-node-temp.js
@@ -3,9 +3,9 @@ const glob = require('glob');
 const fs = require('fs');
 
 const base = path.join(__dirname, '..');
-const dirs = glob
-  .sync('**/node_modules/**', { cwd: base })
-  .concat(glob.sync('**/tmp/**', { cwd: base }))
+const dirs = []
+  //.concat(glob.sync('**/node_modules/**', { cwd: base }))
+  //.concat(glob.sync('**/tmp/**', { cwd: base }))
   .concat(glob.sync('**/package-lock.json', { cwd: base }))
   .sort((x, y) => x.length - y.length)
   .reverse()
