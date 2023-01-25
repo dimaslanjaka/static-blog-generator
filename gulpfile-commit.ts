4c7e7a420a3853813f797a66d5ebde0a27744346
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-25 16:52:04 +0700

    chore: update build and dependencies

---
+++ gulpfile-commit.ts
@@ -0,0 +1,29 @@
+import fs from 'fs-extra';
+import git from 'git-command-helper';
+import path from 'upath';
+
+const repo = 'https://github.com/dimaslanjaka/static-blog-generator';
+const branch = 'master';
+
+const base = path.join(__dirname, 'dist');
+
+export async function commitDist() {
+  // clone when not exist
+  if (!fs.existsSync(base)) {
+    await git.ext.spawnAsync('git', ['clone', '-b', branch, '--single-branch', repo, base], {
+      cwd: __dirname,
+      stdio: 'inherit'
+    });
+  }
+  const rootgh = new git(__dirname);
+  rootgh.latestCommit(null, { cwd: __dirname }).then(async (hash) => {
+    const distgh = new git(base);
+    if (await distgh.hasChanged()) {
+      await distgh.add('-A').catch(rootgh.noop);
+      await distgh.commit(`chore(dist): update build (${repo}/commit/${hash})`).catch(rootgh.noop);
+      if (await distgh.canPush()) {
+        await distgh.push();
+      }
+    }
+  });
+}
