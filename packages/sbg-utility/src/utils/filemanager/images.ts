a9328dc064f3e27d57fbea404a75f999ff82ed1b
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-26 16:25:12 +0700

    chore: add more helpers (array,string,object,file)

---
+++ packages/sbg-utility/src/utils/filemanager/images.ts
@@ -0,0 +1,13 @@
+import fs from 'fs-extra';
+
+/**
+ * function to encode file data to base64 encoded string
+ * @param file path file
+ * @returns
+ */
+export function image_base64_encode(file: fs.PathOrFileDescriptor) {
+  // read binary data
+  const bitmap = fs.readFileSync(file);
+  // convert binary data to base64 encoded string
+  return bitmap.toString('base64');
+}
