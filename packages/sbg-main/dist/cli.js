9f85d53fbc0f5a09eca6e0cdc3e127b21cf850d2
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-28 13:41:40 +0700

    fix(cli): add generate operation
    operating inside generated folder (config.public_dir)

--- packages/sbg-main/dist/cli.js
+++ packages/sbg-main/dist/cli.js
@@ -6,6 +6,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const ansi_colors_1 = __importDefault(require("ansi-colors"));
 const sbg_api_1 = require("sbg-api");
+const upath_1 = __importDefault(require("upath"));
 const yargs_1 = __importDefault(require("yargs"));
 const api = new sbg_api_1.Application(process.cwd());
 const rootColor = ansi_colors_1.default.bgYellowBright.black('<root>');
@@ -39,6 +40,23 @@ yargs_1.default
         yargs_1.default.showHelp();
     }
 })
+    .command('generate <key>', `operation inside ${rootColor}/${api.config.public_dir}`, function (yargs) {
+    yargs.positional(`seo`, {
+        type: `string`,
+        describe: `fix seo`
+    });
+    yargs.positional(`safelink`, {
+        type: `string`,
+        describe: `anonymize external links`
+    });
+}, async function ({ key }) {
+    if (key === 'seo') {
+        await api.seo(upath_1.default.join(api.config.cwd, api.config.public_dir));
+    }
+    else if (key === 'safelink') {
+        await api.safelink(upath_1.default.join(api.config.cwd, api.config.public_dir));
+    }
+})
     .command('deploy <key>', `operation inside ${rootColor}/.deploy_${api.config.deploy?.type || 'git'}`, function (yargs) {
     yargs.positional(`seo`, {
         type: `string`,
@@ -50,10 +68,10 @@ yargs_1.default
     });
 }, async function ({ key }) {
     if (key === 'seo') {
-        await api.seo();
+        await api.seo(upath_1.default.join(api.config.cwd, `/.deploy_${api.config.deploy?.type || 'git'}`));
     }
     else if (key === 'safelink') {
-        await api.safelink();
+        await api.safelink(upath_1.default.join(api.config.cwd, `/.deploy_${api.config.deploy?.type || 'git'}`));
     }
 })
     .help('help')
