17fd213700cbc18f1d458c794a57b79f1dc95e7f
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-03 21:59:54 +0700

    chore: add gulp-dom

---
+++ packages/sbg-utility/src/gulp-utils/gulp-dom.ts
@@ -0,0 +1,61 @@
+'use strict';
+
+import jsdom from 'jsdom';
+import PluginError from 'plugin-error';
+import through2 from 'through2';
+import truecasepath from 'true-case-path';
+import upath from 'upath';
+const pluginName = 'gulp-dom';
+const path = {
+  join: (...str: string[]) => upath.toUnix(truecasepath.trueCasePathSync(upath.join(...str))),
+  dirname: (str: string) => upath.toUnix(truecasepath.trueCasePathSync(upath.dirname(str))),
+  toUnix: (str: string) => upath.toUnix(truecasepath.trueCasePathSync(str))
+};
+
+export const customPath = path;
+export const gulpDomPath = path;
+
+/**
+ * Callback/Mutator
+ * * this: jsdom
+ * * path: current file.path
+ */
+export type GulpDomCallback = (/** jsdom bind */ this: Document, /** current file path */ path: string) => any;
+
+/**
+ * gulp-dom
+ * @param mutator callback
+ * @returns
+ */
+export default function gulpDom(mutator: GulpDomCallback) {
+  const stream = through2.obj(function (file, _enc, callback) {
+    if (file.isNull()) {
+      return callback(null, file);
+    }
+
+    if (file.isStream()) {
+      return stream.emit('error', new PluginError(pluginName, 'Streaming not supported'));
+    }
+
+    if (file.isBuffer()) {
+      try {
+        const dom = new jsdom.JSDOM(file.contents.toString('utf8'));
+        const mutated = mutator.call(dom.window.document, file.path);
+
+        file.contents = Buffer.from(typeof mutated === 'string' ? mutated : dom.serialize());
+        callback(null, file);
+
+        dom.window.close();
+      } catch (e) {
+        if (e instanceof Error) {
+          console.log(e.message);
+        }
+        console.log(pluginName, 'drop file', file.path);
+        // drop file
+        callback();
+      }
+    }
+  });
+
+  return stream;
+}
