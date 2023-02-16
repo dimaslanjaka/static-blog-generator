7aea988f6fabd133f34bac34ef4402854b8154f7
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-16 19:08:50 +0700

    chore: init feed generator cli

--- packages/sbg-api/src/feed/index.ts
+++ packages/sbg-api/src/feed/index.ts
@@ -1,3 +1,4 @@
+import Bluebird from 'bluebird';
 import { PathOrFileDescriptor, readFileSync, writeFileSync } from 'fs';
 import gulp from 'gulp';
 import gulpDom from 'gulp-dom';
@@ -9,84 +10,97 @@ import { join } from 'upath';
 
 const env = envNunjucks();
 
-gulp.task('feed', function (done) {
-  const config = getConfig();
+/**
+ * generate feed with hexo
+ * @param done
+ * @param config
+ */
+export function hexoGenerateFeed(done: gulp.TaskFunctionCallback, config = getConfig()) {
   const instance = new hexo(config.cwd);
 
-  instance.init().then(() => {
-    instance.load().then(() => {
-      env.addFilter('formatUrl', (str) => {
-        return full_url_for.call(instance, str);
+  return new Bluebird((resolve) => {
+    instance.init().then(() => {
+      instance.load().then(() => {
+        env.addFilter('formatUrl', (str) => {
+          return full_url_for.call(instance, str);
+        });
+
+        function build(tmplSrc: PathOrFileDescriptor, dest: PathOrFileDescriptor) {
+          const template = nunjucks.compile(readFileSync(tmplSrc, 'utf-8'), env);
+
+          let posts = instance.locals.get('posts');
+          posts = posts.sort('-date');
+          posts = posts.filter((post) => {
+            return post.draft !== true;
+          });
+
+          const { email, feed, url: urlCfg } = config;
+          const { icon: iconCfg } = feed;
+
+          let url = urlCfg;
+          if (url[url.length - 1] !== '/') url += '/';
+          // remove broken hexo lang shortcode
+          if (url.includes(':lang/')) url = url.replace('/:lang/', '/');
+
+          let icon = '';
+          if (iconCfg) icon = full_url_for.call(instance, iconCfg);
+          else if (email) icon = gravatar(email, {});
+
+          const feed_url = full_url_for.call(instance, 'rss.xml');
+
+          const data = template.render({
+            config,
+            url,
+            icon,
+            posts,
+            feed_url
+          });
+
+          writeFileSync(dest, data);
+        }
+
+        const templateRSS = join(__dirname, '_config_template_rss.xml');
+        const destRSS = join(config.cwd, config.public_dir, 'rss.xml');
+        build(templateRSS, destRSS);
+
+        const templateATOM = join(__dirname, '_config_template_atom.xml');
+        const destATOM = join(config.cwd, config.public_dir, 'atom.xml');
+        build(templateATOM, destATOM);
+
+        const baseURL = config.url.endsWith('/') ? config.url : config.url + '/';
+
+        const publicDir = join(config.cwd, config.public_dir);
+        gulp
+          .src('**/*.html', { cwd: publicDir, ignore: commonIgnore })
+          .pipe(
+            gulpDom(function () {
+              // auto discovery rss
+              if (
+                this.querySelectorAll(`link[href="${baseURL}rss.xml"]`).length === 0 &&
+                this.querySelectorAll(`link[href="/rss.xml"]`).length === 0
+              ) {
+                this.head.innerHTML += `<link id="rss-site-url" type="application/rss+xml" rel="alternate" href="${baseURL}rss.xml" />`;
+              }
+              // auto discovery atom
+              if (
+                this.querySelectorAll(`link[href="${baseURL}atom.xml"]`).length === 0 &&
+                this.querySelectorAll(`link[href="/atom.xml"]`).length === 0
+              ) {
+                this.head.innerHTML += `<link id="atom-site-url" type="application/atom+xml" rel="alternate" href="${baseURL}atom.xml" />`;
+              }
+              //this.querySelectorAll('body')[0].setAttribute('data-version', '1.0');
+            })
+          )
+          .pipe(gulp.dest(publicDir))
+          .once('end', () => {
+            if (typeof done === 'function') {
+              done();
+            }
+            resolve(null);
+          });
       });
-
-      function build(tmplSrc: PathOrFileDescriptor, dest: PathOrFileDescriptor) {
-        const template = nunjucks.compile(readFileSync(tmplSrc, 'utf-8'), env);
-
-        let posts = instance.locals.get('posts');
-        posts = posts.sort('-date');
-        posts = posts.filter((post) => {
-          return post.draft !== true;
-        });
-
-        const { email, feed, url: urlCfg } = config;
-        const { icon: iconCfg } = feed;
-
-        let url = urlCfg;
-        if (url[url.length - 1] !== '/') url += '/';
-        // remove broken hexo lang shortcode
-        if (url.includes(':lang/')) url = url.replace('/:lang/', '/');
-
-        let icon = '';
-        if (iconCfg) icon = full_url_for.call(instance, iconCfg);
-        else if (email) icon = gravatar(email, {});
-
-        const feed_url = full_url_for.call(instance, 'rss.xml');
-
-        const data = template.render({
-          config,
-          url,
-          icon,
-          posts,
-          feed_url
-        });
-
-        writeFileSync(dest, data);
-      }
-
-      const templateRSS = join(__dirname, '_config_template_rss.xml');
-      const destRSS = join(config.cwd, config.public_dir, 'rss.xml');
-      build(templateRSS, destRSS);
-
-      const templateATOM = join(__dirname, '_config_template_atom.xml');
-      const destATOM = join(config.cwd, config.public_dir, 'atom.xml');
-      build(templateATOM, destATOM);
-
-      const baseURL = config.url.endsWith('/') ? config.url : config.url + '/';
-
-      const publicDir = join(config.cwd, config.public_dir);
-      gulp
-        .src('**/*.html', { cwd: publicDir, ignore: commonIgnore })
-        .pipe(
-          gulpDom(function () {
-            // auto discovery rss
-            if (
-              this.querySelectorAll(`link[href="${baseURL}rss.xml"]`).length === 0 &&
-              this.querySelectorAll(`link[href="/rss.xml"]`).length === 0
-            ) {
-              this.head.innerHTML += `<link id="rss-site-url" type="application/rss+xml" rel="alternate" href="${baseURL}rss.xml" />`;
-            }
-            // auto discovery atom
-            if (
-              this.querySelectorAll(`link[href="${baseURL}atom.xml"]`).length === 0 &&
-              this.querySelectorAll(`link[href="/atom.xml"]`).length === 0
-            ) {
-              this.head.innerHTML += `<link id="atom-site-url" type="application/atom+xml" rel="alternate" href="${baseURL}atom.xml" />`;
-            }
-            //this.querySelectorAll('body')[0].setAttribute('data-version', '1.0');
-          })
-        )
-        .pipe(gulp.dest(publicDir))
-        .once('end', () => done());
     });
   });
-});
+}
+
+gulp.task('feed', hexoGenerateFeed);
