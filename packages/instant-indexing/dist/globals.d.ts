07cc45102f76ead33f5f5251ad1257d3a8208f11
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-19 23:46:50 +0700

    chore: remove unused files, export webmaster tools

--- packages/instant-indexing/dist/globals.d.ts
+++
@@ -1,7 +0,0 @@
-export interface Key {
-    client_email: string;
-    private_key: string;
-}
-export interface ServiceConfig extends Key {
-    keyFile: string;
-}
--- packages/instant-indexing/src/index.ts
+++ packages/instant-indexing/src/index.ts
@@ -1,5 +1,6 @@
 export * from './config';
 export * from './notify';
 export * from './oauth2';
+export * from './webmaster';
 
 //
