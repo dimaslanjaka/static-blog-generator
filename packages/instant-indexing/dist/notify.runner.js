7e29f4817bee38f9a57855241305ee1fce051f34
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-19 23:10:45 +0700

    fix: change typescript config

--- packages/instant-indexing/dist/notify.runner.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var notify_1 = require("./notify");
-var oauth2_1 = require("./oauth2");
-(0, oauth2_1.jwtAuthenticate)().then(function (client) {
-    (0, notify_1.notify2)('https://www.webmanajemen.com/chimeraland/monsters/cicada.html', 'URL_UPDATED', client.credentials);
-});
-//# sourceMappingURL=notify.runner.js.map
