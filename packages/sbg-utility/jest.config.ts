58d1c601561d36b5ecf6fec4e362db8d382a5e48
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-21 10:54:54 +0700

    feat: add jsonc-parser fix: cannot parse tsconfig.json jest config

--- packages/sbg-utility/jest.config.ts
+++ packages/sbg-utility/jest.config.ts
@@ -1,4 +1,11 @@
-import tsconfigTest from './tsconfig.json';
+import fs from 'fs';
+import jsonc from 'jsonc-parser';
+
+const jparse = jsonc.parse;
+const parsed = jparse(fs.readFileSync(__dirname + '/tsconfig.json').toString(), [], {
+  allowTrailingComma: true,
+  disallowComments: false
+});
 
 const config: Extract<NonNullable<import('jest').Config['projects']>[number], Record<string, any>> = {
   transform: {
@@ -6,7 +13,7 @@ const config: Extract<NonNullable<import('jest').Config['projects']>[number], Re
       'ts-jest',
       // required due to custom location of tsconfig.json configuration file
       // https://kulshekhar.github.io/ts-jest/docs/getting-started/options/tsconfig
-      { tsconfig: tsconfigTest.compilerOptions }
+      { tsconfig: parsed.compilerOptions }
     ]
   }
 };
