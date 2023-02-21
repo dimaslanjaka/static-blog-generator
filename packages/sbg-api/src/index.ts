52a18a4acb07310f2541799b2eb12e4b842ae016
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-21 11:29:49 +0700

    chore: fix exports

--- packages/sbg-api/src/index.ts
+++ packages/sbg-api/src/index.ts
@@ -1,18 +1,4 @@
-export * from './api';
-export { default as Application } from './api';
-export * from './clean';
-export * as clean from './clean';
-export * from './feed';
-export * as feed from './feed';
-export * from './gulp.safelink';
-export * as safelink from './gulp.safelink';
-export * from './gulp.seo';
-export * as seo from './gulp.seo';
-export * from './post';
-export * as post from './post';
-export * from './sitemap';
-export * as sitemap from './sitemap';
-import { default as Application } from './api';
-export default Application;
+export * from './index-exports';
 
-//
+import * as exporter from './index-exports';
+export default exporter;
