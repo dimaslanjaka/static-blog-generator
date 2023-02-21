e3538a750a55831508b07ccf6a4b833ae876da49
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-21 12:51:54 +0700

    chore: remove persistent-cache
    prefer using persistent cache from sbg-utility

--- packages/sbg-server/server-dev.runner.js
+++ packages/sbg-server/server-dev.runner.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 require('ts-node').register({ projectSearchDir: __dirname });
 
 process.cwd = () => __dirname + '/../../test';
+const { default: axios } = require('axios');
 const { pathJoin } = require('sbg-utility');
 const { SBGServer } = require('./src/server');
 
@@ -13,4 +14,7 @@ const _server = new SBGServer({
   port: 4000,
   root: pathJoin(__dirname, '../../test')
 });
-_server.start();
+_server.start().once('listening', function () {
+  console.log('check connection');
+  axios.get('http://localhost:4000/post/json');
+});
