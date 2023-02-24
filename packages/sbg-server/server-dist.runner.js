20fd82b7d1285f657844eaf4b4593b55160bd53a
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-24 17:07:58 +0700

    refactor: separate function to be shared

--- packages/sbg-server/server-dist.runner.js
+++ packages/sbg-server/server-dist.runner.js
@@ -1,15 +1,11 @@
 require('dotenv').config();
 
 process.cwd = () => __dirname + '/../../test';
-const { pathJoin } = require('sbg-utility');
+const { server_runner } = require('server.runner');
 const { SBGServer } = require('./dist/server');
 
 // dev server
 // just test unit
 // not for compile
 
-const _server = new SBGServer({
-  port: 4000,
-  root: pathJoin(__dirname, '../../test')
-});
-_server.start();
+server_runner(SBGServer);
