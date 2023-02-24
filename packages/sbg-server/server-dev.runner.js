2e992c887110e2aef93ef52abf7de17faa8bcb73
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-24 17:07:58 +0700

    refactor: separate function to be shared

--- packages/sbg-server/server-dev.runner.js
+++ packages/sbg-server/server-dev.runner.js
@@ -2,35 +2,11 @@ require('dotenv').config();
 require('ts-node').register({ projectSearchDir: __dirname });
 
 process.cwd = () => __dirname + '/../../test';
-const { default: axios } = require('axios');
-const { pathJoin } = require('sbg-utility');
+const { server_runner } = require('server.runner');
 const { SBGServer } = require('./src/server');
 
 // dev server
 // just test unit
 // not for compile
 
-const _server = new SBGServer({
-  port: 4000,
-  root: pathJoin(__dirname, '../../test'),
-  cache: false
-});
-const app = _server.startExpress();
-app.get('/session-test', function (req, res) {
-  if (req.session.views) {
-    req.session.views++;
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<p>views: ' + req.session.views + '</p>');
-    res.end();
-  } else {
-    req.session.views = 1;
-    res.end('Welcome to the file session demo. Refresh page!');
-  }
-});
-_server.start(app).once('listening', function () {
-  console.log('check connection');
-  axios
-    .get('http://localhost:4000/post/json')
-    .then(() => console.log('check connection success'))
-    .catch(() => console.log('check connection failed'));
-});
+server_runner(SBGServer);
