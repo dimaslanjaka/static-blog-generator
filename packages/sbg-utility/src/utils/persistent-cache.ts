6ca6e3bd2187e4223c08aace9c5c30c0dbbfa81a
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-28 23:41:00 +0700

    chore: release 1.0.5
    hotfix persistent-cache

--- packages/sbg-utility/src/utils/persistent-cache.ts
+++ packages/sbg-utility/src/utils/persistent-cache.ts
@@ -139,12 +139,13 @@ export class persistentCache implements PersistentCacheOpt {
    * @param fallback
    * @returns
    */
-  getSync<T = string>(name: string, fallback?: T): T {
+  getSync<T>(name: string, fallback?: T): T {
     if (this.memory && !!this.memoryCache[name]) {
       const entry = this.memoryCache[name];
 
       if (entry.cacheUntil && new Date().getTime() > entry.cacheUntil) {
-        return undefined;
+        // cache expired
+        return fallback;
       }
 
       return JSON.parse(entry.data);
@@ -162,9 +163,149 @@ export class persistentCache implements PersistentCacheOpt {
     return data.data;
   }
 
+  get<T>(name: string, fallback?: T): Promise<T>;
+  get<T>(name: string, fallback?: (e: Error | undefined | null, entry?: T) => any): void;
+  get(name: string, fallback?: any) {
+    if (typeof fallback !== 'function') {
+      return new Promise((resolve) => {
+        resolve(this.getSync(name, fallback));
+      });
+    } else {
+      if (this.memory && !!this.memoryCache[name]) {
+        let entry = this.memoryCache[name];
+
+        if (!!entry.cacheUntil && new Date().getTime() > entry.cacheUntil) {
+          return safeCb(fallback)(null, undefined);
+        }
+
+        try {
+          entry = JSON.parse(entry.data);
+        } catch (e) {
+          return safeCb(fallback)(e);
+        }
+
+        return safeCb(fallback)(null, entry);
+      }
+      fs.readFile(this.buildFilePath(name), 'utf8', function (err: any, content: string) {
+        if (err != null) {
+          return safeCb(fallback)(null, undefined);
+        }
+
+        let entry: ReturnType<typeof JSON.parse>;
+        try {
+          entry = JSON.parse(content);
+        } catch (e) {
+          return safeCb(fallback)(e);
+        }
+
+        if (!!entry.cacheUntil && new Date().getTime() > entry.cacheUntil) {
+          return safeCb(fallback)(null, undefined);
+        }
+
+        return safeCb(fallback)(null, entry.data);
+      });
+    }
+  }
+
+  /**
+   * delete cache
+   * @param name cache key
+   * @param cb
+   */
+  deleteEntry(name: string, cb: { (e: Error, ...args: any[]): any; (err: NodeJS.ErrnoException): void }) {
+    if (this.memory) {
+      delete this.memoryCache[name];
+
+      if (!this.persist) safeCb(cb)(null);
+    }
+
+    fs.unlink(this.buildFilePath(name), cb);
+  }
+
+  /**
+   * delete cache sync
+   * @param name cache key
+   * @returns
+   */
+  deleteEntrySync(name: string) {
+    if (this.memory) {
+      delete this.memoryCache[name];
+
+      if (!this.persist) return;
+    }
+
+    fs.unlinkSync(this.buildFilePath(name));
+  }
+
+  getCacheDir() {
+    return path.normalize(this.base + '/' + (this.name || 'cache'));
+  }
+
+  /**
+   * remove current cache directory
+   * @param cb
+   * @returns
+   */
+  unlink(cb: { (e: Error, ...args: any[]): any; (e: Error, ...args: any[]): any }) {
+    if (this.persist) {
+      if (typeof cb !== 'function') {
+        return fs.rmSync(this.getCacheDir(), { recursive: true, force: true });
+      } else {
+        return fs.rm(this.getCacheDir(), { recursive: true, force: true }, safeCb(cb));
+      }
+    }
+
+    safeCb(cb)(null);
+  }
+
+  private transformFileNameToKey(fileName: string) {
+    return fileName.slice(0, -5);
+  }
+
+  /**
+   * get all cache keys
+   * @param cb
+   * @returns
+   */
+  keys(cb: (e: Error, ...args: any[]) => any) {
+    const self = this;
+    cb = safeCb(cb);
+
+    if (this.memory && !this.persist) return cb(null, Object.keys(this.memoryCache));
+
+    fs.readdir(this.getCacheDir(), onDirRead);
+
+    function onDirRead(err: Error, files: string[]) {
+      return err ? cb(err) : cb(err, files.map(self.transformFileNameToKey));
+    }
+  }
+
+  /**
+   * get cache keys sync
+   * @returns
+   */
+  keysSync() {
+    const self = this;
+    if (this.memory && !this.persist) return Object.keys(this.memoryCache);
+    if (fs.existsSync(this.getCacheDir())) {
+      return fs.readdirSync(this.getCacheDir()).map(self.transformFileNameToKey);
+    } else {
+      return [];
+    }
+  }
+
+  /**
+   * get all values
+   * @returns
+   */
+  valuesSync() {
+    return this.keysSync().map((key) => {
+      return this.getSync(key);
+    });
+  }
+
   buildFilePath(name: string) {
-    const cacheDir = path.normalize(this.base + '/' + (this.name || 'cache'));
-    return path.normalize(cacheDir + '/' + name + '.json');
+    return path.normalize(this.getCacheDir() + '/' + name + '.json');
   }
 
   buildCacheEntry(data: any) {
