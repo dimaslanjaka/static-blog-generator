21ff7fc52a75a4c61569e81d6e202bc0b1402afe
dimaslanjaka <dimaslanjaka@gmail.com>
2023-04-20 14:52:11 +0700

    chore(utility): hotfix exports

--- packages/sbg-utility/src/sitemap-crawler/index.ts
+++ packages/sbg-utility/src/sitemap-crawler/index.ts
@@ -3,9 +3,5 @@ import * as siteMap from './sitemap-crawler';
 //module 'sitemap-crawler'
 
 export * from './sitemap-crawler';
-export {
-  sitemapCrawler,
-  sitemapCrawlerAsync,
-  SiteMapCrawlerCore as SiteMapCrawler
-} from './sitemap-crawler';
+export { SiteMapCrawlerCore as SiteMapCrawler, sitemapCrawler, sitemapCrawlerAsync } from './sitemap-crawler';
 export default siteMap.sitemapCrawler;
