e786e68412098df1631c92a21b4aa77af974d693
dimaslanjaka <dimaslanjaka@gmail.com>
2023-04-24 16:52:04 +0700

    refactor(test): extends timeout

--- packages/sbg-api/test/validate-copy.ts
+++ packages/sbg-api/test/validate-copy.ts
@@ -11,8 +11,8 @@ import { Application } from '../src';
 export default function validateCopy(api: Application) {
   test('run copy', function (done) {
     api.copy().then(() => done());
-  }, 60000);
+  }, 600000);
   test('validate total markdown', function () {
     expect(readdirSync(join(api.config.cwd, api.config.source_dir, '_posts')).length).toBeGreaterThan(8);
-  }, 60000);
+  }, 600000);
 }
