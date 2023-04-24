d69a2e81b2432ff43c7b31cb25278676caf0b87c
dimaslanjaka <dimaslanjaka@gmail.com>
2023-04-24 15:16:12 +0700

    feat: add config test

---
+++ packages/sbg-utility/test/config.test.ts
@@ -0,0 +1,48 @@
+import { beforeAll, describe, it } from '@jest/globals';
+import spawn from 'cross-spawn';
+import { resolve } from 'upath';
+import { deployConfig, fetchConfig, getConfig, setConfig } from '../src';
+import findYarnRootWorkspace from '../src/utils/findYarnRootWorkspace';
+
+describe('CONFIG', function () {
+  let testCwd = '';
+  beforeAll(async function () {
+    const rootWorkspace = findYarnRootWorkspace({ base_dir: resolve(__dirname, '..') });
+    const listWorkspaces = await spawn
+      .async('yarn', 'workspaces list --json'.split(' '), { cwd: resolve(__dirname, '..'), shell: true })
+      .then((res) =>
+        res.stdout
+          .split(/\r?\n/gm)
+          .map((str) => str.length > 10 && (JSON.parse(str.trim()) as { name: string; location: string }))
+          .filter((o) => typeof o === 'object')
+          .map((o) => {
+            o.location = resolve(rootWorkspace, o.location);
+            return o;
+          })
+      );
+    testCwd = listWorkspaces.filter((o) => o.name === 'test')[0].location;
+  }, 70000);
+
+  it('fetchConfig()', function () {
+    expect(() => fetchConfig(__dirname)).toThrow('ENOENT: no such file or directory');
+    expect(() => fetchConfig(testCwd)).not.toThrow('ENOENT: no such file or directory');
+  }, 70000);
+
+  it('setConfig()', function () {
+    expect(() => setConfig({ silent: true })).not.toThrow();
+  }, 70000);
+
+  it('deployConfig()', function () {
+    expect(typeof deployConfig).toBe('function');
+    const cfg = deployConfig();
+    expect(cfg).toHaveProperty('github');
+    expect(cfg).toHaveProperty('deployDir');
+  }, 70000);
+
+  it('getConfig()', () => {
+    expect(typeof getConfig).toBe('function');
+    const cfg = getConfig();
+    expect(cfg).toHaveProperty('title');
+    expect(cfg).toHaveProperty('url');
+  }, 70000);
+});
