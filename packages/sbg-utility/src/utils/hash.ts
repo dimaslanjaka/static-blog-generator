c6848e8da99b3dbbac4851782e5ed69804c274c4
dimaslanjaka <dimaslanjaka@gmail.com>
2023-04-26 15:46:27 +0700

    feat(hash): add md5 utility

--- packages/sbg-utility/src/utils/hash.ts
+++ packages/sbg-utility/src/utils/hash.ts
@@ -5,6 +5,47 @@ import * as glob from 'glob';
 import * as path from 'upath';
 
 /**
+ * MD5 file synchronously
+ * @param path
+ * @returns
+ */
+export function md5FileSync(path: string) {
+  let fileBuffer = Buffer.from(path);
+  if (fs.existsSync(path)) {
+    if (fs.statSync(path).isFile()) fileBuffer = fs.readFileSync(path);
+  }
+  const hashSum = crypto.createHash('md5'); // sha256
+  hashSum.update(fileBuffer);
+  return hashSum.digest('hex');
+}
+
+/**
+ * PHP MD5 Equivalent
+ * @param data
+ * @returns
+ */
+export function md5(data: string) {
+  return crypto.createHash('md5').update(data).digest('hex');
+}
+
+export default function md5File(path: string) {
+  return new Promise((resolve, reject) => {
+    const output = crypto.createHash('md5');
+    const input = fs.createReadStream(path);
+
+    input.on('error', (err) => {
+      reject(err);
+    });
+
+    output.once('readable', () => {
+      resolve(output.read().toString('hex'));
+    });
+
+    input.pipe(output);
+  });
+}
+
+/**
  * convert file to hash
  * @param alogarithm
  * @param path
