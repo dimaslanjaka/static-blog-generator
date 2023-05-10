12892b3a4f456aea9708a01fa746c549f0ac7ba7
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-11 05:55:48 +0700

    refactor: update EOL

--- packages/sitemap-crawler/dist/index.js
+++ packages/sitemap-crawler/dist/index.js
@@ -1,11 +1,26 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.SiteMapCrawler = exports.sitemapCrawlerAsync = exports.sitemapCrawler = void 0;
-const tslib_1 = require("tslib");
-const siteMap = tslib_1.__importStar(require("./sitemap"));
+const tslib_1 = require('tslib');
+const siteMap = tslib_1.__importStar(require('./sitemap'));
 //module 'sitemap-crawler'
-var sitemap_1 = require("./sitemap");
-Object.defineProperty(exports, "sitemapCrawler", { enumerable: true, get: function () { return sitemap_1.sitemapCrawler; } });
-Object.defineProperty(exports, "sitemapCrawlerAsync", { enumerable: true, get: function () { return sitemap_1.sitemapCrawlerAsync; } });
-Object.defineProperty(exports, "SiteMapCrawler", { enumerable: true, get: function () { return sitemap_1.SiteMapCrawlerCore; } });
+var sitemap_1 = require('./sitemap');
+Object.defineProperty(exports, 'sitemapCrawler', {
+  enumerable: true,
+  get: function () {
+    return sitemap_1.sitemapCrawler;
+  }
+});
+Object.defineProperty(exports, 'sitemapCrawlerAsync', {
+  enumerable: true,
+  get: function () {
+    return sitemap_1.sitemapCrawlerAsync;
+  }
+});
+Object.defineProperty(exports, 'SiteMapCrawler', {
+  enumerable: true,
+  get: function () {
+    return sitemap_1.SiteMapCrawlerCore;
+  }
+});
 exports.default = siteMap.sitemapCrawler;
