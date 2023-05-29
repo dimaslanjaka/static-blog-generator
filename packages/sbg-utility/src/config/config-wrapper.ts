772894daf700e67233a0eb6158e1f9a9862e6528
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-30 00:12:49 +0700

    fix(TS2801): This condition will always return true since this 'Promise<void>' is always defined.

--- packages/sbg-utility/src/config/config-wrapper.ts
+++ packages/sbg-utility/src/config/config-wrapper.ts
@@ -57,9 +57,8 @@ export class createConfig<T extends Record<string, any>> extends EventEmitter {
    */
   update(value: Record<string, any>) {
     configWrapper[this.cname] = Object.assign({}, this.get(), value);
-    if (fs.access(configWrapperFile, fs.constants.W_OK)) {
-      writefile(configWrapperFile, JSON.stringify(configWrapper, null, 2));
-      this.emit('update');
-    }
+    fs.accessSync(configWrapperFile, fs.constants.W_OK);
+    writefile(configWrapperFile, JSON.stringify(configWrapper, null, 2));
+    this.emit('update');
   }
 }
