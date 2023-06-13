13f8897606b6e7771c115337f7c145d0acd24105
dimaslanjaka <dimaslanjaka@gmail.com>
2023-06-13 18:10:35 +0700

    refactor: generate exports using glob

--- packages/sbg-utility/src/utils/index.builder.ts
+++ packages/sbg-utility/src/utils/index.builder.ts
@@ -1,22 +1,24 @@
 import { spawnSync } from 'child_process';
 import fs from 'fs-extra';
+import * as glob from 'glob';
 import path from 'path';
 
 // src/functions/index.ts exports builder
 // this only for development and excluded from build config
 
 // create export
-const contents = fs
-  .readdirSync(__dirname)
-  .filter((file) => !file.includes('.builder') && path.extname(file) === '.ts')
-  .map((file) => {
-    return `export * from './${file.replace(/.ts$/, '')}';`;
-  });
-// dump
-console.log(contents);
-// fix eslint
-contents.push('', '//', '');
+glob.glob('**/*.{ts,js,jsx,tsx}', { ignore: ['**/*.builder.*'], cwd: __dirname, posix: true }).then((files) => {
+  const contents = files
+    .filter((file) => !file.includes('.builder') && path.extname(file) === '.ts')
+    .map((file) => {
+      return `export * from './${file.replace(/.(ts|js|tsx|jsx)$/, '')}';`;
+    });
+  // dump
+  console.log(contents);
+  // fix eslint
+  contents.push('', '//', '');
 
-fs.writeFileSync(path.join(__dirname, 'index.ts'), contents.join('\n'));
+  fs.writeFileSync(path.join(__dirname, 'index.ts'), contents.join('\n'));
 
-spawnSync('eslint', ['--fix', 'src/**/*.ts'], { cwd: path.join(__dirname, '../..') });
+  spawnSync('eslint', ['--fix', 'src/**/*.ts'], { cwd: path.join(__dirname, '../..') });
+});
