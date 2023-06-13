7901aedb86e281795b896670ae206448d3fd5f9e
dimaslanjaka <dimaslanjaka@gmail.com>
2023-06-13 13:54:53 +0700

    docs: add JSDoc

--- packages/sbg-utility/src/utils/JSON-serializer.js
+++ packages/sbg-utility/src/utils/JSON-serializer.js
@@ -45,6 +45,12 @@ const set = (known, input, value) => {
   return index;
 };
 
+/**
+ * parse json string with circular references
+ * @param {string} text
+ * @param {(...args:any[])=>any} reviver
+ * @returns
+ */
 const parse = (text, reviver) => {
   const input = $parse(text, Primitives).map(primitives);
   const value = input[0];
@@ -52,8 +58,14 @@ const parse = (text, reviver) => {
   const tmp = typeof value === object && value ? revive(input, new Set(), value, $) : value;
   return $.call({ '': tmp }, '', tmp);
 };
-module.exports.parse = parse;
 
+/**
+ * json stringify object with circular references
+ * @param {any} value
+ * @param {(this: any, key: string, value: any) => any} [replacer]
+ * @param {string|number} [space]
+ * @returns
+ */
 const stringify = (value, replacer, space) => {
   const $ =
     replacer && typeof replacer === object
@@ -85,10 +97,12 @@ const stringify = (value, replacer, space) => {
     return after;
   }
 };
+
+module.exports.parse = parse;
 module.exports.stringify = stringify;
 
 /**
- * to JSON
+ * stringify circular object into JSON
  * @param {any} any
  * @returns
  */
@@ -96,7 +110,7 @@ const toJSON = (any) => $parse(stringify(any));
 module.exports.toJSON = toJSON;
 
 /**
- * from JSON
+ * parse circular object from JSON
  * @param {string} any
  * @returns
  */
