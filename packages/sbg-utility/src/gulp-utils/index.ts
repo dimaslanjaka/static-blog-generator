79797e70a1093089a5cecd2287529d66f6d0199c
dimaslanjaka <dimaslanjaka@gmail.com>
2023-06-13 18:11:34 +0700

    chore: update exports

--- packages/sbg-utility/src/gulp-utils/index.ts
+++ packages/sbg-utility/src/gulp-utils/index.ts
@@ -1,6 +1,7 @@
-export * from './gulp-cache';
-export * from './gulp-dom';
-export * from './gulp-options';
+export * from './index';
 export * from './gulp.debug';
+export * from './gulp-options';
+export * from './gulp-dom';
+export * from './gulp-cache';
 
 //
