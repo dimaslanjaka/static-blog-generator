28885ddb763a6e41f967246142f63de315add732
dimaslanjaka <dimaslanjaka@gmail.com>
2023-06-13 17:36:01 +0700

    chore: update build

---
+++ packages/static-blog-generator/dist/index.d.ts
@@ -0,0 +1,8 @@
+export * from 'sbg-api';
+export * from 'sbg-cli';
+export * from 'sbg-server';
+export * from 'sbg-utility';
+export * as api from 'sbg-api';
+export * as cli from 'sbg-cli';
+export * as server from 'sbg-server';
+export * as utility from 'sbg-utility';
