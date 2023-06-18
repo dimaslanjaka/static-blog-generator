39e2d6d316577859c4c9f6cf1a5c16b96a058392
dimaslanjaka <dimaslanjaka@gmail.com>
2023-06-18 18:14:32 +0700

    chore: clean build

---
+++ packages/sbg-api/dist/gulp.seo.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="node" />
+import gulp from 'gulp';
+/**
+ * Auto seo runner
+ * @param cwd working directory to scan html's
+ */
+export declare function taskSeo(_done: gulp.TaskFunctionCallback | null | undefined, cwd: string): NodeJS.ReadWriteStream;
