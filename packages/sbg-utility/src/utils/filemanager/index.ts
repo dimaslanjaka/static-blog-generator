3f9c99a672ce7c0eb0d32d6600050cb323f44f1e
dimaslanjaka <dimaslanjaka@gmail.com>
2023-09-05 05:18:03 +0700

    chore: split stream to their file remove export fs

--- packages/sbg-utility/src/utils/filemanager/index.ts
+++ packages/sbg-utility/src/utils/filemanager/index.ts
@@ -1,8 +1,5 @@
 // filemanager
 
-import * as fs from 'fs-extra';
-import * as path from 'upath';
-
 export * from './copy';
 export * from './del';
 export * from './emptyDir';
@@ -11,19 +8,8 @@ export * from './images';
 export * from './normalizePath';
 export { normalizePath as joinPath, normalizePath as pathJoin } from './normalizePath';
 export * from './readDir';
+export * from './stream';
 export * from './writefile';
-export { fs, path };
-
-/**
- * create writestream (auto create dirname)
- * @param dest
- * @param options
- * @returns
- */
-export function createWriteStream(dest: string, options?: Parameters<(typeof fs)['createWriteStream']>[1]) {
-  if (!fs.existsSync(path.dirname(dest))) fs.mkdirSync(path.dirname(dest));
-  return fs.createWriteStream(dest, options);
-}
 
 /**
  * is non-markdown file
