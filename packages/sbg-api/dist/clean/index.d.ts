761150b519de4629c4eb14c98f78f2ca338ba0ef
dimaslanjaka <dimaslanjaka@gmail.com>
2024-01-20 12:10:00 +0700

    update indexes after migrated branch

--- packages/sbg-api/dist/clean/index.d.ts
+++
@@ -1,4 +0,0 @@
-import cleanArchive from './cleanArchive';
-import { cleanDb } from './cleanDb';
-import { cleanGeneratedPosts } from './cleanGeneratedPosts';
-export { cleanDb, cleanArchive, cleanGeneratedPosts };
