11e417482b2fd1aeaf7a7d00390aa787e451f0fb
dimaslanjaka <dimaslanjaka@gmail.com>
2024-02-17 11:54:26 +0700

    using `npx`

--- packages/sbg-server/gulpfile.js
+++ packages/sbg-server/gulpfile.js
@@ -55,7 +55,7 @@ const copyPublic = () =>
 gulp.task('copy', gulp.series(copyPublic, copyNonJS));
 
 function tsc(done) {
-  spawnAsync(cmd('tsc'), ['--build', 'tsconfig.build.json'], {
+  spawnAsync('npx', ['tsc', '--build', 'tsconfig.build.json'], {
     cwd: __dirname,
     shell: true,
     stdio: 'inherit'
