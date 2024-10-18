c62fc2b0526fe497f70524ef8cecb36965ffdba0
dimaslanjaka <dimaslanjaka@gmail.com>
2024-10-18 17:11:44 +0700

    fix(TS2684):  Cannot assign an abstract constructor type to a non-abstract constructor type

--- packages/sbg-server/src/middleware/session-file-store.ts
+++ packages/sbg-server/src/middleware/session-file-store.ts
@@ -11,38 +11,31 @@ import * as sessionFileHelper from './session-file-helpers';
  */
 export function sessionFileStore(session: typeof import('express-session')) {
   const Store = session.Store;
-  type implStore = Partial<typeof Store> & {
-    options: Record<string, any>;
-  };
 
-  /**
-   * Initialize FileStore with the given `options`
-   *
-   * @param options (optional)
-   *
-   * @api public
-   */
-  class FileStore implements implStore {
+  class FileStore extends Store {
     options: ReturnType<typeof sessionFileHelper.defaults>;
-    store: typeof Store;
-    constructor(options: ReturnType<typeof sessionFileHelper.defaults>) {
-      const self = this;
 
-      // fallback options as empty object
+    constructor(
+      options: ReturnType<typeof sessionFileHelper.defaults> = {} as ReturnType<
+        typeof sessionFileHelper.defaults
+      >
+    ) {
+      // Call the parent Store constructor
+      super(options as any);
+
+      // Fallback options as empty object
       const opt = options || ({} as typeof options);
       if (!fs.existsSync(opt.path)) {
         fs.mkdirSync(opt.path, { recursive: true });
       }
 
-      // call session store
-      Store.call(self, opt);
-      this.store = Store;
-
-      self.options = sessionFileHelper.defaults(opt);
-      fs.mkdirsSync(self.options.path);
-      sessionFileHelper.scheduleReap(self.options);
-      options.reapIntervalObject = self.options.reapIntervalObject;
+      // Initialize options
+      this.options = sessionFileHelper.defaults(opt);
+      fs.mkdirsSync(this.options.path);
+      sessionFileHelper.scheduleReap(this.options);
+      options.reapIntervalObject = this.options.reapIntervalObject;
     }
+
     /**
      * Attempts to fetch session from a session file by the given `sessionId`
      *
@@ -54,6 +47,7 @@ export function sessionFileStore(session: typeof import('express-session')) {
     get(sessionId: string, callback: (...args: any[]) => any) {
       sessionFileHelper.get(sessionId, this.options, callback);
     }
+
     /**
      * Attempts to commit the given session associated with the given `sessionId` to a session file
      *
@@ -83,9 +77,8 @@ export function sessionFileStore(session: typeof import('express-session')) {
     /**
      * Attempts to unlink a given session by its id
      *
-     * @param    sessionId   Files are serialized to disk by their
-     *                                sessionId
-     * @param  callback
+     * @param sessionId
+     * @param callback
      *
      * @api public
      */
@@ -96,7 +89,7 @@ export function sessionFileStore(session: typeof import('express-session')) {
     /**
      * Attempts to fetch number of the session files
      *
-     * @param  callback
+     * @param callback
      *
      * @api public
      */
@@ -107,7 +100,7 @@ export function sessionFileStore(session: typeof import('express-session')) {
     /**
      * Attempts to clear out all of the existing session files
      *
-     * @param  callback
+     * @param callback
      *
      * @api public
      */
@@ -118,7 +111,7 @@ export function sessionFileStore(session: typeof import('express-session')) {
     /**
      * Attempts to find all of the session files
      *
-     * @param  callback
+     * @param callback
      *
      * @api public
      */
@@ -129,8 +122,8 @@ export function sessionFileStore(session: typeof import('express-session')) {
     /**
      * Attempts to detect whether a session file is already expired or not
      *
-     * @param    sessionId
-     * @param  callback
+     * @param sessionId
+     * @param callback
      *
      * @api public
      */
@@ -138,11 +131,6 @@ export function sessionFileStore(session: typeof import('express-session')) {
       sessionFileHelper.expired(sessionId, this.options, callback);
     }
   }
-
-  /**
-   * Inherit from Store
-   */
-  FileStore.prototype['__proto__'] = Store.prototype;
 
   return FileStore;
 }
