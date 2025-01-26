faa31127bf61b2f2959a208292e326162158a8a8
dimaslanjaka <dimaslanjaka@gmail.com>
2025-01-26 13:48:48 +0700

    fix: copy generated hexo site into deployment directory

--- packages/sbg-api/src/deploy/copy.ts
+++ packages/sbg-api/src/deploy/copy.ts
@@ -1,3 +1,4 @@
+import ansiColors from 'ansi-colors';
 import fs from 'fs-extra';
 import { getConfig, normalizePathUnix } from 'sbg-utility';
 import { gulpCopyAsync } from '../utils/gulp-utils';
@@ -8,28 +9,42 @@ export interface deployCopyOptions {
 }
 
 /**
- * copy generated site to deployment directory
- * @param opt
- * @param ignore
+ * Copy generated site to deployment directory.
+ * @param opt - Optional configuration for deploying.
+ * @param ignore - Files or directories to exclude from the copy.
  */
 export async function deployCopy(opt?: deployCopyOptions) {
   const defaultConf = getConfig();
   const config = Object.assign(defaultConf, opt?.config || {});
-  await fs.copy(config.public_dir, config.deploy.deployDir, { overwrite: true, errorOnExist: false });
+  const public_dir = normalizePathUnix(config.cwd, config.public_dir);
+  const deploy_dir = normalizePathUnix(config.deploy.deployDir);
+
+  // Log with color for variables inside `${}`
+  console.log(`Copying folder ${ansiColors.cyan(public_dir)} -> ${ansiColors.cyan(deploy_dir)}`);
+  await fs.copy(public_dir, deploy_dir, { overwrite: true, errorOnExist: false });
+
   const ignore = ['**/.git/**', '**/.gitignore', '**/node_modules/**', '**/tmp/**'].concat(...config.exclude);
-  await gulpCopyAsync(['**/*.*', '**/.*.*', '**/*'], config.deploy.deployDir, {
-    ignore,
-    cwd: config.public_dir
-  });
-  // Copy post assets into deploy directory
-  const sourcePostDir = normalizePathUnix(config.cwd, config.post_dir);
-  // const generatedPostDir = normalizePathUnix(config.cwd, config.source_dir, '_posts');
-  await gulpCopyAsync(
-    ['**/*.{jpg,png,jpeg,bmp,svg,webp,gif,ico,tiff,tif,heif,heic,raw,apng,jfif}'],
-    config.deploy.deployDir,
-    {
-      ignore,
-      cwd: sourcePostDir
-    }
+  console.log(
+    `Copying all files ${ansiColors.cyan(public_dir)} -> ${ansiColors.cyan(deploy_dir)} except ${ansiColors.yellow(ignore.join(', '))}`
   );
+  await gulpCopyAsync(['**/*.*', '**/.*.*', '**/*'], deploy_dir, {
+    ignore,
+    cwd: public_dir
+  });
+
+  // Copy source post assets into deployment directory
+  const sourcePostDir = normalizePathUnix(config.cwd, config.post_dir);
+  console.log(`Copying images ${ansiColors.cyan(sourcePostDir)} -> ${ansiColors.cyan(deploy_dir)}`);
+  await gulpCopyAsync(['**/*.{jpg,png,jpeg,bmp,svg,webp,gif,ico,tiff,tif,heif,heic,raw,apng,jfif}'], deploy_dir, {
+    ignore,
+    cwd: sourcePostDir
+  });
+
+  // Copy generated post assets into deployment directory
+  const generatedPostDir = normalizePathUnix(config.cwd, config.source_dir, '_posts');
+  console.log(`Copying images ${ansiColors.cyan(generatedPostDir)} -> ${ansiColors.cyan(deploy_dir)}`);
+  await gulpCopyAsync(['**/*.{jpg,png,jpeg,bmp,svg,webp,gif,ico,tiff,tif,heif,heic,raw,apng,jfif}'], deploy_dir, {
+    ignore,
+    cwd: generatedPostDir
+  });
 }
--- packages/sbg-api/test/deploy/copy.explicit.ts
+++ packages/sbg-api/test/deploy/copy.explicit.ts
@@ -7,7 +7,12 @@ import { fixturesCwd, testCwd } from '../env.mjs';
 process.cwd = () => (typeof testCwd === 'string' ? testCwd : fixturesCwd);
 
 // start import application
+import fs from 'fs-extra';
+import { normalizePath } from 'sbg-utility';
 import { Application } from '../../src';
 
 const api = new Application(fixturesCwd);
+// cleanup deployment directory
+fs.rmSync(normalizePath(api.cwd, '.deploy_git'), { force: true, recursive: true });
+// copy generated hexo site into deployment directory
 api.deploy.copy();
