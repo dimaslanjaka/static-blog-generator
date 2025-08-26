12ce58116822cbc70100c9626b2ea55273eaeeef
dimaslanjaka <dimaslanjaka@gmail.com>
2025-08-26 12:38:07 +0700

    refactor(sbg-utility): move fs and path exports to new external module
    - Created `src/external.ts` to centralize external module imports (`fs-extra`, `upath`)
    - Updated `index.ts` to re-export from `external.ts` instead of direct imports
    - Simplified module export structure

--- packages/sbg-utility/src/index.ts
+++ packages/sbg-utility/src/index.ts
@@ -1,13 +1,11 @@
 // Pollyfill globalThis in Node.js
 import './pollyfill/global-file';
 
-// Importing necessary modules
-import fs from 'fs-extra';
-import path from 'upath';
+// Import/Export necessary modules
 
 import * as wildcards from './index-exports';
+export * from './external';
 export * from './index-exports';
-export { fs, path };
 export default wildcards;
 
 //
