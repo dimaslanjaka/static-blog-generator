48f046f27b3eeb62b972db2adb58b3b9d4811a71
dimaslanjaka <dimaslanjaka@gmail.com>
2026-05-10 07:33:33 +0700

    fix: add missing 'it' import in empty.test.ts

--- packages/sbg-utility/test/utils/empty.test.ts
+++ packages/sbg-utility/test/utils/empty.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect } from '@jest/globals';
+import { describe, expect, it } from '@jest/globals';
 import isEmpty, { isNotEmpty } from '../../src/utils/empty';
 
 describe('isEmpty', () => {
